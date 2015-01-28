395e68715ca1710bfbc30246c4379e86bd65907f
Antonin Bas <antonin@barefootnetworks.com>
2015-01-27 17:22:27 -0800

    one phv proposal, where the PHV class is only storing pointers to fields and headers

--- modules/behavioral_sim/module/src/phv.h
+++ modules/behavioral_sim/module/src/phv.h
@@ -1,22 +1,69 @@
 #ifndef _BM_PHV_H_
 #define _BM_PHV_H_
 
+#include <cassert>
+
 #include "fields.h"
+
+class Header
+{
+  Field **fields;
+  int num_fields;
+  int field_offset;
+
+public:
+  Header(int num_fields)
+    : num_fields(num_fields) {
+    field_offset = 0;
+    fields = new Field *[num_fields];
+  }
+
+  ~Header() {
+    delete[] fields;
+  }
+
+  void add_field(Field *field) {
+    fields[field_offset++] = field;
+  }
+};
 
 class PHV
 {
+private:
   int num_fields;
-  Field *fields;
+  int num_headers;
+
+  Field **fields;
+  Header **headers;
 
 public:
-  PHV(int n) {
-    num_fields = n;
-    fields = new Field[n];
+  PHV(int num_fields, int num_headers)
+    : num_fields(num_fields), num_headers(num_headers) {
+    fields = new Field *[num_fields];
+    headers = new Header *[num_headers];
+  }
+
+  ~PHV() {
+    delete[] headers;
+    delete[] fields;
+  }
+
+  void add_field(int field_index, Field *field) {
+    fields[field_index] = field;
+  }
+
+  void add_header(int header_index, Header *header) {
+    headers[header_index] = header;
   }
 
   Field *get_field(int field_index) const {
-    return &fields[field_index];
+    return fields[field_index];
   }
+
+  Header *get_header(int header_index) const {
+    return headers[header_index];
+  }
+
 };
 
 
