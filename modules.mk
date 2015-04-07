09764f23a83276a6b63d82163e14947c1c437527
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-04-07 14:40:08 -0700

    Move to automake for bf_lpm_trie, bm_sim, bm_runtime, and BMI

--- modules.mk
+++
@@ -1,8 +0,0 @@
-THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
-MODULES_DIR := $(THIS_DIR)modules
-MODULES_NAMES := jsoncpp BMI bf_lpm_trie bm_sim bm_runtime
-MODULES := $(patsubst %, $(MODULES_DIR)/%, $(MODULES_NAMES))
-
-include $(patsubst %, %/module.mk, $(MODULES))
-
