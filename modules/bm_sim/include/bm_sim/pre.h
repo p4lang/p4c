8b12cd89d129b79015fcca673220558986a98453
Antonin Bas <antonin@barefootnetworks.com>
2015-04-14 15:56:51 -0700

    changed tab from 4 spaces to 2, added McPre::PortMap class to hide implementation of port map

--- modules/bm_sim/include/bm_sim/pre.h
+++ modules/bm_sim/include/bm_sim/pre.h
@@ -9,86 +9,108 @@
 #include <boost/thread/shared_mutex.hpp>
 
 class McPre {
-    public:
-        typedef unsigned int mgrp_t;
-        typedef unsigned int rid_t;
-        typedef unsigned int egress_port_t;
-        typedef uintptr_t mgrp_hdl_t;
-        typedef uintptr_t l1_hdl_t;
-        typedef uintptr_t l2_hdl_t;
-        static constexpr int PORT_MAP_SIZE = 512;
+public:
+  typedef unsigned int mgrp_t;
+  typedef unsigned int rid_t;
+  typedef unsigned int egress_port_t;
+  typedef uintptr_t mgrp_hdl_t;
+  typedef uintptr_t l1_hdl_t;
+  typedef uintptr_t l2_hdl_t;
+  
+public:
+  enum McReturnCode {
+    SUCCESS = 0,
+    TABLE_FULL,
+    INVALID_MGID,
+    INVALID_L1_HANDLE,
+    INVALID_L2_HANDLE,
+    ERROR
+  };
+  
+  struct McPre_In {
+    mgrp_t mgid;
+  };
+  
+  struct McPre_Out {
+    rid_t rid;
+    egress_port_t egress_port;
+  };
 
-    private:
-        static constexpr int MGID_TABLE_SIZE = 4096;
-        static constexpr int L1_MAX_ENTRIES = 4096;
-        static constexpr int L2_MAX_ENTRIES = 8192;
+  class PortMap {
+  public:
+    static constexpr size_t PORT_MAP_SIZE = 512;
 
-        struct L1Entry {
-            mgrp_hdl_t mgrp_hdl;
-            rid_t rid;
-            l2_hdl_t l2_hdl;
+  public:
+    typedef std::bitset<PORT_MAP_SIZE>::reference reference;
 
-            L1Entry() {}
-            L1Entry(rid_t rid) : rid(rid) {}
-        };
+  public:
+    bool operator[] (size_t pos) const { return port_map[pos]; }
+    reference operator[] (size_t pos) { return port_map[pos]; }
 
-        struct L2Entry {
-            l1_hdl_t l1_hdl;
-            std::bitset<PORT_MAP_SIZE> port_map;
+    constexpr size_t size() noexcept { return port_map.size(); }
 
-            L2Entry() {}
-            L2Entry(l1_hdl_t l1_hdl, const std::bitset<PORT_MAP_SIZE> &port_map)
-	    : l1_hdl(l1_hdl), port_map(port_map) {}
-        };
+  private:
+    std::bitset<PORT_MAP_SIZE> port_map;
+  };
+  
+  McPre() {}
+  McReturnCode mc_mgrp_create(const mgrp_t, mgrp_hdl_t *);
+  McReturnCode mc_mgrp_destroy(const mgrp_hdl_t);
+  McReturnCode mc_l1_node_create(const rid_t, l1_hdl_t *);
+  McReturnCode mc_l1_node_associate(const mgrp_hdl_t, const l1_hdl_t);
+  McReturnCode mc_l1_node_destroy(const l1_hdl_t);
+  McReturnCode mc_l2_node_create(const l1_hdl_t, l2_hdl_t *,
+				 const PortMap &port_map);
+  McReturnCode mc_l2_node_update(const l2_hdl_t l2_hdl,
+				 const PortMap &port_map);
+  McReturnCode mc_l2_node_destroy(const l2_hdl_t);
+  std::vector<McPre_Out> replicate(const McPre_In) const;
+  
+  McPre(const McPre &other) = delete;
+  McPre &operator=(const McPre &other) = delete;
+  McPre(McPre &&other) = delete;
+  McPre &operator=(McPre &&other) = delete;
 
-        struct MgidEntry {
-            mgrp_t mgid;
-            std::vector<l1_hdl_t> l1_list;
+private:
+  static constexpr int MGID_TABLE_SIZE = 4096;
+  static constexpr int L1_MAX_ENTRIES = 4096;
+  static constexpr int L2_MAX_ENTRIES = 8192;
+  
+  struct L1Entry {
+    mgrp_hdl_t mgrp_hdl;
+    rid_t rid;
+    l2_hdl_t l2_hdl;
+    
+    L1Entry() {}
+    L1Entry(rid_t rid) : rid(rid) {}
+  };
+  
+  struct L2Entry {
+    l1_hdl_t l1_hdl;
+    PortMap port_map;
+    
+    L2Entry() {}
+    L2Entry(l1_hdl_t l1_hdl, const PortMap &port_map)
+      : l1_hdl(l1_hdl), port_map(port_map) {}
+  };
+  
+  struct MgidEntry {
+    mgrp_t mgid;
+    std::vector<l1_hdl_t> l1_list;
+    
+    MgidEntry() {}
+    MgidEntry(mgrp_t mgid) : mgid(mgid) {}
+  };
 
-            MgidEntry() {}
-            MgidEntry(mgrp_t mgid) : mgid(mgid) {}
-        };
-
-        std::unordered_map<mgrp_hdl_t, MgidEntry> mgid_entries;
-        std::unordered_map<l1_hdl_t, L1Entry> l1_entries;
-        std::unordered_map<l2_hdl_t, L2Entry> l2_entries;
-        HandleMgr l1_handles;
-        HandleMgr l2_handles;
-        mutable boost::shared_mutex mgid_lock;
-        mutable boost::shared_mutex l1_lock;
-        mutable boost::shared_mutex l2_lock;
-
-    public:
-
-        enum McReturnCode {
-            SUCCESS = 0,
-            TABLE_FULL,
-            INVALID_MGID,
-            INVALID_L1_HANDLE,
-            INVALID_L2_HANDLE,
-            ERROR
-        };
-
-        struct McPre_In {
-            mgrp_t mgid;
-        };
-
-        struct McPre_Out {
-            rid_t rid;
-            egress_port_t egress_port;
-        };
-
-        McPre() {}
-        McReturnCode mc_mgrp_create(const mgrp_t, mgrp_hdl_t *);
-        McReturnCode mc_mgrp_destroy(const mgrp_hdl_t);
-        McReturnCode mc_l1_node_create(const rid_t, l1_hdl_t *);
-        McReturnCode mc_l1_node_associate(const mgrp_hdl_t, const l1_hdl_t);
-        McReturnCode mc_l1_node_destroy(const l1_hdl_t);
-        McReturnCode mc_l2_node_create(const l1_hdl_t, l2_hdl_t *,
-				       const std::bitset<PORT_MAP_SIZE> &port_map);
-        McReturnCode mc_l2_node_update(const l2_hdl_t l2_hdl,
-				       const std::bitset<PORT_MAP_SIZE> &port_map);
-        McReturnCode mc_l2_node_destroy(const l2_hdl_t);
-        std::vector<McPre_Out> replicate(const McPre_In);
+private:
+  std::unordered_map<mgrp_hdl_t, MgidEntry> mgid_entries;
+  std::unordered_map<l1_hdl_t, L1Entry> l1_entries;
+  std::unordered_map<l2_hdl_t, L2Entry> l2_entries;
+  HandleMgr l1_handles;
+  HandleMgr l2_handles;
+  mutable boost::shared_mutex mgid_lock;
+  mutable boost::shared_mutex l1_lock;
+  mutable boost::shared_mutex l2_lock;
 };
+
 #endif
--- tests/test_pre.cpp
+++ tests/test_pre.cpp
@@ -4,77 +4,79 @@
 
 TEST(McPre, Replicate)
 {
-    McPre pre;
-    McPre::mgrp_t mgid = 0x400;
-    McPre::mgrp_hdl_t mgrp_hdl;
-    std::vector<McPre::l1_hdl_t> l1_hdl_list;
-    std::vector<McPre::l2_hdl_t> l2_hdl_list;
-    McPre::rid_t rid_list[] = {0x200, 0x211, 0x221};
-    std::vector<std::vector<McPre::egress_port_t>> port_list1 = {{1, 4}, {5, 6}, {2, 7, 8, 9}};
-    std::vector<std::vector<McPre::egress_port_t>> port_list2 = {{1, 4}, {5, 6}, {2, 9}};
-    McPre::McReturnCode rc;
-    McPre::McPre_In ingress_info;
-    std::vector<McPre::McPre_Out> egress_info;
-    int count = 0;
+  McPre pre;
+  McPre::mgrp_t mgid = 0x400;
+  McPre::mgrp_hdl_t mgrp_hdl;
+  std::vector<McPre::l1_hdl_t> l1_hdl_list;
+  std::vector<McPre::l2_hdl_t> l2_hdl_list;
+  McPre::rid_t rid_list[] = {0x200, 0x211, 0x221};
+  std::vector<std::vector<McPre::egress_port_t>> port_list1 = {{1, 4}, {5, 6}, {2, 7, 8, 9}};
+  std::vector<std::vector<McPre::egress_port_t>> port_list2 = {{1, 4}, {5, 6}, {2, 9}};
+  McPre::McReturnCode rc;
+  McPre::McPre_In ingress_info;
+  std::vector<McPre::McPre_Out> egress_info;
+  unsigned int count = 0;
 
-    rc = pre.mc_mgrp_create(mgid, &mgrp_hdl);
+  rc = pre.mc_mgrp_create(mgid, &mgrp_hdl);
+  ASSERT_EQ(rc, McPre::SUCCESS);
+
+  for (unsigned int i = 0; i < 3; i++) {
+    McPre::l1_hdl_t l1_hdl;
+    rc = pre.mc_l1_node_create(rid_list[i], &l1_hdl);
     ASSERT_EQ(rc, McPre::SUCCESS);
-
-    for (unsigned int i = 0; i < 3; i++) {
-        McPre::l1_hdl_t l1_hdl;
-        rc = pre.mc_l1_node_create(rid_list[i], &l1_hdl);
-        ASSERT_EQ(rc, McPre::SUCCESS);
-        l1_hdl_list.push_back(l1_hdl);
-        rc = pre.mc_l1_node_associate(mgrp_hdl, l1_hdl);
-        ASSERT_EQ(rc, McPre::SUCCESS);
-    }
-
-    std::bitset<McPre::PORT_MAP_SIZE> port_map[3];
-    for (unsigned int i = 0; i < 3; i++) {
-        for (unsigned int j = 0; j < port_list1[i].size(); j++) {
-            port_map[i][port_list1[i][j]] = 1;
-        }
-        McPre::l2_hdl_t l2_hdl;
-        rc = pre.mc_l2_node_create(l1_hdl_list[i], &l2_hdl, port_map[i]);
-        ASSERT_EQ(rc, McPre::SUCCESS);
-        l2_hdl_list.push_back(l2_hdl);
-    }
-
-    ingress_info.mgid = mgid;
-    egress_info = pre.replicate(ingress_info);
-    count = 0;
-    for (unsigned int i = 0; i < 3; i++) {
-        for (unsigned int j = 0; j < port_list1[i].size(); j++) {
-            ASSERT_EQ(egress_info[count].rid, rid_list[i]);
-            ASSERT_EQ(egress_info[count].egress_port, port_list1[i][j]);
-            count++;
-        }
-    }
-    ASSERT_EQ(egress_info.size(), count);
-
-    port_map[2][7] = 0;
-    port_map[2][8] = 0;
-    rc = pre.mc_l2_node_update(l2_hdl_list[2], port_map[2]);
+    l1_hdl_list.push_back(l1_hdl);
+    rc = pre.mc_l1_node_associate(mgrp_hdl, l1_hdl);
     ASSERT_EQ(rc, McPre::SUCCESS);
-    ingress_info.mgid = mgid;
+  }
 
-    egress_info = pre.replicate(ingress_info);
-    count = 0;
-    for (unsigned int i = 0; i < 3; i++) {
-        for (unsigned int j = 0; j < port_list2[i].size(); j++) {
-            ASSERT_EQ(egress_info[count].rid, rid_list[i]);
-            ASSERT_EQ(egress_info[count].egress_port, port_list2[i][j]);
-            count++;
-        }
+  McPre::PortMap port_maps[3];
+  for (unsigned int i = 0; i < 3; i++) {
+    McPre::PortMap &port_map = port_maps[i];
+    for (unsigned int j = 0; j < port_list1[i].size(); j++) {
+      port_map[port_list1[i][j]] = 1;
     }
-    ASSERT_EQ(egress_info.size(), count);
-
-    for (unsigned int i = 0; i < l1_hdl_list.size(); i++) {
-        rc = pre.mc_l2_node_destroy(l2_hdl_list[i]);
-        ASSERT_EQ(rc, McPre::SUCCESS);
-        rc = pre.mc_l1_node_destroy(l1_hdl_list[i]);
-        ASSERT_EQ(rc, McPre::SUCCESS);
-    }
-    rc = pre.mc_mgrp_destroy(mgrp_hdl);
+    McPre::l2_hdl_t l2_hdl;
+    rc = pre.mc_l2_node_create(l1_hdl_list[i], &l2_hdl, port_map);
     ASSERT_EQ(rc, McPre::SUCCESS);
+    l2_hdl_list.push_back(l2_hdl);
+  }
+
+  ingress_info.mgid = mgid;
+  egress_info = pre.replicate(ingress_info);
+  count = 0;
+  for (unsigned int i = 0; i < 3; i++) {
+    for (unsigned int j = 0; j < port_list1[i].size(); j++) {
+      ASSERT_EQ(egress_info[count].rid, rid_list[i]);
+      ASSERT_EQ(egress_info[count].egress_port, port_list1[i][j]);
+      count++;
+    }
+  }
+  ASSERT_EQ(egress_info.size(), count);
+
+  McPre::PortMap &port_map_2 = port_maps[2];
+  port_map_2[7] = 0;
+  port_map_2[8] = 0;
+  rc = pre.mc_l2_node_update(l2_hdl_list[2], port_map_2);
+  ASSERT_EQ(rc, McPre::SUCCESS);
+  ingress_info.mgid = mgid;
+  
+  egress_info = pre.replicate(ingress_info);
+  count = 0;
+  for (unsigned int i = 0; i < 3; i++) {
+    for (unsigned int j = 0; j < port_list2[i].size(); j++) {
+      ASSERT_EQ(egress_info[count].rid, rid_list[i]);
+      ASSERT_EQ(egress_info[count].egress_port, port_list2[i][j]);
+      count++;
+    }
+  }
+  ASSERT_EQ(egress_info.size(), count);
+  
+  for (unsigned int i = 0; i < l1_hdl_list.size(); i++) {
+    rc = pre.mc_l2_node_destroy(l2_hdl_list[i]);
+    ASSERT_EQ(rc, McPre::SUCCESS);
+    rc = pre.mc_l1_node_destroy(l1_hdl_list[i]);
+    ASSERT_EQ(rc, McPre::SUCCESS);
+  }
+  rc = pre.mc_mgrp_destroy(mgrp_hdl);
+  ASSERT_EQ(rc, McPre::SUCCESS);
 }
