b8043dc8cd08c0e2411a310f05e08a991e7dc4e3
Antonin Bas <antonin@barefootnetworks.com>
2015-05-06 17:46:25 -0700

    removed generated thrift files from version control, updated Makefiles

--- pd_gen/libpd.mk
+++ pd_gen/libpd.mk
@@ -2,6 +2,14 @@ LIBPD_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 $(TARGET)_TARGET := $(LIBPD_MK_DIR)$(TARGET)
 
+.thrift_gen: $(LIBPD_MK_DIR)../thrift_src/runtime.thrift
+	thrift --gen cpp -out $(LIBPD_MK_DIR)fixed-cpp/src/ $(LIBPD_MK_DIR)../thrift_src/runtime.thrift
+	rm $(LIBPD_MK_DIR)fixed-cpp/src/*skeleton.cpp
+	@touch .thrift_gen
+
+.thrift_gen_clean:
+	rm -f .thrift_gen
+
 include $(LIBPD_MK_DIR)gen-cpp/module.mk
 include $(LIBPD_MK_DIR)fixed-cpp/module.mk
 
