d3bc5add21fe9fc49ff8bb13cf224f16977431bd
Antonin Bas <antonin@barefootnetworks.com>
2015-07-01 11:39:17 -0700

    fixed bug in P4v1 primitives implementation, wrong field name for learning

--- targets/simple_switch/primitives.h
+++ targets/simple_switch/primitives.h
@@ -88,7 +88,7 @@ REGISTER_PRIMITIVE(drop);
 class generate_digest : public ActionPrimitive<const Data &, const Data &> {
   void operator ()(const Data &receiver, const Data &learn_id) {
     // discared receiver for now
-    get_field("intrinsic_metadata.learn_id").set(learn_id);
+    get_field("intrinsic_metadata.lf_field_list").set(learn_id);
   }
 };
 
