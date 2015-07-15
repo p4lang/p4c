0c22d72c727cc424d78650e01e7d6f04911546e8
Antonin Bas <antonin@barefootnetworks.com>
2015-07-15 10:15:34 -0700

    updated veth_teardown script to reflect new number of interfaces

--- tools/veth_teardown.sh
+++ tools/veth_teardown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for idx in 0 1 2 3; do
+for idx in 0 1 2 3 4 5 6 7 8; do
     intf="veth$(($idx*2))"
     if ip link show $intf &> /dev/null; then
         ip link delete $intf type veth
