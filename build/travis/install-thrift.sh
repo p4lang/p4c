5373e40b67c7ce1a815d80b824303514a973e054
Antonin Bas <antonin@barefootnetworks.com>
2015-08-11 03:54:49 -0700

    forgot installation scripts for thrift and nanomsg

---
+++ build/travis/install-thrift.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
+tar -xzvf thrift-0.9.2.tar.gz
+cd thrift-0.9.2
+./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
+# ./configure --prefix=$HOME/thrift --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
+make && sudo make install
+cd ..
