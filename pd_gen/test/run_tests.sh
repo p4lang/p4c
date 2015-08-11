16c9b49989046281895c7992b86a396a8e90f1eb
Antonin Bas <antonin@barefootnetworks.com>
2015-08-11 03:17:58 -0700

    removing pd_gen directory (was moved to a separate repo)

--- pd_gen/test/run_tests.sh
+++
@@ -1,8 +0,0 @@
-./test > test.output 2>/dev/null
-DIFF=$(diff test.output test.output.ref) 
-if [ "$DIFF" != "" ] 
-then
-    echo "FAIL!"
-else
-    echo "PASS!"
-fi
--- pd_gen/test/thrift_endpoint/include/thrift_endpoint.h
+++
@@ -1,6 +0,0 @@
-#ifndef _THRIFT_ENDPOINT_H_
-#define _THRIFT_ENDPOINT_H_
-
-int start_server(void);
-
-#endif
