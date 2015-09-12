c3cab3cfb125ba0e3a9ef9bae76df1275e31c5b9
Antonin Bas <antonin@barefootnetworks.com>
2015-09-11 19:27:30 -0700

    compiling thrift using 2 jobs (for Travis)

--- build/travis/install-thrift.sh
+++ build/travis/install-thrift.sh
@@ -5,5 +5,5 @@ tar -xzvf thrift-0.9.2.tar.gz
 cd thrift-0.9.2
 ./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
 # ./configure --prefix=$HOME/thrift --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
-make && sudo make install
+make -j2 && sudo make install
 cd ..
