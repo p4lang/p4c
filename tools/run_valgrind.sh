14d214536ffb64e4f50975b0008a91a44365de9b
Antonin Bas <antonin@barefootnetworks.com>
2016-02-01 13:35:50 -0800

    added script to run valgrind (on unit tests code) easily

---
+++ tools/run_valgrind.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+TEST_DIR=$THIS_DIR/../tests
+
+cd $TEST_DIR
+
+libtool --mode=execute valgrind --leak-check=full --show-reachable=yes ./test_all
+return_status = $?
+
+cd -
+
+exit $return_status
