7ab20c76e60f9b72a606abd43b7508ef9553e28b
Antonin Bas <antonin@barefootnetworks.com>
2016-02-22 17:35:40 -0800

    fixed implementation of copy_header for simple_switch (made it conformant with P4 spec)

--- targets/simple_switch/primitives.cpp
+++ targets/simple_switch/primitives.cpp
@@ -180,7 +180,10 @@ REGISTER_PRIMITIVE(remove_header);
 
 class copy_header : public ActionPrimitive<Header &, const Header &> {
   void operator ()(Header &dst, const Header &src) {
-    if (!src.is_valid()) return;
+    if (!src.is_valid()) {
+      dst.mark_invalid();
+      return;
+    }
     dst.mark_valid();
     assert(dst.get_header_type_id() == src.get_header_type_id());
     for (unsigned int i = 0; i < dst.size(); i++) {
