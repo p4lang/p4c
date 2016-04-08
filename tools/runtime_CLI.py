p4lang/p4c

commit 16458e1cc6bdfd5c524884be5a5ef41478c0ada5

added ability to query JSON config currently used by the switch

tools/runtime_CLI.py
@@ -27,6 +27,7 @@ import sys
 import struct
 import json
 from functools import wraps
+import md5
 
 from thrift import Thrift
 from thrift.transport import TSocket
@@ -93,7 +94,7 @@ def get_parser():
                         type=str, action="store", default='localhost')
 
     parser.add_argument('--json', help='JSON description of P4 program',
-                        type=str, action="store", required=True)
+                        type=str, action="store", required=False)
 
     parser.add_argument('--pre', help='Packet Replication Engine used by target',
                         type=str, choices=['None', 'SimplePre', 'SimplePreLAG'],
@@ -198,7 +199,14 @@ class RegisterArray:
     def register_str(self):
         return "{0:30} [{1}]".format(self.name, self.size)
 
-def load_json(json_src):
+def reset_config():
+    TABLES.clear()
+    ACTIONS.clear()
+    METER_ARRAYS.clear()
+    COUNTER_ARRAYS.clear()
+    REGISTER_ARRAYS.clear()
+
+def load_json_str(json_str):
     def get_header_type(header_name, j_headers):
         for h in j_headers:
             if h["name"] == header_name:
@@ -213,58 +221,61 @@ def load_json(json_src):
                     return bw
         assert(0)
 
-    with open(json_src, 'r') as f:
-        json_ = json.load(f)
+    reset_config()
+    json_ = json.loads(json_str)
 
-        for j_action in json_["actions"]:
-            action = Action(j_action["name"], j_action["id"])
-            for j_param in j_action["runtime_data"]:
-                action.runtime_data += [(j_param["name"], j_param["bitwidth"])]
+    for j_action in json_["actions"]:
+        action = Action(j_action["name"], j_action["id"])
+        for j_param in j_action["runtime_data"]:
+            action.runtime_data += [(j_param["name"], j_param["bitwidth"])]
 
-        for j_pipeline in json_["pipelines"]:
-            for j_table in j_pipeline["tables"]:
-                table = Table(j_table["name"], j_table["id"])
-                table.match_type = MatchType.from_str(j_table["match_type"])
-                table.type_ = TableType.from_str(j_table["type"])
-                for action in j_table["actions"]:
-                    table.actions[action] = ACTIONS[action]
-                for j_key in j_table["key"]:
-                    target = j_key["target"]
-                    match_type = MatchType.from_str(j_key["match_type"])
-                    if match_type == MatchType.VALID:
-                        field_name = target + "_valid"
-                        bitwidth = 1
-                    else:
-                        field_name = ".".join(target)
-                        header_type = get_header_type(target[0],
-                                                      json_["headers"])
-                        bitwidth = get_field_bitwidth(header_type, target[1],
-                                                      json_["header_types"])
+    for j_pipeline in json_["pipelines"]:
+        for j_table in j_pipeline["tables"]:
+            table = Table(j_table["name"], j_table["id"])
+            table.match_type = MatchType.from_str(j_table["match_type"])
+            table.type_ = TableType.from_str(j_table["type"])
+            for action in j_table["actions"]:
+                table.actions[action] = ACTIONS[action]
+            for j_key in j_table["key"]:
+                target = j_key["target"]
+                match_type = MatchType.from_str(j_key["match_type"])
+                if match_type == MatchType.VALID:
+                    field_name = target + "_valid"
+                    bitwidth = 1
+                else:
+                    field_name = ".".join(target)
+                    header_type = get_header_type(target[0],
+                                                  json_["headers"])
+                    bitwidth = get_field_bitwidth(header_type, target[1],
+                                                  json_["header_types"])
                     table.key += [(field_name, match_type, bitwidth)]
 
-        if "meter_arrays" in json_:
-            for j_meter in json_["meter_arrays"]:
-                meter_array = MeterArray(j_meter["name"], j_meter["id"])
-                meter_array.size = j_meter["size"]
-                meter_array.type_ = MeterType.from_str(j_meter["type"])
-                meter_array.rate_count = j_meter["rate_count"]
+    if "meter_arrays" in json_:
+        for j_meter in json_["meter_arrays"]:
+            meter_array = MeterArray(j_meter["name"], j_meter["id"])
+            meter_array.size = j_meter["size"]
+            meter_array.type_ = MeterType.from_str(j_meter["type"])
+            meter_array.rate_count = j_meter["rate_count"]
 
-        if "counter_arrays" in json_:
-            for j_counter in json_["counter_arrays"]:
-                counter_array = CounterArray(j_counter["name"], j_counter["id"])
-                counter_array.is_direct = j_counter["is_direct"]
-                if counter_array.is_direct:
-                    counter_array.binding = j_counter["binding"]
-                else:
-                    counter_array.size = j_counter["size"]
+    if "counter_arrays" in json_:
+        for j_counter in json_["counter_arrays"]:
+            counter_array = CounterArray(j_counter["name"], j_counter["id"])
+            counter_array.is_direct = j_counter["is_direct"]
+            if counter_array.is_direct:
+                counter_array.binding = j_counter["binding"]
+            else:
+                counter_array.size = j_counter["size"]
 
-        if "register_arrays" in json_:
-            for j_register in json_["register_arrays"]:
-                register_array = RegisterArray(j_register["name"],
-                                               j_register["id"])
-                register_array.size = j_register["size"]
-                register_array.width = j_register["bitwidth"]
+    if "register_arrays" in json_:
+        for j_register in json_["register_arrays"]:
+            register_array = RegisterArray(j_register["name"],
+                                           j_register["id"])
+            register_array.size = j_register["size"]
+            register_array.width = j_register["bitwidth"]
 
+def load_json(json_src):
+    with open(json_src, 'r') as f:
+        load_json_str(f.read())
 
 class UIn_Error(Exception):
     def __init__(self, info=""):
@@ -1345,7 +1356,9 @@ class RuntimeAPI(cmd.Cmd):
     @handle_bad_input
     def do_load_new_config_file(self, line):
         "Load new json config: load_new_config_file <path to .json file>"
-        filename = line
+        args = line.split()
+        self.exactly_n_args(args, 1)
+        filename = args[0]
         if not os.path.isfile(filename):
             raise UIn_Error("Not a valid filename")
         print "Loading new Json config"
@@ -1356,6 +1369,7 @@ class RuntimeAPI(cmd.Cmd):
             except:
                 raise UIn_Error("Not a valid JSON file")
             self.client.bm_load_new_config(json_str)
+        load_json(filename)
 
     @handle_bad_input
     def do_swap_configs(self, line):
@@ -1576,15 +1590,65 @@ class RuntimeAPI(cmd.Cmd):
         self.exactly_n_args(args, 0)
         self.client.bm_reset_state()
 
+    @handle_bad_input
+    def do_write_config_to_file(self, line):
+        "Retrieves the JSON config currently used by the switch and dumps it to user-specified file"
+        args = line.split()
+        self.exactly_n_args(args, 1)
+        filename = args[0]
+        json_cfg = self.client.bm_get_config()
+        with open(filename, 'w') as f:
+            f.write(json_cfg)
+
+def check_JSON_md5(client, json_src):
+    with open(json_src, 'r') as f:
+        m = md5.new()
+        for L in f:
+            m.update(L)
+        md5sum = m.digest()
+
+    try:
+        bm_md5sum = client.bm_get_config_md5()
+    except:
+        print "Error when requesting config md5 sum from switch"
+        sys.exit(1)
+
+    if md5sum != bm_md5sum:
+        print "**********"
+        print "WARNING: the JSON files loaded into the switch and the one you",
+        print "just provided to this CLI don't have the same md5 sum.",
+        print "Are you sure they describe the same program?"
+        bm_md5sum_str = "".join("{:02x}".format(ord(c)) for c in bm_md5sum)
+        md5sum_str = "".join("{:02x}".format(ord(c)) for c in md5sum)
+        print "{:<15}: {}".format("switch md5", bm_md5sum_str)
+        print "{:<15}: {}".format("CLI input md5", md5sum_str)
+        print "**********"
+
+def load_json_config(standard_client = None, json_path = None):
+    if json_path:
+        load_json(json_path)
+        if standard_client is not None:
+            check_JSON_md5(standard_client, json_path)
+    else:
+        assert(standard_client is not None)
+        try:
+            print "Obtaining JSON from switch..."
+            json_cfg = standard_client.bm_get_config()
+            print "Done"
+        except:
+            print "Error when requesting JSON config from switch"
+            sys.exit(1)
+        load_json_str(json_cfg)
+
 def main():
     args = get_parser().parse_args()
-
-    load_json(args.json)
 
     standard_client, mc_client = thrift_connect(
         args.thrift_ip, args.thrift_port,
         RuntimeAPI.get_thrift_services(args.pre)
     )
+
+    load_json_config(standard_client, args.json)
 
     RuntimeAPI(args.pre, standard_client, mc_client).cmdloop()
 

modules/bm_runtime/src/Standard_server.ipp
@@ -644,6 +644,16 @@ public:
     switch_->reset_state();
   }
 
+  void bm_get_config(std::string& _return) {
+    Logger::get()->trace("bm_get_config");
+    _return.append(switch_->get_config());
+  }
+
+  void bm_get_config_md5(std::string& _return) {
+    Logger::get()->trace("bm_get_config_md5");
+    _return.append(switch_->get_config_md5());
+  }
+
 private:
   SwitchWContexts *switch_;
 };

modules/bm_sim/Makefile.am
@@ -34,6 +34,8 @@ src/learning.cpp \
 src/logger.cpp \
 src/match_units.cpp \
 src/match_tables.cpp \
+src/md5.c \
+src/md5.h \
 src/meters.cpp \
 src/options_parse.cpp \
 src/P4Objects.cpp \

modules/bm_sim/include/bm_sim/runtime_interface.h
@@ -237,6 +237,12 @@ class RuntimeInterface {
   virtual ErrorCode
   swap_configs() = 0;
 
+  virtual std::string
+  get_config() = 0;
+
+  virtual std::string
+  get_config_md5() = 0;
+
   virtual ErrorCode
   reset_state() = 0;
 

modules/bm_sim/include/bm_sim/switch.h
@@ -513,6 +513,9 @@ class SwitchWContexts : public DevMgr, public RuntimeInterface {
   RuntimeInterface::ErrorCode
   swap_configs() override;
 
+  std::string get_config() override;
+  std::string get_config_md5() override;
+
   // ---------- End RuntimeInterface ----------
 
  protected:
@@ -577,6 +580,9 @@ class SwitchWContexts : public DevMgr, public RuntimeInterface {
   std::shared_ptr<TransportIface> notifications_transport{nullptr};
 
   mutable boost::shared_mutex ongoing_swap_mutex{};
+
+  std::string current_config{};
+  mutable std::mutex config_mutex{};
 };
 
 

modules/bm_sim/src/CPPLINT.cfg
@@ -1,1 +1,2 @@
 exclude_files=xxhash\.h
+exclude_files=md5\.h

modules/bm_sim/src/md5.c
@@ -1,0 +1,296 @@
+/*
+ * This is an OpenSSL-compatible implementation of the RSA Data Security, Inc.
+ * MD5 Message-Digest Algorithm (RFC 1321).
+ *
+ * Homepage:
+ * http://openwall.info/wiki/people/solar/software/public-domain-source-code/md5
+ *
+ * Author:
+ * Alexander Peslyak, better known as Solar Designer <solar at openwall.com>
+ *
+ * This software was written by Alexander Peslyak in 2001.  No copyright is
+ * claimed, and the software is hereby placed in the public domain.
+ * In case this attempt to disclaim copyright and place the software in the
+ * public domain is deemed null and void, then the software is
+ * Copyright (c) 2001 Alexander Peslyak and it is hereby released to the
+ * general public under the following terms:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ *
+ * (This is a heavily cut-down "BSD license".)
+ *
+ * This differs from Colin Plumb's older public domain implementation in that
+ * no exactly 32-bit integer data type is required (any 32-bit or wider
+ * unsigned integer data type will do), there's no compile-time endianness
+ * configuration, and the function prototypes match OpenSSL's.  No code from
+ * Colin Plumb's implementation has been reused; this comment merely compares
+ * the properties of the two independent implementations.
+ *
+ * The primary goals of this implementation are portability and ease of use.
+ * It is meant to be fast, but not as fast as possible.  Some known
+ * optimizations are not included to reduce source code size and avoid
+ * compile-time configuration.
+ */
+
+#ifndef HAVE_OPENSSL
+
+#include <string.h>
+
+#include "md5.h"
+
+/*
+ * The basic MD5 functions.
+ *
+ * F and G are optimized compared to their RFC 1321 definitions for
+ * architectures that lack an AND-NOT instruction, just like in Colin Plumb's
+ * implementation.
+ */
+#define F(x, y, z)			((z) ^ ((x) & ((y) ^ (z))))
+#define G(x, y, z)			((y) ^ ((z) & ((x) ^ (y))))
+#define H(x, y, z)			(((x) ^ (y)) ^ (z))
+#define H2(x, y, z)			((x) ^ ((y) ^ (z)))
+#define I(x, y, z)			((y) ^ ((x) | ~(z)))
+
+/*
+ * The MD5 transformation for all four rounds.
+ */
+#define STEP(f, a, b, c, d, x, t, s) \
+	(a) += f((b), (c), (d)) + (x) + (t); \
+	(a) = (((a) << (s)) | (((a) & 0xffffffff) >> (32 - (s)))); \
+	(a) += (b);
+
+/*
+ * SET reads 4 input bytes in little-endian byte order and stores them
+ * in a properly aligned word in host byte order.
+ *
+ * The check for little-endian architectures that tolerate unaligned
+ * memory accesses is just an optimization.  Nothing will break if it
+ * doesn't work.
+ */
+#if defined(__i386__) || defined(__x86_64__) || defined(__vax__)
+#define SET(n) \
+	(*(MD5_u32plus *)&ptr[(n) * 4])
+#define GET(n) \
+	SET(n)
+#else
+#define SET(n) \
+	(ctx->block[(n)] = \
+	(MD5_u32plus)ptr[(n) * 4] | \
+	((MD5_u32plus)ptr[(n) * 4 + 1] << 8) | \
+	((MD5_u32plus)ptr[(n) * 4 + 2] << 16) | \
+	((MD5_u32plus)ptr[(n) * 4 + 3] << 24))
+#define GET(n) \
+	(ctx->block[(n)])
+#endif
+
+/*
+ * This processes one or more 64-byte data blocks, but does NOT update
+ * the bit counters.  There are no alignment requirements.
+ */
+static const void *body(MD5_CTX *ctx, const void *data, unsigned long size)
+{
+	const unsigned char *ptr;
+	MD5_u32plus a, b, c, d;
+	MD5_u32plus saved_a, saved_b, saved_c, saved_d;
+
+	ptr = (const unsigned char *)data;
+
+	a = ctx->a;
+	b = ctx->b;
+	c = ctx->c;
+	d = ctx->d;
+
+	do {
+		saved_a = a;
+		saved_b = b;
+		saved_c = c;
+		saved_d = d;
+
+/* Round 1 */
+		STEP(F, a, b, c, d, SET(0), 0xd76aa478, 7)
+		STEP(F, d, a, b, c, SET(1), 0xe8c7b756, 12)
+		STEP(F, c, d, a, b, SET(2), 0x242070db, 17)
+		STEP(F, b, c, d, a, SET(3), 0xc1bdceee, 22)
+		STEP(F, a, b, c, d, SET(4), 0xf57c0faf, 7)
+		STEP(F, d, a, b, c, SET(5), 0x4787c62a, 12)
+		STEP(F, c, d, a, b, SET(6), 0xa8304613, 17)
+		STEP(F, b, c, d, a, SET(7), 0xfd469501, 22)
+		STEP(F, a, b, c, d, SET(8), 0x698098d8, 7)
+		STEP(F, d, a, b, c, SET(9), 0x8b44f7af, 12)
+		STEP(F, c, d, a, b, SET(10), 0xffff5bb1, 17)
+		STEP(F, b, c, d, a, SET(11), 0x895cd7be, 22)
+		STEP(F, a, b, c, d, SET(12), 0x6b901122, 7)
+		STEP(F, d, a, b, c, SET(13), 0xfd987193, 12)
+		STEP(F, c, d, a, b, SET(14), 0xa679438e, 17)
+		STEP(F, b, c, d, a, SET(15), 0x49b40821, 22)
+
+/* Round 2 */
+		STEP(G, a, b, c, d, GET(1), 0xf61e2562, 5)
+		STEP(G, d, a, b, c, GET(6), 0xc040b340, 9)
+		STEP(G, c, d, a, b, GET(11), 0x265e5a51, 14)
+		STEP(G, b, c, d, a, GET(0), 0xe9b6c7aa, 20)
+		STEP(G, a, b, c, d, GET(5), 0xd62f105d, 5)
+		STEP(G, d, a, b, c, GET(10), 0x02441453, 9)
+		STEP(G, c, d, a, b, GET(15), 0xd8a1e681, 14)
+		STEP(G, b, c, d, a, GET(4), 0xe7d3fbc8, 20)
+		STEP(G, a, b, c, d, GET(9), 0x21e1cde6, 5)
+		STEP(G, d, a, b, c, GET(14), 0xc33707d6, 9)
+		STEP(G, c, d, a, b, GET(3), 0xf4d50d87, 14)
+		STEP(G, b, c, d, a, GET(8), 0x455a14ed, 20)
+		STEP(G, a, b, c, d, GET(13), 0xa9e3e905, 5)
+		STEP(G, d, a, b, c, GET(2), 0xfcefa3f8, 9)
+		STEP(G, c, d, a, b, GET(7), 0x676f02d9, 14)
+		STEP(G, b, c, d, a, GET(12), 0x8d2a4c8a, 20)
+
+/* Round 3 */
+		STEP(H, a, b, c, d, GET(5), 0xfffa3942, 4)
+		STEP(H2, d, a, b, c, GET(8), 0x8771f681, 11)
+		STEP(H, c, d, a, b, GET(11), 0x6d9d6122, 16)
+		STEP(H2, b, c, d, a, GET(14), 0xfde5380c, 23)
+		STEP(H, a, b, c, d, GET(1), 0xa4beea44, 4)
+		STEP(H2, d, a, b, c, GET(4), 0x4bdecfa9, 11)
+		STEP(H, c, d, a, b, GET(7), 0xf6bb4b60, 16)
+		STEP(H2, b, c, d, a, GET(10), 0xbebfbc70, 23)
+		STEP(H, a, b, c, d, GET(13), 0x289b7ec6, 4)
+		STEP(H2, d, a, b, c, GET(0), 0xeaa127fa, 11)
+		STEP(H, c, d, a, b, GET(3), 0xd4ef3085, 16)
+		STEP(H2, b, c, d, a, GET(6), 0x04881d05, 23)
+		STEP(H, a, b, c, d, GET(9), 0xd9d4d039, 4)
+		STEP(H2, d, a, b, c, GET(12), 0xe6db99e5, 11)
+		STEP(H, c, d, a, b, GET(15), 0x1fa27cf8, 16)
+		STEP(H2, b, c, d, a, GET(2), 0xc4ac5665, 23)
+
+/* Round 4 */
+		STEP(I, a, b, c, d, GET(0), 0xf4292244, 6)
+		STEP(I, d, a, b, c, GET(7), 0x432aff97, 10)
+		STEP(I, c, d, a, b, GET(14), 0xab9423a7, 15)
+		STEP(I, b, c, d, a, GET(5), 0xfc93a039, 21)
+		STEP(I, a, b, c, d, GET(12), 0x655b59c3, 6)
+		STEP(I, d, a, b, c, GET(3), 0x8f0ccc92, 10)
+		STEP(I, c, d, a, b, GET(10), 0xffeff47d, 15)
+		STEP(I, b, c, d, a, GET(1), 0x85845dd1, 21)
+		STEP(I, a, b, c, d, GET(8), 0x6fa87e4f, 6)
+		STEP(I, d, a, b, c, GET(15), 0xfe2ce6e0, 10)
+		STEP(I, c, d, a, b, GET(6), 0xa3014314, 15)
+		STEP(I, b, c, d, a, GET(13), 0x4e0811a1, 21)
+		STEP(I, a, b, c, d, GET(4), 0xf7537e82, 6)
+		STEP(I, d, a, b, c, GET(11), 0xbd3af235, 10)
+		STEP(I, c, d, a, b, GET(2), 0x2ad7d2bb, 15)
+		STEP(I, b, c, d, a, GET(9), 0xeb86d391, 21)
+
+		a += saved_a;
+		b += saved_b;
+		c += saved_c;
+		d += saved_d;
+
+		ptr += 64;
+	} while (size -= 64);
+
+	ctx->a = a;
+	ctx->b = b;
+	ctx->c = c;
+	ctx->d = d;
+
+	return ptr;
+}
+
+void MD5_Init(MD5_CTX *ctx)
+{
+	ctx->a = 0x67452301;
+	ctx->b = 0xefcdab89;
+	ctx->c = 0x98badcfe;
+	ctx->d = 0x10325476;
+
+	ctx->lo = 0;
+	ctx->hi = 0;
+}
+
+void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size)
+{
+	MD5_u32plus saved_lo;
+	unsigned long used, available;
+
+	saved_lo = ctx->lo;
+	if ((ctx->lo = (saved_lo + size) & 0x1fffffff) < saved_lo)
+		ctx->hi++;
+	ctx->hi += size >> 29;
+
+	used = saved_lo & 0x3f;
+
+	if (used) {
+		available = 64 - used;
+
+		if (size < available) {
+			memcpy(&ctx->buffer[used], data, size);
+			return;
+		}
+
+		memcpy(&ctx->buffer[used], data, available);
+		data = (const unsigned char *)data + available;
+		size -= available;
+		body(ctx, ctx->buffer, 64);
+	}
+
+	if (size >= 64) {
+		data = body(ctx, data, size & ~(unsigned long)0x3f);
+		size &= 0x3f;
+	}
+
+	memcpy(ctx->buffer, data, size);
+}
+
+void MD5_Final(unsigned char *result, MD5_CTX *ctx)
+{
+	unsigned long used, available;
+
+	used = ctx->lo & 0x3f;
+
+	ctx->buffer[used++] = 0x80;
+
+	available = 64 - used;
+
+	if (available < 8) {
+		memset(&ctx->buffer[used], 0, available);
+		body(ctx, ctx->buffer, 64);
+		used = 0;
+		available = 64;
+	}
+
+	memset(&ctx->buffer[used], 0, available - 8);
+
+	ctx->lo <<= 3;
+	ctx->buffer[56] = ctx->lo;
+	ctx->buffer[57] = ctx->lo >> 8;
+	ctx->buffer[58] = ctx->lo >> 16;
+	ctx->buffer[59] = ctx->lo >> 24;
+	ctx->buffer[60] = ctx->hi;
+	ctx->buffer[61] = ctx->hi >> 8;
+	ctx->buffer[62] = ctx->hi >> 16;
+	ctx->buffer[63] = ctx->hi >> 24;
+
+	body(ctx, ctx->buffer, 64);
+
+	result[0] = ctx->a;
+	result[1] = ctx->a >> 8;
+	result[2] = ctx->a >> 16;
+	result[3] = ctx->a >> 24;
+	result[4] = ctx->b;
+	result[5] = ctx->b >> 8;
+	result[6] = ctx->b >> 16;
+	result[7] = ctx->b >> 24;
+	result[8] = ctx->c;
+	result[9] = ctx->c >> 8;
+	result[10] = ctx->c >> 16;
+	result[11] = ctx->c >> 24;
+	result[12] = ctx->d;
+	result[13] = ctx->d >> 8;
+	result[14] = ctx->d >> 16;
+	result[15] = ctx->d >> 24;
+
+	memset(ctx, 0, sizeof(*ctx));
+}
+
+#endif

modules/bm_sim/src/md5.h
@@ -1,0 +1,53 @@
+/*
+ * This is an OpenSSL-compatible implementation of the RSA Data Security, Inc.
+ * MD5 Message-Digest Algorithm (RFC 1321).
+ *
+ * Homepage:
+ * http://openwall.info/wiki/people/solar/software/public-domain-source-code/md5
+ *
+ * Author:
+ * Alexander Peslyak, better known as Solar Designer <solar at openwall.com>
+ *
+ * This software was written by Alexander Peslyak in 2001.  No copyright is
+ * claimed, and the software is hereby placed in the public domain.
+ * In case this attempt to disclaim copyright and place the software in the
+ * public domain is deemed null and void, then the software is
+ * Copyright (c) 2001 Alexander Peslyak and it is hereby released to the
+ * general public under the following terms:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ *
+ * See md5.c for more information.
+ */
+
+#ifdef HAVE_OPENSSL
+#include <openssl/md5.h>
+#elif !defined(_MD5_H)
+#define _MD5_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Any 32-bit or wider unsigned integer data type will do */
+typedef unsigned int MD5_u32plus;
+
+typedef struct {
+	MD5_u32plus lo, hi;
+	MD5_u32plus a, b, c, d;
+	unsigned char buffer[64];
+	MD5_u32plus block[16];
+} MD5_CTX;
+
+extern void MD5_Init(MD5_CTX *ctx);
+extern void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size);
+extern void MD5_Final(unsigned char *result, MD5_CTX *ctx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

commit 19c50934ac0c56a4025d00573a64854da0b6d9ee

fixed identation issue in Python introduced by commit 16458e1

tools/runtime_CLI.py
@@ -248,7 +248,7 @@ def load_json_str(json_str):
                                                   json_["headers"])
                     bitwidth = get_field_bitwidth(header_type, target[1],
                                                   json_["header_types"])
-                    table.key += [(field_name, match_type, bitwidth)]
+                table.key += [(field_name, match_type, bitwidth)]
 
     if "meter_arrays" in json_:
         for j_meter in json_["meter_arrays"]: