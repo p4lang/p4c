bfd94c59ed9dbe25ea65cc52efbc81ab3dfa60fc
Mihai Budiu <mbudiu@barefootnetworks.com>
2016-04-14 08:38:04 -0700

    pass bootstrap.sh arguments to configure

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ cd build
 # TODO: the "prefix" is needed for finding the p4include folder.
 # It should be an absolute path.  This may need to change
 # when we have a proper installation procedure.
-../configure CXXFLAGS="-g -O0" --prefix=`pwd`/..
+../configure CXXFLAGS="-g -O0" --prefix=`pwd`/.. $*
 echo "### Configured for building in 'build' folder"
