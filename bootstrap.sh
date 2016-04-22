becca6c4807b5988076069cf608d56d6c0b4074c
Mihai Budiu <mbudiu@barefootnetworks.com>
2016-04-22 15:14:29 -0700

    configure detection of clang; fail on errors in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,16 @@
-./find-makefiles.sh
-mkdir -p extensions
+#!/bin/sh
+# This command bootstraps the autoconf-based configuratino
+# for the P4c compiler
+
+set -e  # exit on error
+./find-makefiles.sh # creates otherMakefiles.am, included in Makefile.am
+mkdir -p extensions # place where additional back-ends are expected
 echo "Running autoconf/configure tools"
 autoreconf -i
-mkdir -p build
+mkdir -p build # recommended folder for build
 cd build
 # TODO: the "prefix" is needed for finding the p4include folder.
 # It should be an absolute path.  This may need to change
 # when we have a proper installation procedure.
-../configure CXXFLAGS="-g -O0" --prefix=`pwd`/.. $*
+../configure CXXFLAGS="-g -O0" --prefix=`pwd`/.. $* 
 echo "### Configured for building in 'build' folder"
