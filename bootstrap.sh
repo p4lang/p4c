bcfffd3055a24f07ecc56abc0533d5fa8d734486
Chris Dodd <chris@barefootnetworks.com>
2016-05-18 14:59:16 -0700

    change default config (in bootstrap.sh) to -O1 instead of -O0

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,10 @@ mkdir -p extensions # place where additional back-ends are expected
 echo "Running autoconf/configure tools"
 autoreconf -i
 mkdir -p build # recommended folder for build
+sourcedir=`pwd`
 cd build
 # TODO: the "prefix" is needed for finding the p4include folder.
 # It should be an absolute path.  This may need to change
 # when we have a proper installation procedure.
-../configure CXXFLAGS="-g -O0" --prefix=`pwd`/.. $* 
+../configure CXXFLAGS="-g -O1" --prefix=$sourcedir $*
 echo "### Configured for building in 'build' folder"
