20a2a6f4a1d52da1a04b5d20630358a24ad3e9b1
Seth Fowler <seth@blackhail.net>
2017-01-23 18:48:37 -0800

    Install the thrift python package when building thrift

--- travis/install-thrift.sh
+++ travis/install-thrift.sh
@@ -11,4 +11,6 @@ tar -xzvf thrift-0.9.2.tar.gz
 cd thrift-0.9.2
 ./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
 make -j2 && sudo make install
-cd ..
+cd lib/py
+sudo python setup.py install
+cd ../../..
