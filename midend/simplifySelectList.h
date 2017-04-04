67888dd5b308b6027937beeec233a9144edb9f69
Tom Everman <teverman@google.com>
2017-04-04 07:30:30 -0700

    Add missing includes exposed by non-unified build

--- midend/simplifySelectList.h
+++ midend/simplifySelectList.h
@@ -17,6 +17,11 @@ limitations under the License.
 #ifndef _MIDEND_SIMPLIFYSELECTLIST_H_
 #define _MIDEND_SIMPLIFYSELECTLIST_H_
 
+#include "frontends/common/resolveReferences/referenceMap.h"
+#include "frontends/p4/typeChecking/typeChecker.h"
+#include "frontends/p4/typeMap.h"
+#include "ir/ir.h"
+
 namespace P4 {
 
 // Replace a reference to a structure with a list of all its fields
