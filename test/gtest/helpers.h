9ddcd8d297b491c795523d343b4dcf28de498f86
Seth Fowler <seth@blackhail.net>
2017-04-10 15:43:25 -0700

    Map errors in P4 programs in GTests to the right source line

--- test/gtest/helpers.h
+++ test/gtest/helpers.h
@@ -14,12 +14,29 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#ifndef _TEST_GTEST_HELPER_
-#define _TEST_GTEST_HELPER_
+#ifndef TEST_GTEST_HELPERS_H_
+#define TEST_GTEST_HELPERS_H_
 
 #include <string>
 
-/* preprocessing by prepending the content of core.p4 to test program */
-std::string with_core_p4 (const std::string& pgm);
+namespace detail {
 
-#endif
+/**
+ * Creates a "nice" version of the P4 program in @rawSource which contains
+ * additional information for the preprocessor to aid in debugging. @file and
+ * @line should be the __FILE__ and __LINE__, respectively, at which @rawSource
+ * is defined.
+ */
+std::string makeP4Source(const char* file, unsigned line, const char* rawSource);
+
+}  // namespace detail
+
+// A macro which should be used by unit tests to define P4 source code. It adds
+// additional information to the source code to aid in debugging; see
+// makeP4Source for more information.
+#define P4_SOURCE(SRC) detail::makeP4Source(__FILE__, __LINE__, SRC)
+
+/* preprocessing by prepending the content of core.p4 to test program */
+std::string with_core_p4(const std::string& pgm);
+
+#endif /* TEST_GTEST_HELPERS_H_ */
