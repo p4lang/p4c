2521d8b487f5a6905549b671467299358fb336ff
Mihai Budiu <mbudiu@vmware.com>
2017-05-04 10:40:24 -0700

    Fix for issue #539 (#557)

--- frontends/p4/tableKeyNames.h
+++ frontends/p4/tableKeyNames.h
@@ -23,13 +23,39 @@ limitations under the License.
 
 namespace P4 {
 
+/** Generate control plane names for simple expressions that appear in table
+ * keys without `@name` annotations.
+ *
+ * Emit a compilation error if the IR contains complex expressions without
+ * `@name` annotations.
+ */
+class KeyNameGenerator : public Inspector {
+    std::map<const IR::Expression*, cstring> name;
+    const TypeMap* typeMap;  // can be nullptr
+
+ public:
+    explicit KeyNameGenerator(const TypeMap* typeMap) : typeMap(typeMap)
+    { setName("KeyNameGenerator"); }
+    void error(const IR::Expression* expression);
+    void postorder(const IR::Expression* expression) override
+    { error(expression); }
+    void postorder(const IR::PathExpression* expression) override;
+    void postorder(const IR::Member* expression) override;
+    void postorder(const IR::ArrayIndex* expression) override;
+    void postorder(const IR::Constant* expression) override;
+    void postorder(const IR::Slice* expression) override;
+    void postorder(const IR::MethodCallExpression* expression) override;
+    cstring getName(const IR::Expression* expression) {
+        return ::get(name, expression);
+    }
+};
+
 /** Adds a "@name" annotation to each table key that does not have a name.
  * The string used for the name is derived from the expression itself - if
  * the expression is "simple" enough.  If the expression is not simple the
  * compiler will give an error.  Simple expressions are:
  * - .isValid(),
  * - ArrayIndex,
- * - BAnd (where at least one operand is constant),
  * - Constant,
  * - Member,
  * - PathExpression,
@@ -37,7 +63,6 @@ namespace P4 {
  *
  * Examples of control plane names generated from expressions:
  * - `arr[16w5].f` : `@name("arr[5].f")`
- * - `f & 0x3` : `@name("f")`
  * - `.foo` : `@name(".foo")`
  * - `foo.bar` : `@name("foo.bar")`
  * - `f.isValid()` : `@name("f.isValid()")`
@@ -46,13 +71,13 @@ namespace P4 {
  * @pre This must run before passes that change key expressions, eg. constant
  * folding.  Otherwise the generated control plane names may not match the
  * syntax of the original P4 program.
- * 
+ *
  * @post All key fields have `@name` annotations.
  *
  * Emit a compilation error if the program contains complex key expressions
  * without `@name` annotations.
  */
-class DoTableKeyNames : public Transform {
+class DoTableKeyNames final : public Transform {
     const TypeMap* typeMap;
  public:
     explicit DoTableKeyNames(const TypeMap* typeMap) :typeMap(typeMap)
@@ -60,7 +85,7 @@ class DoTableKeyNames : public Transform {
     const IR::Node* postorder(IR::KeyElement* keyElement) override;
 };
 
-class TableKeyNames : public PassManager {
+class TableKeyNames final : public PassManager {
  public:
     TableKeyNames(ReferenceMap* refMap, TypeMap* typeMap) {
         passes.push_back(new TypeChecking(refMap, typeMap));
