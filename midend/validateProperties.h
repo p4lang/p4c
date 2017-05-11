5f59a6d4c71a8b1a67fa1db1aced59115c780cd7
Han Wang <han.wang@barefootnetworks.com>
2017-05-11 07:40:16 -0700

    doxygen comment for validateProperties

--- midend/validateProperties.h
+++ midend/validateProperties.h
@@ -22,7 +22,9 @@ limitations under the License.
 
 namespace P4 {
 
-// Checks to see if there are any unknown properties.
+/**
+ * Checks to see if there are any unknown properties.
+ */
 class ValidateTableProperties : public Inspector {
     std::set<cstring> legalProperties;
  public:
@@ -36,7 +38,7 @@ class ValidateTableProperties : public Inspector {
             legalProperties.emplace(l);
     }
     void postorder(const IR::Property* property) override;
-    /* don't check properties in externs (Declaration_Instances) */
+    /// don't check properties in externs (Declaration_Instances)
     bool preorder(const IR::Declaration_Instance *) override { return false; }
 };
 
