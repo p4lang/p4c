d79673594490ddd9b19954a1c5e4a047a23c6fff
mbudiu-vmw <mbudiu@vmware.com>
2017-05-23 14:41:16 -0700

    Update comment

--- frontends/p4/validateParsedProgram.h
+++ frontends/p4/validateParsedProgram.h
@@ -35,7 +35,7 @@ namespace P4 {
    - width of int<> types is larger than 1
    - no parser state is named 'accept' or 'reject'
    - constructor parameters are direction-less
-   - tables have an actions and a default_action properties
+   - tables have an actions property
    - instantiations appear at the top-level only
    - switch statements do not occur in actions
    - instantiations do not occur in actions
