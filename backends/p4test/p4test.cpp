7c8ab17fb799e0ed93f9296c204450fea8afc28b
Seth Fowler <seth@blackhail.net>
2017-06-23 13:34:29 -0700

    Move unnecessary inclusions and code out of json_parser.h

--- backends/p4test/p4test.cpp
+++ backends/p4test/p4test.cpp
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <fstream>
+#include <iostream>
+
 #include "ir/ir.h"
 #include "ir/json_loader.h"
 #include "lib/log.h"
