6a9b6b96e17d09ef9f5c57b550368d854ca17620
Calin Cascaval <cascaval@barefootnetworks.com>
2017-06-29 22:15:32 -0700

    irgenerator

---
+++ cmake/config.h.cmake
@@ -0,0 +1,14 @@
+/* Define to 1 if you have the execinfo.h header */
+#cmakedefine HAVE_EXECINFO_H 1
+
+/* Define to 1 if you have the LIBGC library. */
+#cmakedefine HAVE_LIBGC 1
+
+/* Define to 1 if you have the memchr function. */
+#cmakedefine HAVE_MEMCHR 1
+
+/* Define to 1 if you have the pipe2 function. */
+#cmakedefine HAVE_PIPE2 1
+
+/* Define to 1 if you have the ucontext.h header */
+#cmakedefine HAVE_UCONTEXT_H 1
