e12231ce40466ab570fc4769bac354622850a9d3
Calin Cascaval <cascaval@barefootnetworks.com>
2017-07-04 18:48:30 -0700

    add extra paths to search for simple_switch

--- cmake/FindBMV2.cmake
+++ cmake/FindBMV2.cmake
@@ -1,10 +1,15 @@
 
+set(BMV2_SEARCH_PATHS
+  ${CMAKE_INSTALL_PREFIX}/bin
+  ${P4C_SOURCE_DIR}/../behavioral-model/build/targets/simple_switch
+  ${P4C_SOURCE_DIR}/../../behavioral-model/build/targets/simple_switch)
+
 # check for simple_switch
 find_program (SIMPLE_SWITCH_CLI simple_switch_CLI
-  PATHS ${CMAKE_INSTALL_PREFIX}/bin )
+  PATHS ${BMV2_SEARCH_PATHS} )
 if (SIMPLE_SWITCH_CLI)
   find_program (SIMPLE_SWITCH simple_switch
-    PATHS ${CMAKE_INSTALL_PREFIX}/bin )
+    PATHS ${BMV2_SEARCH_PATHS} )
   if (SIMPLE_SWITCH)
     set (HAVE_SIMPLE_SWITCH 1)
     find_python_module (scapy REQUIRED)
@@ -15,5 +20,5 @@ endif(SIMPLE_SWITCH_CLI)
 mark_as_advanced(SIMPLE_SWITCH SIMPLE_SWITCH_CLI)
 
 find_package_handle_standard_args ("BMv2 programs"
-  "Program 'simple_switch_CLI' (https://github.com/p4lang/behavioral-model.git) not found; will not run BMv2 tests."
+  "Program 'simple_switch_CLI' (https://github.com/p4lang/behavioral-model.git) not found;\nSearched ${BMV2_SEARCH_PATHS}.\nWill not run BMv2 tests."
   SIMPLE_SWITCH SIMPLE_SWITCH_CLI)
