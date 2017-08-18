9efc595857b18fdf68ba93fb3dc772f5c5dfe6c3
Han Wang <hanw@users.noreply.github.com>
2017-08-17 21:03:38 -0700

    allow counter and meter enum names to be all capitalized (#856)

--- ir/ir.h
+++ ir/ir.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2013-present Barefoot Networks, Inc. 
+Copyright 2013-present Barefoot Networks, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ limitations under the License.
 #ifndef _IR_IR_H_
 #define _IR_IR_H_
 
+#include <strings.h>
 #include <cassert>
 #include <sstream>
 
