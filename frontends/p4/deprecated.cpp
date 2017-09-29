9190034173f4bfe6ecaa81a7515b3b1b6bb1d3ca
Calin Cascaval <cascaval@barefootnetworks.com>
2017-09-29 10:43:22 -0700

    fix missing includes and namespace qualifier for non-unified build

--- frontends/p4/deprecated.cpp
+++ frontends/p4/deprecated.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include "frontends/common/resolveReferences/resolveReferences.h"
 #include "deprecated.h"
 
 namespace P4 {
