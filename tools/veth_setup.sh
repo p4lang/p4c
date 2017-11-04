e1fa4347f1915c1d6852f65eabca730c2cc2e368
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-11-04 10:13:21 -0700

    Enhance veth_setup.sh to disable Linux kernel sending IPv6 pkts, enable jumbo frames

--- tools/veth_setup.sh
+++ tools/veth_setup.sh
@@ -6,5 +6,24 @@ for idx in 0 1 2 3 4 5 6 7 8; do
         ip link add name $intf0 type veth peer name $intf1
         ip link set dev $intf0 up
         ip link set dev $intf1 up
+
+        # Set the MTU of these interfaces to be larger than default of
+        # 1500 bytes, so that P4 behavioral-model testing can be done
+        # on jumbo frames.
+        ifconfig $intf0 mtu 9500 up
+        ifconfig $intf1 mtu 9500 up
+
+        # Disable IPv6 on the interfaces, so that the Linux kernel
+        # will not automatically send IPv6 MDNS, Router Solicitation,
+        # and Multicast Listener Report packets on the interface,
+        # which can make P4 program debugging more confusing.
+        #
+        # Testing indicates that we can still send IPv6 packets across
+        # such interfaces, both from scapy to simple_switch, and from
+        # simple_switch out to scapy sniffing.
+        #
+        # https://superuser.com/questions/356286/how-can-i-switch-off-ipv6-nd-ra-transmissions-in-linux
+        sysctl net.ipv6.conf.${intf0}.disable_ipv6=1
+        sysctl net.ipv6.conf.${intf1}.disable_ipv6=1
     fi
 done
