9033f66e8ac594d78c6029d556d6ba337b22aa69
Han Wang <hanwang@barefootnetworks.com>
2018-01-17 21:10:04 -0800

    remove strength reduction pass from frontend

--- frontends/p4/frontend.cpp
+++ frontends/p4/frontend.cpp
@@ -140,7 +140,6 @@ const IR::P4Program *FrontEnd::run(const CompilerOptions &options, const IR::P4P
         new ClearTypeMap(&typeMap),
         new TableKeyNames(&refMap, &typeMap),
         new ConstantFolding(&refMap, &typeMap),
-        new StrengthReduction(),
         new UselessCasts(&refMap, &typeMap),
         new SimplifyControlFlow(&refMap, &typeMap),
         new FrontEndDump(),  // used for testing the program at this point
