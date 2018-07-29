3006b72dd21a49955bf9814fc625cdc18b4f0c31
Calin Cascaval <cascaval@barefootnetworks.com>
2018-07-29 15:47:09 -0700

    cpplint

--- frontends/p4/fromv1.0/programStructure.cpp
+++ frontends/p4/fromv1.0/programStructure.cpp
@@ -564,7 +564,7 @@ void ProgramStructure::include(cstring filename, cstring ppoptions) {
         options.preprocessor_options += ppoptions; }
     options.langVersion = CompilerOptions::FrontendVersion::P4_16;
     options.file = path.toString();
-    if (!::errorCount())
+    if (!::errorCount()) {
         if (FILE* file = options.preprocess()) {
             auto code = P4::P4ParserDriver::parse(file, options.file);
             if (code && !::errorCount())
@@ -572,6 +572,7 @@ void ProgramStructure::include(cstring filename, cstring ppoptions) {
                     declarations->push_back(decl);
             options.closeInput(file);
         }
+    }
 }
 
 void ProgramStructure::loadModel() {
