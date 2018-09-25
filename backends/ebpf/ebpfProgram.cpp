c09db2ffa12fb77b7d3dd806b52d20168ef80114
Mihai Budiu <mbudiu@vmware.com>
2018-09-25 10:06:20 -0700

    Fix for issue #1508 (#1509)

--- backends/ebpf/ebpfProgram.cpp
+++ backends/ebpf/ebpfProgram.cpp
@@ -29,6 +29,10 @@ namespace EBPF {
 
 bool EBPFProgram::build() {
     auto pack = toplevel->getMain();
+    if (pack->type->name != "ebpfFilter")
+        ::warning("%1%: the main ebpf package should be called ebpfFilter"
+                  "; are you using the wrong architecture?", pack->type->name);
+
     if (pack->getConstructorParameters()->size() != 2) {
         ::error("Expected toplevel package %1% to have 2 parameters", pack->type);
         return false;
