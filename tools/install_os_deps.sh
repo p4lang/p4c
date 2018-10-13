5a3f6dea995e4156cbfff3e9d92116264d5b1573
Calin Cascaval <cascaval@acm.org>
2018-10-13 14:41:02 -0700

    add python dependencies

--- tools/install_os_deps.sh
+++ tools/install_os_deps.sh
@@ -11,4 +11,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     $BREW install autoconf automake bdw-gc bison boost ccache cmake git libtool openssl pkg-config protobuf
     $BREW link --force bison
     $BREW install gmp --c++11
+
+    # install pip and required pip packages
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --user
+    pip install --user scapy ply
 fi
