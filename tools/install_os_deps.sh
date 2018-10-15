faa61167e85beb230ce9bc2006df32424adea92b
Nate Foster <jnfoster@cs.cornell.edu>
2018-10-14 21:45:13 -0400

    Write PATH to .bash_profile and add forced link for bison

--- tools/install_os_deps.sh
+++ tools/install_os_deps.sh
@@ -6,12 +6,14 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     if [[ ! -x $BREW ]]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
-    # Update PATH to prefer Homebrew's bison over the macOS-provided version
-    export PATH="/usr/local/opt/bison/bin:$PATH"
 
     $BREW update
     $BREW install autoconf automake bdw-gc bison boost ccache cmake git libtool openssl pkg-config protobuf
     $BREW install gmp --c++11
+
+    # Prefer Homebrew's bison over the macOS-provided version
+    $BREW link --force bison
+    echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' > ~/.bash_profile
 
     # install pip and required pip packages
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
