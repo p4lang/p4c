85c50836ca8dc9a9175d12a20a5a508889c7cf7a
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-10-21 12:51:29 +0200

    Include missing header

--- frontends/p4/defaultArguments.h
+++ frontends/p4/defaultArguments.h
@@ -19,6 +19,7 @@ limitations under the License.
 
 #include "ir/ir.h"
 #include "frontends/common/resolveReferences/referenceMap.h"
+#include "frontends/p4/typeChecking/typeChecker.h"
 #include "frontends/p4/typeMap.h"
 
 namespace P4 {
