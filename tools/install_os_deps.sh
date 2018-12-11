eb9ac7dfe05796e235f7ccf7958f1242c26f3796
Jed Liu <jliu@barefootnetworks.com>
2018-12-11 00:13:49 -0500

    Fix Travis OS X.
    Modifying `.bash_profile` isn't good enough for Travis. Have to set
    environment variables in `.travis.yml`.

--- tools/install_os_deps.sh
+++ tools/install_os_deps.sh
@@ -1,47 +1,6 @@
 #! /bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' >> ~/.bash_profile
-    echo 'echo "===== RAN BASH_PROFILE ====="' >> ~/.bash_profile
-    echo 'echo "===== RAN BASH_PROFILE =====" >&2' >> ~/.bash_profile
-    echo 'echo "===== RAN BASH_PROFILE ===== >> /tmp/log"' >> ~/.bash_profile
-    echo 'echo "===== RAN BASHRC ====="' >> ~/.bashrc
-    echo 'echo "===== RAN BASHRC =====" >&2' >> ~/.bashrc
-    echo 'echo "===== RAN BASHRC =====" >> /tmp/log' >> ~/.bashrc
-
-    echo 'env ========================================='
-
-    env | sort
-
-    echo 'profile ======================================'
-
-    cat ~/.bash_profile
-
-    echo 'rc ==========================================='
-
-    cat ~/.bashrc
-
-    echo 'job_stages ==================================='
-
-    cat /Users/travis/.travis/job_stages
-
-    echo 'moo =========================================='
-
-    cat > /tmp/moo <<EOF
-#!/bin/bash
-
-env | sort
-echo 'RAN MOO' >> /tmp/log
-EOF
-
-    chmod a+x /tmp/moo
-    /tmp/moo
-
-    echo 'log =========================================='
-
-    cat /tmp/log
-
-    echo '=============================================='
     # Install some custom requirements on OS X using brew
     BREW=/usr/local/bin/brew
     if [[ ! -x $BREW ]]; then
@@ -54,6 +13,8 @@ EOF
 
     # Prefer Homebrew's bison over the macOS-provided version
     $BREW link --force bison
+    echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' >> ~/.bash_profile
+    export PATH="/usr/local/opt/bison/bin:$PATH"
 
     # install pip and required pip packages
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
