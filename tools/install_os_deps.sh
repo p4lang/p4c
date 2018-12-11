bde3ec6bcb5596935eb6aa83a11ee7b7b640cd33
Jed Liu <jliu@barefootnetworks.com>
2018-12-10 23:39:23 -0500

    More debugging...

--- tools/install_os_deps.sh
+++ tools/install_os_deps.sh
@@ -1,23 +1,13 @@
 #! /bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    # Install some custom requirements on OS X using brew
-    BREW=/usr/local/bin/brew
-    if [[ ! -x $BREW ]]; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-
-    $BREW update
-    $BREW install autoconf automake bdw-gc bison boost ccache cmake git libtool openssl pkg-config protobuf
-    $BREW install gmp --c++11
-
-    # Prefer Homebrew's bison over the macOS-provided version
-    $BREW link --force bison
     echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' >> ~/.bash_profile
     echo 'echo "===== RAN BASH_PROFILE ====="' >> ~/.bash_profile
     echo 'echo "===== RAN BASH_PROFILE =====" >&2' >> ~/.bash_profile
+    echo 'echo "===== RAN BASH_PROFILE ===== >> /tmp/log"' >> ~/.bash_profile
     echo 'echo "===== RAN BASHRC ====="' >> ~/.bashrc
     echo 'echo "===== RAN BASHRC =====" >&2' >> ~/.bashrc
+    echo 'echo "===== RAN BASHRC =====" >> /tmp/log' >> ~/.bashrc
 
     echo 'env ========================================='
 
@@ -35,18 +25,35 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     cat /Users/travis/.travis/job_stages
 
-    echo '=============================================='
+    echo 'moo =========================================='
 
     cat > /tmp/moo <<EOF
 #!/bin/bash
 
 env | sort
+echo 'RAN MOO' >> /tmp/log
 EOF
 
-    chmod a+r /tmp/moo
+    chmod a+x /tmp/moo
     /tmp/moo
 
+    echo 'log =========================================='
+
+    cat /tmp/log
+
     echo '=============================================='
+    # Install some custom requirements on OS X using brew
+    BREW=/usr/local/bin/brew
+    if [[ ! -x $BREW ]]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    $BREW update
+    $BREW install autoconf automake bdw-gc bison boost ccache cmake git libtool openssl pkg-config protobuf
+    $BREW install gmp --c++11
+
+    # Prefer Homebrew's bison over the macOS-provided version
+    $BREW link --force bison
 
     # install pip and required pip packages
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
