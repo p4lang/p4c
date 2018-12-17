1932d3b7db832acfff34e0b1d58a6ff0a3c72995
Zhao Ma <zma@barefootnetworks.com>
2018-12-17 15:32:26 -0800

    documentation for match_t
        * added != operator

--- lib/match.h
+++ lib/match.h
@@ -20,12 +20,21 @@ limitations under the License.
 #include <stdint.h>
 #include <iostream>
 
+/// The ternary expression being matched, given as a pair of bitmasks:
+/// (word0, word1). The ternary expression cares about the value of an
+/// input bit if the corresponding bit is set in exactly one of the
+/// bitmasks. If it is set in word0, then the input must have a 0 in that
+/// position. Otherwise, if it is set in word1, then the input must have a
+/// 1 in that position. More formally, the ternary expression matches when
+/// the following expression is true:
+///     (input ^ word1) & (word0 ^ word1) == 0
 struct match_t {
     uintmax_t   word0, word1;
     match_t() : word0(0), word1(0) {}
     match_t(uintmax_t w0, uintmax_t w1) : word0(w0), word1(w1) {}
     explicit operator bool() const { return (word0 | word1) != 0; }
     bool operator==(const match_t &a) const { return word0 == a.word0 && word1 == a.word1; }
+    bool operator!=(const match_t &a) const { return word0 != a.word0 || word1 != a.word1; }
     bool matches(uintmax_t v) const {
         return (v | word1) == word1 && ((~v & word1) | word0) == word0; }
     void setwidth(int bits) {
