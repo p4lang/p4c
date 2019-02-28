188c72be8a907e8c1f77e8c626a323f8a98be80b
hemant_mnkcg <hemant@mnkcg.com>
2019-02-27 22:11:57 -0500

    Restore SerEnum back to code

--- control-plane/p4RuntimeSerializer.cpp
+++ control-plane/p4RuntimeSerializer.cpp
@@ -1385,6 +1385,10 @@ class P4RuntimeEntriesConverter {
             return stringRepr(k->to<IR::Constant>(), keyWidth);
         } else if (k->is<IR::BoolLiteral>()) {
             return stringRepr(k->to<IR::BoolLiteral>(), keyWidth);
+        } else if (k->type->is<IR::Type_SerEnum>()) {
+            auto mem = k->to<IR::Member>();
+            auto name = mem->expr->toString();
+            return stringRepr(name);
         } else {
             ::error("%1% invalid key expression", k);
             return boost::none;
@@ -1495,6 +1499,14 @@ class P4RuntimeEntriesConverter {
     boost::optional<std::string> stringRepr(const IR::BoolLiteral* constant, int width) const {
         auto v = static_cast<mpz_class>(constant->value ? 1 : 0);
         return stringReprConstant(v, width);
+    }
+
+    boost::optional<std::string> stringRepr(cstring value) const {
+        if (strlen(value) == 0) {
+           ::error("%1%: P4Runtime does not support zero len in match key", value);
+           return boost::none;
+        }
+        return std::string(value);
     }
 
     /// We represent all static table entries as one P4Runtime WriteRequest object
