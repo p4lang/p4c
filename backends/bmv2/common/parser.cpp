39a31b45210225af48b54f2db59ebefd09c30836
mbudiu-vmw <mbudiu@vmware.com>
2019-03-07 14:51:53 -0800

    Delete spurious space

--- backends/bmv2/common/parser.cpp
+++ backends/bmv2/common/parser.cpp
@@ -230,7 +230,7 @@ Util::IJson* ParserConverter::convertParserStatement(const IR::StatOrDecl* stat)
 
             paramsValue->emplace("op", primitive);
             return result;
-         }
+        }
     }
     ::error(ErrorType::ERR_UNSUPPORTED, "in parser on this target", stat);
     return result;
