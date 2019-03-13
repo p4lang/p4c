5676f51a46a2aad26acbb2c9ef30d7c6d66d6bf8
Mihai Budiu <mbudiu@vmware.com>
2019-03-13 13:50:00 -0700

    Handle Newtype for table keys; fixes #1739 (#1787)
    * Handle Newtype for table keys; fixes #1739

--- frontends/p4/typeChecking/typeChecker.cpp
+++ frontends/p4/typeChecking/typeChecker.cpp
@@ -3326,9 +3326,10 @@ const IR::Node* TypeInference::postorder(IR::KeyElement* elem) {
     if (ktype == nullptr)
         return elem;
     while (ktype->is<IR::Type_Newtype>())
-        ktype = ktype->to<IR::Type_Newtype>()->type;
-    if (!ktype->is<IR::Type_Bits>() && !ktype->is<IR::Type_Enum>() &&
-        !ktype->is<IR::Type_Error>() && !ktype->is<IR::Type_Boolean>())
+        ktype = getTypeType(ktype->to<IR::Type_Newtype>()->type);
+    if (!ktype->is<IR::Type_Bits>() && !ktype->is<IR::Type_SerEnum>() &&
+        !ktype->is<IR::Type_Error>() && !ktype->is<IR::Type_Boolean>() &&
+        !ktype->is<IR::Type_Enum>())
         typeError("Key %1% field type must be a scalar type; it cannot be %2%",
                   elem->expression, ktype->toString());
     auto type = getType(elem->matchType);
