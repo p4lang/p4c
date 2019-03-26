5aa3875554e8901951a6d14c861209b20fadfa7c
Enisa Hadzic <enisa.hadzic@rt-rk.com>
2019-03-26 14:33:02 +0100

    Fix for the issue when number of error values is more than 9
    Changed the way error code value is generated in bmv2 backend.
    Instead of decimal value, it is generated as hex value into json file
    (issue #1824)
    
    Signed-off-by: Enisa Hadzic <enisa.hadzic@rt-rk.com>

--- backends/bmv2/common/expression.cpp
+++ backends/bmv2/common/expression.cpp
@@ -251,7 +251,9 @@ void ExpressionConverter::postorder(const IR::Member* expression)  {
         result->emplace("type", "hexstr");
         auto decl = type->to<IR::Type_Error>()->getDeclByName(expression->member.name);
         auto errorValue = structure->errorCodesMap.at(decl);
-        result->emplace("value", Util::toString(errorValue));
+        // this generates error constant like hex value
+        auto reprValue = stringRepr(errorValue);
+        result->emplace("value", reprValue);
         mapExpression(expression, result);
         return;
     }
