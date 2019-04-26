97b5d67cc4f8141b90c8b237f4304524b8853e8b
Peter J. Li <pl488@cornell.edu>
2019-04-26 20:11:13 +0000

    psa switch should invalidate all headers for pkts entering egress (#769)
    * psa switch should invalidate all headers for pkts entering egress
    
    * psa switch marks packets as drop by default in ingress processing
    
    * fix incorrect ordering of post ingress-mau logic, fix missed reset_exit after maus
    
    * clean up inconsistency in setting metadata, add debug log for ingress drop
    
    * setting egress_port is unicast specific logic, move it after mcast check
    
    * remove unused old temp var and unnecessary explicit cast

--- targets/psa_switch/psa_switch.cpp
+++ targets/psa_switch/psa_switch.cpp
@@ -308,55 +308,52 @@ PsaSwitch::ingress_thread() {
     input_buffer.pop_back(&packet);
     if (packet == nullptr) break;
 
-    Parser *parser = this->get_parser("ingress_parser");
-    Pipeline *ingress_mau = this->get_pipeline("ingress");
+    port_t ingress_port = packet->get_ingress_port();
+    BMLOG_DEBUG_PKT(*packet, "Packet received on port {}", ingress_port);
+
     phv = packet->get_phv();
 
-    port_t ingress_port = packet->get_ingress_port();
-    (void) ingress_port;
-    BMLOG_DEBUG_PKT(*packet, "Processing packet received on port {}",
-                    ingress_port);
-
+    Parser *parser = this->get_parser("ingress_parser");
     parser->parse(packet.get());
+
+    // set default std metadata values
+    phv->get_field("psa_ingress_output_metadata.drop").set(1);
+
+    Pipeline *ingress_mau = this->get_pipeline("ingress");
     ingress_mau->apply(packet.get());
+    packet->reset_exit();
 
     // prioritize dropping if marked as such - do not move below other checks
-    if (phv->has_field("psa_ingress_output_metadata.drop")) {
-      Field &f_drop = phv->get_field("psa_ingress_output_metadata.drop");
-      if (f_drop.get_int()) {
-        continue;
-      }
+    Field &f_drop = phv->get_field("psa_ingress_output_metadata.drop");
+    if (f_drop.get_int()) {
+      BMLOG_DEBUG_PKT(*packet, "Dropping packet marked as such from ingress");
+      continue;
     }
-
-    // handling multicast
-    unsigned int mgid = 0u;
-    if (phv->has_field("psa_ingress_output_metadata.multicast_group")) {
-      Field &f_mgid = phv->get_field("psa_ingress_output_metadata.multicast_group");
-      mgid = f_mgid.get_uint();
-
-      if(mgid != 0){
-        const auto pre_out = pre->replicate({mgid});
-        auto packet_size = packet->get_register(PACKET_LENGTH_REG_IDX);
-        for(const auto &out : pre_out){
-          auto egress_port = out.egress_port;
-          std::unique_ptr<Packet> packet_copy = packet->clone_with_phv_ptr();
-          packet_copy->set_register(PACKET_LENGTH_REG_IDX, packet_size);
-          enqueue(egress_port, std::move(packet_copy));
-        }
-        continue;
-      }
-    }
-
-    packet->reset_exit();
-    Field &f_egress_spec = phv->get_field("psa_ingress_output_metadata.egress_port");
-    port_t egress_spec = f_egress_spec.get_uint();
-    port_t egress_port = egress_spec;
-
-    egress_port = egress_spec;
-    BMLOG_DEBUG_PKT(*packet, "Egress port is {}", egress_port);
 
     Deparser *deparser = this->get_deparser("ingress_deparser");
     deparser->deparse(packet.get());
+
+    // handling multicast
+    unsigned int mgid = 0u;
+    Field &f_mgid = phv->get_field("psa_ingress_output_metadata.multicast_group");
+    mgid = f_mgid.get_uint();
+
+    if(mgid != 0){
+      const auto pre_out = pre->replicate({mgid});
+      auto packet_size = packet->get_register(PACKET_LENGTH_REG_IDX);
+      for(const auto &out : pre_out){
+        auto egress_port = out.egress_port;
+        std::unique_ptr<Packet> packet_copy = packet->clone_with_phv_ptr();
+        packet_copy->set_register(PACKET_LENGTH_REG_IDX, packet_size);
+        enqueue(egress_port, std::move(packet_copy));
+      }
+      continue;
+    }
+
+    Field &f_egress_port = phv->get_field("psa_ingress_output_metadata.egress_port");
+    port_t egress_port = f_egress_port.get_uint();
+    BMLOG_DEBUG_PKT(*packet, "Egress port is {}", egress_port);
+
     enqueue(egress_port, std::move(packet));
   }
 }
@@ -378,16 +375,27 @@ PsaSwitch::egress_thread(size_t worker_id) {
 
     if (packet == nullptr) break;
 
+    phv = packet->get_phv();
+
+    // this reset() marks all headers as invalid - this is important since PSA
+    // deparses packets after ingress processing - so no guarantees can be made
+    // about their existence or validity while entering egress processing
+    phv->reset();
+
     Parser *parser = this->get_parser("egress_parser");
     parser->parse(packet.get());
-    Deparser *deparser = this->get_deparser("egress_deparser");
+
     Pipeline *egress_mau = this->get_pipeline("egress");
     egress_mau->apply(packet.get());
+    packet->reset_exit();
+    // TODO: add stf test where exit is invoked but packet still gets
+    // recirc'd
+
+    Deparser *deparser = this->get_deparser("egress_deparser");
     deparser->deparse(packet.get());
 
     if (port == PSA_PORT_RECIRCULATE) {
       BMLOG_DEBUG_PKT(*packet, "Recirculating packet");
-      phv = packet->get_phv();
 
       phv->reset();
       phv->reset_header_stacks();
