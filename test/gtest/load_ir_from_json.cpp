e2cbb9deb41c1349bc285e263d1c2750a00774e2
Mihai Budiu <mbudiu@vmware.com>
2019-04-26 16:42:38 -0700

    Temporarily disable test which hangs (#1902)
    * Use system instead of fork/exec; fixes #1900

--- test/gtest/load_ir_from_json.cpp
+++ test/gtest/load_ir_from_json.cpp
@@ -16,6 +16,7 @@ limitations under the License.
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -35,38 +36,17 @@ namespace Test {
 class FromJSONTest : public P4CTest { };
 
 TEST_F(FromJSONTest, load_ir_from_json) {
-    #if defined(__linux__)
-    pid_t pid = vfork();
-    if (pid == -1) {
-        ::error("fork() failed");
-    } else if (pid == 0) {
-        execlp("p4c-bm2-ss", "p4c-bm2-ss", "-o", "outputTO.json", "../test/test_fromJSON.p4", "--toJSON", "jsonFile.json", (char *)NULL);    //NOLINT
-    }
-    int child_status;
-    int child_pid = wait(&child_status);
-    pid_t pid2 = vfork();
-    if (pid2 == -1) {
-        ::error("fork() failed");
-    } else if (pid2 == 0) {
-        execlp("p4c-bm2-ss", "p4c-bm2-ss", "-o", "outputFROM.json", "--fromJSON", "jsonFile.json", (char *)NULL);   //NOLINT
-    }
-    int child_status2;
-    int child_pid2 = wait(&child_status2);
-
-    fstream file1("outputTO.json"), file2("outputFROM.json");
-    char string1[256], string2[256];
-    int j; j = 0;
-    while (!file1.eof()) {
-        file1.getline(string1, 256);
-        file2.getline(string2, 256);
-        j++;
-        if (strcmp(string1, string2) != 0) {
-            ASSERT_FALSE(strstr(string1, "program") == nullptr \
-                || strstr(string2, "program") == nullptr);
-        }
-    }
-    #endif
-    ASSERT_TRUE(true);
+    int exitCode = system("./p4c-bm2-ss -o outputTO.json ../test/test_fromJSON.p4 "
+                          "--toJSON jsonFile.json");
+    ASSERT_FALSE(exitCode);
+    exitCode = system("./p4c-bm2-ss -o outputFROM.json --fromJSON jsonFile.json");
+    ASSERT_FALSE(exitCode);
+    system("grep -v program outputTO.json > outputTO.json.tmp; "
+           "mv outputTO.json.tmp outputTO.json");
+    system("grep -v program outputFROM.json > outputFROM.json.tmp; "
+           " mv outputFROM.json.tmp outputFROM.json");
+    exitCode = system("diff outputTO.json outputFROM.json");
+    ASSERT_FALSE(exitCode);
 }
 
 }  // namespace Test
