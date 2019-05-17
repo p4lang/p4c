17dbc8ca5e364999f13e245d3f56fda0280f5bf1
Prathima Kotikalapudi <pkotikalapudi@barefootnetworks.com>
2019-05-16 22:02:04 -0700

    method should not be const as we could add to the invocations

--- midend/removeParameters.h
+++ midend/removeParameters.h
@@ -122,7 +122,7 @@ class DoRemoveActionParameters : public Transform {
     const IR::Node* postorder(IR::P4Action* table) override;
     const IR::Node* postorder(IR::ActionListElement* element) override;
     const IR::Node* postorder(IR::MethodCallExpression* expression) override;
-    const ActionInvocation* getInvocations() {
+    ActionInvocation* getInvocations() {
         return invocations;
     }
 };
