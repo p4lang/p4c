dcf090381ffaa75c206651860b827c292c6be6c6
Tomasz Osinski <osinstom@gmail.com>
2019-05-21 15:51:53 +0200

    Copyright headers added

--- backends/ubpf/ubpfProgram.h
+++ backends/ubpf/ubpfProgram.h
@@ -1,20 +1,29 @@
-//
-// Created by p4dev on 08.04.19.
-//
+/*
+Copyright 2019 Orange
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #ifndef P4C_UBPFPROGRAM_H
 #define P4C_UBPFPROGRAM_H
 
 #include "target.h"
 #include "ubpfModel.h"
-//#include "backends/ebpf/ebpfModel.h"
 #include "ir/ir.h"
 #include "frontends/p4/typeMap.h"
 #include "frontends/p4/evaluator/evaluator.h"
 #include "backends/ebpf/ebpfProgram.h"
-//#include "ubpfModel.h"
-//#include "ubpfParser.h"
-//#include "ubpfControl.h"
+
 
 namespace UBPF {
 
