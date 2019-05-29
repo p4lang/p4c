enisah/behavioral-model

Added support for assert and assume primitives in bm_sim
* Assert and assume primitives are added as part of the implementation
assert and assume support in p4c with id - #1859

* In simulation, if condition of assert/assume evaluates to false,
assert error message will be printed and simulation will be stopped

Signed-off-by: Enisa Hadzic <enisa.hadzic@rt-rk.com>

include/bm/bm_sim/actions.h
@@ -535,6 +535,10 @@ class ActionPrimitive_ {
     this->p4objects = p4objects;
   }
 
+  void set_source_info(SourceInfo *source_info) {
+    srcInfo = source_info;
+  }
+
  protected:
   // This used to be regular members in ActionPrimitive, but there could be a
   // race condition. Making them thread_local solves the issue. I moved these
@@ -543,7 +547,7 @@ class ActionPrimitive_ {
   // (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60056).
   static thread_local Packet *pkt;
   static thread_local PHV *phv;
-
+  SourceInfo *srcInfo = nullptr;
   P4Objects *get_p4objects() {
     return p4objects;
   }

p4lang/p4c

include/bm/bm_sim/actions.h
@@ -535,6 +535,10 @@ class ActionPrimitive_ {
     this->p4objects = p4objects;
   }
 
+  void set_source_info(SourceInfo *source_info) {
+    call_source_info = source_info;
+  }
+
  protected:
   // This used to be regular members in ActionPrimitive, but there could be a
   // race condition. Making them thread_local solves the issue. I moved these
@@ -543,6 +547,7 @@ class ActionPrimitive_ {
   // (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60056).
   static thread_local Packet *pkt;
   static thread_local PHV *phv;
+  SourceInfo *call_source_info = nullptr;
 
   P4Objects *get_p4objects() {
     return p4objects;