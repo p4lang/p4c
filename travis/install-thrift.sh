c67f85d45feea5c31312c9a1e8a7063a976a1469
Polly <26716201@qq.com>
2019-07-23 00:20:42 +0800

    Make thrift's installation friendly for chinese user (#797)
    * Make it possible to get thrift in China without VPN
    
    * Make it possible to get thrift in China without VPN (From git resource instead of apache.org web site)
    
    * Make thrift's installation friendly for chinese user

--- travis/install-thrift.sh
+++ travis/install-thrift.sh
@@ -6,9 +6,12 @@ source $THIS_DIR/common.sh
 check_lib libthrift libthrift-0.9.2
 
 set -e
-wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
-tar -xzvf thrift-0.9.2.tar.gz
+# Make it possible to get thrift in China
+# wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
+# tar -xzvf thrift-0.9.2.tar.gz
+git clone -b 0.9.2 https://github.com/apache/thrift.git thrift-0.9.2
 cd thrift-0.9.2
+./bootstrap.sh
 ./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
 make -j2 && sudo make install
 cd lib/py
