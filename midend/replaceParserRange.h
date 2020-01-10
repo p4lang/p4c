be18c840a65804df51c82f56c9e478aae6c099ce
hemant_mnkcg <hemant@mnkcg.com>
2020-01-10 17:07:57 -0500

    Remove old filed not needed any more

--- midend/replaceParserRange.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MIDEND_REPLACEPARSERRANGE_H_
-#define _MIDEND_REPLACEPARSERRANGE_H_
-
-#include "ir/ir.h"
-#include "frontends/p4/typeChecking/typeChecker.h"
-
-
-#endif /* _MIDEND_REPLACEPARSERRANGE_H_ */
