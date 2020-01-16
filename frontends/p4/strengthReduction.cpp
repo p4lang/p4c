ae6c76fbcfa62651bca3758c83c20c29a81f5e7e
Han Wang <hanwang@barefootnetworks.com>
2020-01-16 10:40:14 -0800

    remove slice if slice width equals field width

--- frontends/p4/strengthReduction.cpp
+++ frontends/p4/strengthReduction.cpp
@@ -310,6 +310,10 @@ const IR::Node* DoStrengthReduction::postorder(IR::Slice* expr) {
         return new IR::Slice(e, hi, lo);
     }
 
+    auto slice_width = expr->getH() - expr->getL() + 1;
+    if (slice_width == expr->e0->type->width_bits())
+        return expr->e0;
+
     return expr;
 }
 
