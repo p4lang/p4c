86fcffe331714e2a33285158ed9f6bbcf48908bc
hesingh <hemant@mnkcg.com>
2020-01-24 10:25:53 -0800

    Backout typeChecker change

--- frontends/p4/typeChecking/typeChecker.h
+++ frontends/p4/typeChecking/typeChecker.h
@@ -96,8 +96,7 @@ class TypeInference : public Transform {
     }
 
     bool isLeftValue(const IR::Expression* expression) const
-    { return typeMap->isLeftValue(expression) || expression->is<IR::DefaultExpression>()
-      || isArrayIndexRuntime(expression); }
+    { return typeMap->isLeftValue(expression) || expression->is<IR::DefaultExpression>(); }
     void setCompileTimeConstant(const IR::Expression* expression)
     { typeMap->setCompileTimeConstant(expression); }
     bool isCompileTimeConstant(const IR::Expression* expression) const
