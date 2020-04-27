7f6609741560e367aa03d2e3ea47c65c13f7cd36
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-04-27 10:47:41 -0700

    Fix lint warning

--- backends/bmv2/common/control.cpp
+++ backends/bmv2/common/control.cpp
@@ -77,7 +77,8 @@ void ControlConverter::convertTableEntries(const IR::P4Table *table,
                 if (k->is<IR::Mask>()) {
                     auto km = k->to<IR::Mask>();
                     key->emplace("key", stringRepr(km->left->to<IR::Constant>()->value, k8));
-                    auto trailing_zeros = [](unsigned long n, unsigned long keyWidth) { return n ? __builtin_ctzl(n) : (int) keyWidth; };
+                    auto trailing_zeros = [](unsigned long n, unsigned long keyWidth)
+                        { return n ? __builtin_ctzl(n) : (int) keyWidth; };
                     auto count_ones = [](unsigned long n) { return n ? __builtin_popcountl(n) : 0;};
                     auto mask = static_cast<unsigned long>(km->right->to<IR::Constant>()->value);
                     auto len = trailing_zeros(mask, keyWidth);
