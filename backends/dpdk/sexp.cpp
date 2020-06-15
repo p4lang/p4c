3f144a28c8fee1b34643b21a5fa834cbbf9e707d
Han Wang <hanwang@barefootnetworks.com>
2020-06-14 22:06:59 -0700

    add more instructions

--- backends/dpdk/sexp.cpp
+++ backends/dpdk/sexp.cpp
@@ -5,14 +5,14 @@
 using namespace DBPrint;
 
 static void print(std::ostream& out, const IR::DpdkAsmStatement* s) {
-    //if (s->is<IR::DpdkJmpStatement>())
-    //    out << s->to<IR::DpdkJmpStatement>() << std::endl;
-    //else if (s->is<IR::DpdkLabelStatement>())
-    //    out << s->to<IR::DpdkLabelStatement>() << std::endl;
+    if (s->is<IR::DpdkJmpStatement>())
+        out << s->to<IR::DpdkJmpStatement>() << std::endl;
+    else if (s->is<IR::DpdkLabelStatement>())
+        out << s->to<IR::DpdkLabelStatement>() << std::endl;
     if (s->is<IR::DpdkMovStatement>())
         out << s->to<IR::DpdkMovStatement>() << std::endl;
-    //else if (s->is<IR::DpdkExternObjStatement>())
-    //    out << s->to<IR::DpdkExternObjStatement>() << std::endl;
+    else if (s->is<IR::DpdkExternObjStatement>())
+        out << s->to<IR::DpdkExternObjStatement>() << std::endl;
     else if (s->is<IR::DpdkListStatement>())
         out << s->to<IR::DpdkListStatement>() << std::endl;
     else
@@ -95,6 +95,116 @@ std::ostream& IR::DpdkMovStatement::toSexp(std::ostream& out) const {
     return out;
 }
 
+std::ostream& IR::DpdkAddStatement::toSexp(std::ostream& out) const {
+    out << "(add";
+    return out;
+}
+
+std::ostream& IR::DpdkAndStatement::toSexp(std::ostream& out) const {
+    out << "(and";
+    return out;
+}
+
+std::ostream& IR::DpdkApplyStatement::toSexp(std::ostream& out) const {
+    out << "(apply";
+    return out;
+}
+
+std::ostream& IR::DpdkEmitStatement::toSexp(std::ostream& out) const {
+    out << "(emit";
+    return out;
+}
+
+std::ostream& IR::DpdkExtractStatement::toSexp(std::ostream& out) const {
+    out << "(extract";
+    return out;
+}
+
+std::ostream& IR::DpdkJmpStatement::toSexp(std::ostream& out) const {
+    out << "(jmp" << label << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkJmpActionStatement::toSexp(std::ostream& out) const {
+    out << "(jmpa" << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkJmpMissStatement::toSexp(std::ostream& out) const {
+    out << "(jmpm" << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkJmpHitStatement::toSexp(std::ostream& out) const {
+    out << "(jmph" << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkRxStatement::toSexp(std::ostream& out) const {
+    out << "(rx " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkTxStatement::toSexp(std::ostream& out) const {
+    out << "(tx " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkShlStatement::toSexp(std::ostream& out) const {
+    out << "(shl " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkShrStatement::toSexp(std::ostream& out) const {
+    out << "(shr " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkSubStatement::toSexp(std::ostream& out) const {
+    out << "(sub " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkOrStatement::toSexp(std::ostream& out) const {
+    out << "(or " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkXorStatement::toSexp(std::ostream& out) const {
+    out << "(xor " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkValidateStatement::toSexp(std::ostream& out) const {
+    out << "(valid " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkInvalidateStatement::toSexp(std::ostream& out) const {
+    out << "(invalid " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkExternObjStatement::toSexp(std::ostream& out) const {
+    out << "(extern_obj " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkExternFuncStatement::toSexp(std::ostream& out) const {
+    out << "(extern_func " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkReturnStatement::toSexp(std::ostream& out) const {
+    out << "(return " << ")";
+    return out;
+}
+
+std::ostream& IR::DpdkLabelStatement::toSexp(std::ostream& out) const {
+    out << "(label " << label << ")";
+    return out;
+}
+
 std::ostream& operator<<(std::ostream& out, const IR::DpdkAsmProgram* p) {
     return p->toSexp(out); }
 std::ostream& operator<<(std::ostream& out, const IR::DpdkHeaderType* h) {
@@ -107,95 +217,48 @@ std::ostream& operator<<(std::ostream& out, const IR::DpdkListStatement* s) {
     return s->toSexp(out); }
 std::ostream& operator<<(std::ostream& out, const IR::DpdkMovStatement* s) {
     return s->toSexp(out); }
-
-#if 0
-std::ostream& operator<<(std::ostream& out, const IR::DpdkAddStatement* statement) {
-    out << "(add";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkAndStatement* statement) {
-    out << "(and";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkApplyStatement* statement) {
-    out << "(apply";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkEmitStatement* statement) {
-    out << "(emit";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkExtractStatement* statement) {
-    out << "(extract";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpStatement* statement) {
-    out << "(jmp " << statement->label << ")";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpActionStatement* statement) {
-    out << "(jmpa";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpHitStatement* statement) {
-    out << "(jmph";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpMissStatement* statement) {
-    out << "(jmpm";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkRxStatement* statement) {
-    out << "(rx";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkTxStatement* statement) {
-    out << "(tx";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkShlStatement* statement) {
-    out << "(shl";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkShrStatement* statement) {
-    out << "(shr";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkSubStatement* statement) {
-    out << "(sub";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkOrStatement* statement) {
-    out << "(or";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkXorStatement* statement) {
-    out << "(xor";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkValidateStatement* statement) {
-    out << "(valid";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkInvalidateStatement* statement) {
-    out << "(invalid";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkExternObjStatement* statement) {
-    out << "(extern " << statement->methodCall << ")";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkExternFuncStatement* statement) {
-    out << "(extern";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkReturnStatement* statement) {
-    out << "(return";
-    return out;
-}
-std::ostream& operator<<(std::ostream& out, const IR::DpdkLabelStatement* statement) {
-    out << "(label " << statement->label << ")";
-    return out;
-}
-#endif
+std::ostream& operator<<(std::ostream& out, const IR::DpdkAddStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkAndStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkApplyStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkEmitStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkExtractStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpActionStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpHitStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkJmpMissStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkRxStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkTxStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkShlStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkShrStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkSubStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkOrStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkXorStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkValidateStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkInvalidateStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkExternObjStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkExternFuncStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkReturnStatement* s) {
+    return s->toSexp(out); }
+std::ostream& operator<<(std::ostream& out, const IR::DpdkLabelStatement* s) {
+    return s->toSexp(out); }
 
