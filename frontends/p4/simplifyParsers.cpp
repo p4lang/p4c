86564c0b0b0e2030cfe18b38a16106e8869fa576
AashleshaPatil-BFN <apatil@barefootnetworks.com>
2020-07-10 11:42:08 -0700

    let @name annotation states merge

--- frontends/p4/simplifyParsers.cpp
+++ frontends/p4/simplifyParsers.cpp
@@ -115,7 +115,9 @@ class CollapseChains : public Transform {
             auto node = oe.first;
             // Avoid merging in case of state annotation
             if (!node->annotations->annotations.empty()) {
-                continue;
+                if (!node->getAnnotation("name") ||
+                    node->annotations->annotations.size() != 1)
+                    continue;
             }
             auto outedges = oe.second;
             if (outedges->size() != 1)
