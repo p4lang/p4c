916eb86f71085e1d11931e975c1ca400b4fffca6
Han2 Wang <hanwang2@jcaguirr-MOBL2.amr.corp.intel.com>
2020-10-23 13:45:02 -0700

    Add main.cpp

---
+++ backends/dpdk/main.cpp
@@ -0,0 +1,107 @@
+/*
+Copyright 2013-present Barefoot Networks, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "backends/bmv2/common/JsonObjects.h"
+#include "backends/bmv2/common/backend.h"
+#include "backends/bmv2/psa_switch/version.h"
+#include "backends/dpdk/midend.h"
+#include "backends/dpdk/backend.h"
+#include "backends/dpdk/options.h"
+#include "control-plane/p4RuntimeSerializer.h"
+#include "frontends/common/applyOptionsPragmas.h"
+#include "frontends/common/parseInput.h"
+#include "frontends/p4/frontend.h"
+#include "ir/ir.h"
+#include "ir/json_loader.h"
+#include "lib/error.h"
+#include "lib/exceptions.h"
+#include "lib/gc.h"
+#include "lib/log.h"
+#include "lib/nullstream.h"
+
+int main(int argc, char *const argv[]) {
+  setup_gc_logging();
+
+  AutoCompileContext autoPsaSwitchContext(new DPDK::PsaSwitchContext);
+  auto &options = DPDK::PsaSwitchContext::get().options();
+  options.langVersion = CompilerOptions::FrontendVersion::P4_16;
+  options.compilerVersion = BMV2_PSA_VERSION_STRING;
+
+  if (options.process(argc, argv) != nullptr) {
+    if (options.loadIRFromJson == false)
+      options.setInputFile();
+  }
+  if (::errorCount() > 0)
+    return 1;
+
+  auto hook = options.getDebugHook();
+
+  const IR::P4Program *program = nullptr;
+  const IR::ToplevelBlock *toplevel = nullptr;
+
+  if (options.loadIRFromJson == false) {
+    program = P4::parseP4File(options);
+
+    if (program == nullptr || ::errorCount() > 0)
+      return 1;
+    try {
+      P4::P4COptionPragmaParser optionsPragmaParser;
+      program->apply(P4::ApplyOptionsPragmas(optionsPragmaParser));
+
+      P4::FrontEnd frontend;
+      frontend.addDebugHook(hook);
+      program = frontend.run(options, program);
+    } catch (const std::exception &bug) {
+      std::cerr << bug.what() << std::endl;
+      return 1;
+    }
+    if (program == nullptr || ::errorCount() > 0)
+      return 1;
+  } else {
+    std::filebuf fb;
+    if (fb.open(options.file, std::ios::in) == nullptr) {
+      ::error("%s: No such file or directory.", options.file);
+      return 1;
+    }
+    std::istream inJson(&fb);
+    JSONLoader jsonFileLoader(inJson);
+    if (jsonFileLoader.json == nullptr) {
+      ::error("Not valid input file");
+      return 1;
+    }
+    program = new IR::P4Program(jsonFileLoader);
+    fb.close();
+  }
+
+  P4::serializeP4RuntimeIfRequired(program, options);
+  if (::errorCount() > 0)
+    return 1;
+
+  if (!options.outputFile.isNullOrEmpty()) {
+    std::ostream *out = openFile(options.outputFile, false);
+    if (out != nullptr) {
+      backend->codegen(*out);
+      out->flush();
+    }
+  }
+
+  return ::errorCount() > 0;
+}
