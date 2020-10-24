f7d655f6ad49094255bc9322fcbcfa757c7e8f23
Han2 Wang <hanwang2@jcaguirr-MOBL2.amr.corp.intel.com>
2020-10-23 18:30:55 -0700

    Add licenses

--- backends/dpdk/midend.cpp
+++ backends/dpdk/midend.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2013-present Barefoot Networks, Inc.
+Copyright 2020 Intel Corp.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
