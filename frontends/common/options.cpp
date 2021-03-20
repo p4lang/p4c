1957fad3dda92aae818a913b3379afe6092ba99f
Volodymyr Peschanenko <volodymyr.peschanenko@garuda.ai>
2021-03-20 10:27:15 +0200

    Simple bug after resolving conflicts was fixed

--- frontends/common/options.cpp
+++ frontends/common/options.cpp
@@ -71,8 +71,8 @@ CompilerOptions::CompilerOptions() : ParserOptions() {
                        }
                        return true; },
                    "Choose output format for the P4Runtime API description (default is binary).\n"
-                   "[Deprecated; use '--p4runtime-files' instead].")
-    registerOption("-loopsUnroll", nullptr,
+                   "[Deprecated; use '--p4runtime-files' instead].");
+    registerOption("--loopsUnroll", nullptr,
                    [this](const char*) { loopsUnrolling = true; return true; },
                    "Unrolling all parser's loops");
 }
