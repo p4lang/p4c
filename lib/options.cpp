bf38bf22a217fe44c6e3846c417ce6a95b3493a7
fruffy <fruffy@nyu.edu>
2021-04-08 09:18:22 +0200

    Fix ptr location.

--- lib/options.cpp
+++ lib/options.cpp
@@ -17,8 +17,8 @@ limitations under the License.
 #include "options.h"
 
 void Util::Options::registerOption(
-    const char *option, const char *argName, OptionProcessor processor,
-    const char *description, OptionFlags flags /* = OptionFlags::Default */) {
+    const char* option, const char* argName, OptionProcessor processor,
+    const char* description, OptionFlags flags /* = OptionFlags::Default */) {
     if (option == nullptr || processor == nullptr || description == nullptr)
         throw std::logic_error("Null argument to registerOption");
     if (strlen(option) <= 1)
@@ -42,15 +42,15 @@ void Util::Options::registerOption(
 
 // Process options; return list of remaining options.
 // Returns 'nullptr' if an error is signalled
-std::vector<const char *> *Util::Options::process(int argc,
-                                                  char *const argv[]) {
+std::vector<const char*>* Util::Options::process(int argc,
+                                                  char* const argv[]) {
     if (argc == 0 || argv == nullptr)
         throw std::logic_error("No arguments to process");
     binaryName = argv[0];
     for (int i = 1; i < argc; i++) {
         cstring opt = argv[i];
-        const char *arg = nullptr;
-        const Option *option = nullptr;
+        const char* arg = nullptr;
+        const Option* option = nullptr;
 
         if (opt.startsWith("--")) {
             option = get(options, opt);
