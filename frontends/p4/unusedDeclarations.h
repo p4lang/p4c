1cadd167afae2f9b15362eda96ce7a47c04aca62
Han Wang <han2.wang@intel.com>
2021-06-07 18:15:00 -0700

    keep unused method declaration in system files

--- frontends/p4/unusedDeclarations.h
+++ frontends/p4/unusedDeclarations.h
@@ -32,7 +32,7 @@ namespace P4 {
  *  - IR::Type_Method
  *  - IR::Type_StructLike
  *  - IR::TypeParameters
- *  - IR::Method
+ *  - IR::Method if declared in system headers
  *
  * Additionally, IR::Declaration_Instance nodes for extern instances are not
  * removed but still trigger warnings.
@@ -83,6 +83,7 @@ class RemoveUnusedDeclarations : public Transform {
     const IR::Node* preorder(IR::Type_SerEnum* type)  override;
 
     const IR::Node* preorder(IR::Declaration_Instance* decl) override;
+    const IR::Node* preorder(IR::Method* decl) override;
 
     // The following kinds of nodes are not deleted even if they are unreferenced
     const IR::Node* preorder(IR::Type_Error* type) override
@@ -95,8 +96,6 @@ class RemoveUnusedDeclarations : public Transform {
     { prune(); return type; }
     const IR::Node* preorder(IR::Type_Method* type) override
     { prune(); return type; }
-    const IR::Node* preorder(IR::Method* decl) override
-    { prune(); return decl; }
     const IR::Node* preorder(IR::Parameter* param) override { return param; }  // never dead
     const IR::Node* preorder(IR::NamedExpression* ne) override { return ne; }  // idem
     const IR::Node* preorder(IR::TypeParameters* p) override { prune(); return p; }  // "
@@ -104,6 +103,8 @@ class RemoveUnusedDeclarations : public Transform {
     const IR::Node* preorder(IR::Declaration_Variable* decl)  override;
     const IR::Node* preorder(IR::Declaration* decl) override { return process(decl); }
     const IR::Node* preorder(IR::Type_Declaration* decl) override { return process(decl); }
+    bool isSystemFile(cstring file);
+    cstring ifSystemFile(const IR::Node* node);  // return file containing node if system file
 };
 
 /** @brief Iterates RemoveUnusedDeclarations until convergence.
