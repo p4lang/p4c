4df55e68d0b1b4bb299cd1251b51028dd45f09de
Han Wang <han2.wang@intel.com>
2021-06-08 21:07:25 -0700

    update gtest

--- test/gtest/parser_unroll.cpp
+++ test/gtest/parser_unroll.cpp
@@ -231,12 +231,12 @@ std::pair<const IR::P4Parser*, const IR::P4Parser*> rewriteParser(const IR::P4Pr
 
 /// Loads example from a file
 const IR::P4Program* load_model(const char* curFile, CompilerOptions& options) {
-    std::string includeDir = std::string(relPath) + std::string("p4include");
+    std::string includeDir = std::string(buildPath) + std::string("p4include");
     auto originalEnv = getenv("P4C_16_INCLUDE_PATH");
     setenv("P4C_16_INCLUDE_PATH", includeDir.c_str(), 1);
     options.loopsUnrolling = true;
     options.compilerVersion = P4TEST_VERSION_STRING;
-    options.file = relPath;
+    options.file = sourcePath;
     options.file += "testdata/p4_16_samples/";
     options.file += curFile;
     auto program = P4::parseP4File(options);
