6221fcf8e18983e40a59760b09a78f647c058499
Marek Sedlacek <marek.sedlacek@intel.com>
2021-09-09 11:18:33 +0000

    Fixed lint error (extra whitespace)

--- test/gtest/exception_test.cpp
+++ test/gtest/exception_test.cpp
@@ -30,7 +30,7 @@ TEST(UtilException, Messages) {
         cstring err(ex.what());
         cstring redir_msg("Compiler Bug:\ntest\n");
         cstring no_redir_msg = cstring(ANSI_RED) + "Compiler Bug" + ANSI_CLR +":\ntest\n";
-        // The error message might or might not be colorized based on if the test are redirected 
+        // The error message might or might not be colorized based on if the test are redirected
         // or not, to make sure both options are valid an array is used.
         bool is_content_correct = (err == redir_msg) || (err == no_redir_msg);
         EXPECT_EQ(is_content_correct, true);
