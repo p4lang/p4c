3538f07997b474153faf62ffb5af743371116c6a
vladyslav-dubina <82110791+vladyslav-dubina@users.noreply.github.com>
2021-12-28 13:57:42 +0200

    Added new test for parserUnroll

--- test/gtest/parser_unroll.cpp
+++ test/gtest/parser_unroll.cpp
@@ -318,4 +318,11 @@ TEST_F(P4CParserUnroll, header_union) {
     ASSERT_EQ(parsers.first->states.size(), parsers.second->states.size());
 }
 
+TEST_F(P4CParserUnroll, bitIntTypes) {
+  auto parsers = loadExample("runtime-index-bmv2.p4");
+  ASSERT_TRUE(parsers.first);
+  ASSERT_TRUE(parsers.second);
+  ASSERT_EQ(parsers.first->states.size(), parsers.second->states.size());
+}
+
 }  // namespace Test
