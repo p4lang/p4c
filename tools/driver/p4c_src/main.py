1375a438c2f884d2d9bae4fb8d2d08b5d7bf7bbc
Abe Skolnik <abe.skolnik@intel.com>
2022-04-01 19:32:36 -0700

    Restructured the driver to do better checking of the input presumed-pathname strings and to provide better error messages when something is wrong.
    IMPORTANT NOTE: this commit also adds the mutual exclusion between JSON pathname inputs [which are only supported in developer builds]
                    and all other [presumably P4] pathname inputs, assuming there is no help CLI arg./flag in effect
                    and the back-end did _not_ indicate that we should not check the CLI arg.s/flags
                    [e.g. because one of its own help CLI arg./flags are effect].
                    Previous versions would silently override “normal” pathname inputs with
                    the JSON pathname when an opportunity to do so arose.
                    I am adding this mutual exclusion in the hope that it will not break anything,
                    e.g. that nobody has any tests that relied on the old behavior in the event of
                    a JSON pathname input being present at the same time as a non-JSON pathname input.

--- tools/driver/p4c_src/main.py
+++ tools/driver/p4c_src/main.py
@@ -45,6 +45,7 @@ def display_supported_targets(cfg):
         ret += str(target) + "\n"
     return ret
 
+JSON_input_flag = "--fromJson" ### DRY
 def add_developer_options(parser):
     parser.add_argument("-T", dest="log_levels",
                         action="append", default=[],
@@ -58,11 +59,18 @@ def add_developer_options(parser):
                         help="[Compiler debugging] Folder where P4 programs are dumped.")
     parser.add_argument("--toJson", dest="json", default=None,
                         help="Dump IR to JSON in the specified file.")
-    parser.add_argument("--fromJson", dest="json_source", default=None,
-                        help="Use IR from JSON representation dumped previously. \
-                        the compilation starts with reduced midEnd.")
+    parser.add_argument(JSON_input_flag, dest="json_source", default=None,
+                        help="Use IR from JSON representation dumped previously. "
+                             "The compilation starts with reduced midEnd. "
+                             "Only allowed/recognized in developer builds.")
     parser.add_argument("--pp", dest="pretty_print", default=None,
                         help="Pretty-print the program in the specified file.")
+
+
+### a utility function for grammatical correctness [and DRY]
+def s_and_were_or_just_was(parameter):
+    return ("s were" if parameter != 1 else " was")
+
 
 def main():
     parser = argparse.ArgumentParser(conflict_handler='resolve')
@@ -173,7 +181,12 @@ def main():
     if env_indicates_developer_build:
         add_developer_options(parser)
 
-    parser.add_argument("source_file", nargs='?', help="Files to compile", default=None)
+    parser.add_argument("P4_source_files", nargs='*', help="P4 source files to compile.", default=None)
+                     ### ^^^^^^^^^^^^^^^:
+                     ###   as of this writing, when compiling from P4 source [i.e. not from JSON],
+                     ###     this must be a single-pathname list which must be copied to the property “source_file”.
+                     ###
+                     ###   Also, the value of the property “json_source” ―― if it exists ―― must be copied to the property “source_file”.
 
     # load supported configuration.
     # We load these before we parse options, so that backends can register
@@ -182,6 +195,13 @@ def main():
     cfg = config.Config(config_prefix = "p4c")
     for cf in cfg_files:
         cfg.load_from_config(cf, parser)
+    ### ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ###
+    ### Abe`s note: I hope my {“source_file” → “P4_source_files”} change (to the “parser” object)
+    ###   at this point in time can`t cause any ill effects in the callee “config.Config.load_from_config”.
+    ###
+    ###   WIP To Do: check with somebody more experienced with this codebase
+    ###              before even _thinking_ of merging this changeset into “main”.
 
     # parse the arguments
     opts = parser.parse_args()
@@ -228,6 +248,23 @@ def main():
     if backend == None:
         parser.error("Unknown backend: {}-{}".format(str(opts.target),
                                                      str(opts.arch)))
+    error_count = 0
+
+    JSON_input_specified = env_indicates_developer_build and opts.json_source ### DRY
+    P4_input_or_inputs_specified = len(opts.P4_source_files) > 0
+    any_input_specified = JSON_input_specified or P4_input_or_inputs_specified ### for readability
+
+    if JSON_input_specified and P4_input_or_inputs_specified:
+        ### Note: this restriction was added by Abe, so if there is a test case that
+        ###       tries to feed the driver both a JSON input pathname via the relevant flag _and_
+        ###       another input pathname, that will now fail whereas before
+        ###       the JSON input pathname would silently override the other input pathname[s].
+        print ('\nERROR: it seems that both (firstly) a JSON input pathname was specified via "' + JSON_input_flag +
+                '" _and_ (secondly) ' + str( len(opts.P4_source_files) ) +
+                " other input pathname" + s_and_were_or_just_was( len(opts.P4_source_files) ) +
+                " specified.  A JSON input at the same time as a P4 input is currently unsupported.")
+        error_count = 1 ### Replace ‘=’ with “+=” if/when ever moving this line of code to
+                        ###   somewhere it might not be the first error detection.
 
     # When using --help-* options, we don't necessarily need to pass an input file
     # However, by default the driver checks the input and fails if it does not exist.
@@ -236,26 +273,88 @@ def main():
     # should_not_check_input method.
     checkInput = not (opts.help_pragmas or backend.should_not_check_input(opts))
 
-    input_specified = False
-    if opts.source_file:
-        input_specified = True
-    if env_indicates_developer_build and opts.json_source:
-       input_specified = True
-    if checkInput and not input_specified:
-        parser.error('No input specified.')
-    elif not checkInput:
-        opts.source_file = "dummy.p4"
+    use_a_dummy_P4_pseudoPathname = False
+    ### a note from Abe: do we really still need the dummy pathname?
+    ###                  Maybe we can take all the dummy-related code out once my work to improve the
+    ###                    pathname handling in the driver is good enough for merging to “main”.
+    if checkInput:
+        if not any_input_specified:
+          parser.error("no input pathname was specified.")
+          ### reminder: at this point, the program is dead
 
-    if env_indicates_developer_build and opts.json_source:
-        opts.source_file = opts.json_source
+        if len(opts.P4_source_files) > 1:
+            print ("\nSorry; as of this writing, the P4 compiler driver does not support multiple "
+                   "top-level P4 source files in a single invocation.  Multiple P4 source files at a time "
+                   'are currently only supported via "#include" (i.e. additional non-top-level P4 source files).  '
+                   "Number of top-level P4 source-file pathnames detected: " + str( len(opts.P4_source_files) ), file = sys.stderr)
+            error_count += 1
 
-    if checkInput and not os.path.isfile(opts.source_file):
-        print('Input file {} does not exist'.format(opts.source_file),
-              file = sys.stderr)
-        sys.exit(1)
+        ### We need to do the next 3 lines of code this way
+        ###   (instead of iterating over a concise “list if list else []” expression)
+        ###   because “opts.json_source” might be invalid [i.e. we are _not_ in a developer build].
+        pathnames_to_check = opts.P4_source_files.copy()
+        ### Abe`s reminder to self re the preceding line: a plain “pathnames_to_check = opts.P4_source_files” creates an _alias_!!!
+        if JSON_input_specified:
+            pathnames_to_check.append(opts.json_source)
+
+        for pathname in pathnames_to_check:
+            if not os.path.isfile(pathname):
+                print ('\nERROR: the input file "{}" does not exist.'.format(pathname), file = sys.stderr)
+                error_count += 1
+
+    elif not any_input_specified:
+        use_a_dummy_P4_pseudoPathname = True
+
+
+
+    if error_count != 0:
+        print ("\nSorry; {} error{} found while analyzing the command inputs."
+               "  Aborting the P4 compiler driver.\n".format(error_count, s_and_were_or_just_was(error_count) ) )
+        sys.exit( min(255, error_count) ) ### maximum of 255: being extra-careful here,
+                                          ### just in case “error_count” is a positive integer multiple of 256
+
+
+
+    ### A reminder: by this point, “error_count” _must_ equal zero, due to the “sys.exit” call immediately above,
+    ###   so we can make sanity-checking assertions in the following code without double-checking “error_count” first.
+
+
+
+    ### Figure out what we need to pass to the backend as “opts.source_file”,
+    ###   since that is the interface contract between the driver and backends as of this writing.
+    ### Do some sanity checking along the way (e.g. assertions).
+
+    string_to_pass_as___source_file = "" ### _intentionally_ initializing with a value that Python considers false
+
+    if JSON_input_specified:
+        assert 0 == len(opts.P4_source_files)
+        assert not use_a_dummy_P4_pseudoPathname
+        string_to_pass_as___source_file = opts.json_source
+
+    if P4_input_or_inputs_specified:
+        assert 1 == len(opts.P4_source_files)
+        assert not JSON_input_specified
+        string_to_pass_as___source_file = opts.P4_source_files[0]
+
+    if use_a_dummy_P4_pseudoPathname:
+        assert 0 == len(opts.P4_source_files)
+        assert not any_input_specified
+        assert not JSON_input_specified
+        assert not P4_input_or_inputs_specified
+        string_to_pass_as___source_file = "dummy.p4"
+
+
+
+    ### The following is admittedly somewhat hackish, but was the only way Abe could figure out how to set
+    ###   the appropriate attribute for the interface contract with the back end while maintaining
+    ###   the established norm that “opts” is of type “argparse.Namespace”, as it was before Abe touched this file.
+    opts.__setattr__("source_file", string_to_pass_as___source_file)
+
+
 
     # set all configuration and command line options for backend
     backend.process_command_line_options(opts)
+
     # run all commands
     rc = backend.run()
     sys.exit(rc)
