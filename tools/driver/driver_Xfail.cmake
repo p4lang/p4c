4c0499e381b213cd29d025d31cbe461efb59b7eb
Abe Skolnik <abe.skolnik@intel.com>
2022-04-07 14:40:18 -0700

    added “tools/driver/driver_Xfail.cmake” in order to try to placate the GitHub CI/CD about these currently-XFAIL tests

---
+++ tools/driver/driver_Xfail.cmake
@@ -0,0 +1,4 @@
+p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_1)
+p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_2)
+p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_3)
+p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_4)
