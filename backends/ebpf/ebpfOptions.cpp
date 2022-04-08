1d88a39624c299bba60f294cf86cbd48f198c870
Tomasz Osinski <osinstom@gmail.com>
2022-04-08 08:50:30 +0200

    Fix cpplint

--- backends/ebpf/ebpfOptions.cpp
+++ backends/ebpf/ebpfOptions.cpp
@@ -37,5 +37,6 @@ EbpfOptions::EbpfOptions() {
                    }
                    return true;
                 },
-                "[psa only] Select the mode used to pass metadata from XDP to TC (possible values: meta, head, cpumap).");
+                "[psa only] Select the mode used to pass metadata from XDP to TC "
+                "(possible values: meta, head, cpumap).");
 }
