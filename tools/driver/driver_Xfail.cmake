cbecd761f6c8712aa29df166e6fa43f504a9a539
Abe Skolnik <abe.skolnik@intel.com>
2022-04-08 11:52:31 -0700

    in {repo.}/“tools/driver/”, moved _all_ the non-comment contents of “driver_Xfail.cmake” to the end of “CMakeLists.txt”, in yet another attempt to appease the evil CMake/CTest/CI-CD gods.  Are we having fun yet?  😠

--- tools/driver/driver_Xfail.cmake
+++ tools/driver/driver_Xfail.cmake
@@ -1,16 +1,1 @@
 ### Added by Abe starting April 6 2022; these tests are all expected to fail until Abe`s branch with driver improvements is merged.
-
-set(XFAIL driver_inputs_test_1
-          driver_inputs_test_2
-          driver_inputs_test_3
-          driver_inputs_test_4)
-
-set_tests_properties(driver_inputs_test_1 PROPERTIES LABELS "XFAIL")
-set_tests_properties(driver_inputs_test_2 PROPERTIES LABELS "XFAIL")
-set_tests_properties(driver_inputs_test_3 PROPERTIES LABELS "XFAIL")
-set_tests_properties(driver_inputs_test_4 PROPERTIES LABELS "XFAIL")
-
-p4c_add_xfail_reason("driver" "fail" driver_inputs_test_1)
-p4c_add_xfail_reason("driver" "fail" driver_inputs_test_2)
-p4c_add_xfail_reason("driver" "fail" driver_inputs_test_3)
-p4c_add_xfail_reason("driver" "fail" driver_inputs_test_4)
