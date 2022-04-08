28bc414edfca577f0d5f2b9b26440f8ead3fd524
Abe Skolnik <abe.skolnik@intel.com>
2022-04-08 09:27:03 -0700

    kitchen-sink approach to trying to get the GitHub CI testing to accept my new-ish tests as expected-to-fail-for-now

--- tools/driver/driver_Xfail.cmake
+++ tools/driver/driver_Xfail.cmake
@@ -1,4 +1,6 @@
-p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_1)
-p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_2)
-p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_3)
-p4c_add_xfail_reason("driver" "This test is _intended_ to fail on the old (pre-{April 4 2022}) driver, and should pass if/when Abe`s driver improvements will be merged." driver_inputs_test_4)
+### Added by Abe starting April 6 2022; these tests are all expected to fail until Abe`s branch with driver improvements is merged.
+
+p4c_add_xfail_reason("driver" "fail" driver_inputs_test_1)
+p4c_add_xfail_reason("driver" "fail" driver_inputs_test_2)
+p4c_add_xfail_reason("driver" "fail" driver_inputs_test_3)
+p4c_add_xfail_reason("driver" "fail" driver_inputs_test_4)
