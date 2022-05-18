260704eb76bda9b5f963a8322caf14c586db6a92
Mateusz Kossakowski <mateusz.kossakowski@orange.com>
2022-05-18 12:12:02 +0200

    Update psabpf

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -82,7 +82,7 @@ function install_ptf_ebpf_test_deps() (
   git clone --recursive https://github.com/P4-Research/psabpf.git /tmp/psabpf
   cd /tmp/psabpf
   # FIXME: psabpf is under heavy development, later use git tags when it will be ready to use
-  git reset --hard 9cfa06c
+  git reset --hard bbfbf0b
   ./build_libbpf.sh
   mkdir build
   cd build
