c8ea067ecb9e6853230661d2fb8c15531dac3544
Jan Palimąka <25018671+tatry@users.noreply.github.com>
2022-06-13 15:50:54 +0200

    psabpf-ctl: update action-selector CLI - rename commands

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -82,7 +82,7 @@ function install_ptf_ebpf_test_deps() (
   git clone --recursive https://github.com/P4-Research/psabpf.git /tmp/psabpf
   cd /tmp/psabpf
   # FIXME: psabpf is under heavy development, later use git tags when it will be ready to use
-  git reset --hard 78445a5
+  git reset --hard 986981c
   ./build_libbpf.sh
   mkdir build
   cd build
