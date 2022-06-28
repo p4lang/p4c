929705fcf73111de9ceed06f8b1c70f4e930ea62
VolodymyrPeschanenkoIntel <volodymyrx.peschanenko@intel.com>
2022-06-28 15:09:47 +0300

    add def. initialization

--- midend/parserUnroll.cpp
+++ midend/parserUnroll.cpp
@@ -6,7 +6,7 @@
 
 namespace P4 {
 
-StackVariable::StackVariable(const IR::Expression* expr) {
+StackVariable::StackVariable(const IR::Expression* expr) : variable(nullptr) {
     CHECK_NULL(expr);
     BUG_CHECK(repOk(expr), "Invalid stack variable %1%", expr);
     variable = expr;
