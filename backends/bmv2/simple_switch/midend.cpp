7d0f74fad27aee6deffdb8a35e9683afe29f3890
fruffy <fruffy@nyu.edu>
2022-07-30 10:32:17 +0200

    Remove v1model changes.

--- backends/bmv2/simple_switch/midend.cpp
+++ backends/bmv2/simple_switch/midend.cpp
@@ -15,7 +15,6 @@ limitations under the License.
 */
 
 #include "midend.h"
-#include "control-plane/p4RuntimeArchStandard.h"
 #include "frontends/common/constantFolding.h"
 #include "frontends/common/resolveReferences/resolveReferences.h"
 #include "frontends/p4/evaluator/evaluator.h"
@@ -61,7 +60,6 @@ limitations under the License.
 #include "midend/midEndLast.h"
 #include "midend/fillEnumMap.h"
 #include "midend/removeAssertAssume.h"
-#include "control-plane/addMissingIds.h"
 #include "backends/bmv2/simple_switch/options.h"
 
 
