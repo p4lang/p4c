96064944788c9a98c6af84b4aada5aa72ec8aa0c
Usha Gupta <usha.gupta@intel.com>
2022-09-02 07:57:56 +0000

    Add tests and update reference output

--- backends/dpdk/DpdkXfail.cmake
+++ backends/dpdk/DpdkXfail.cmake
@@ -40,6 +40,13 @@ p4c_add_xfail_reason("dpdk"
 )
 
 p4c_add_xfail_reason("dpdk"
+  "Error compiling"
+  testdata/p4_16_samples/pna-dpdk-wrong-warning.p4
+  testdata/p4_16_samples/pna-dpdk-invalid-hdr-warnings6.p4
+  testdata/p4_16_samples/psa-dpdk-lpm-match-err3.p4
+  )
+
+p4c_add_xfail_reason("dpdk"
   "Unknown extern function"
   testdata/p4_16_samples/psa-example-digest-bmv2.p4
   testdata/p4_16_samples/psa-example-parser-checksum.p4
