80e8635f832670fba40f8a1ffd3d189a2d5c717d
Usha Gupta <usha.gupta@intel.com>
2022-09-19 06:20:11 +0000

    Use max configurable value as array size for default timeout array

--- backends/dpdk/constants.h
+++ backends/dpdk/constants.h
@@ -29,7 +29,8 @@ const unsigned dpdk_default_table_size = 65536;
 const unsigned dpdk_learner_max_configurable_timeout_values = 8;
 const unsigned default_learner_table_size = 0x10000;
 // default timeout values for learner table to support common protocol states
-const unsigned default_learner_table_timeout[] = {10, 30, 60, 120, 300, 43200, 120, 120};
+const unsigned default_learner_table_timeout[dpdk_learner_max_configurable_timeout_values] =
+                                            {10, 30, 60, 120, 300, 43200, 120, 120};
 
 // JSON schema versions
 const cstring bfrtSchemaVersion = "1.0.0";
