45b8538373d8ed16d4780c0ceda9adcd32844b81
Mihai Budiu <mbudiu@vmware.com>
2022-11-07 12:46:57 -0800

    Insert casts in action call arguments
    Signed-off-by: Mihai Budiu <mbudiu@vmware.com>

--- frontends/p4/specializeGenericTypes.cpp
+++ frontends/p4/specializeGenericTypes.cpp
@@ -167,7 +167,11 @@ const IR::Node* ReplaceTypeUses::postorder(IR::Type_Specialized* type) {
 
 const IR::Node* ReplaceTypeUses::postorder(IR::StructExpression* expression) {
     auto st = getOriginal<IR::StructExpression>()->structType;
-    CHECK_NULL(st);
+    if (!st) {
+        ::error(ErrorType::ERR_TYPE_ERROR, "%1%: could not infer a type for expression; "
+                "please specify it explicitly", expression);
+        return expression;
+    }
     auto spec = st->to<IR::Type_Specialized>();
     if (spec == nullptr)
         return expression;
--- frontends/p4/typeChecking/typeChecker.cpp
+++ frontends/p4/typeChecking/typeChecker.cpp
@@ -607,6 +607,7 @@ const IR::Type* TypeInference::canonicalize(const IR::Type* type) {
 ///////////////////////////////////// Visitor methods
 
 const IR::Node* TypeInference::preorder(IR::P4Program* program) {
+    currentActionList = nullptr;
     if (typeMap->checkMap(getOriginal()) && readOnly) {
         LOG2("No need to typecheck");
         prune();
@@ -633,6 +634,7 @@ const IR::Node* TypeInference::postorder(IR::P4Table* table) {
     auto type = new IR::Type_Table(table);
     setType(getOriginal(), type);
     setType(table, type);
+    currentActionList = nullptr;
     return table;
 }
 
@@ -801,6 +803,7 @@ TypeInference::assignment(const IR::Node* errorPosition, const IR::Type* destTyp
     if (auto ts = concreteType->to<IR::Type_StructLike>()) {
         auto si = sourceExpression->to<IR::StructExpression>();
         auto type = destType->getP4Type();
+        setType(type, new IR::Type_Type(destType));
         if (initType->is<IR::Type_UnknownStruct>() ||
             (si != nullptr && initType->is<IR::Type_Struct>())) {
             // Even if the structure is a struct expression with the right type,
@@ -2077,7 +2080,7 @@ const IR::Node* TypeInference::postorder(IR::Entry* entry) {
                               ks->to<IR::ListExpression>(), entry->action, entry->singleton);
 
     auto actionRef = entry->getAction();
-    auto ale = validateActionInitializer(actionRef, table);
+    auto ale = validateActionInitializer(actionRef);
     if (ale != nullptr) {
         auto anno = ale->getAnnotation(IR::Annotation::defaultOnlyAnnotation);
         if (anno != nullptr) {
@@ -3255,10 +3258,13 @@ TypeInference::actionCall(bool inActionList,
         left.emplace(p->name, p);
 
     auto paramIt = baseType->parameters->parameters.begin();
+    auto newArgs = new IR::Vector<IR::Argument>();
+    bool changed = false;
     for (auto arg : *actionCall->arguments) {
         cstring argName = arg->name.name;
         bool named = !argName.isNullOrEmpty();
         const IR::Parameter* param;
+        auto newExpr = arg->expression;
 
         if (named) {
             param = baseType->parameters->getParameter(argName);
@@ -3276,7 +3282,7 @@ TypeInference::actionCall(bool inActionList,
 
         LOG2("Action parameter " << dbp(param));
         auto leftIt = left.find(param->name.name);
-        // This shold have been checked by the CheckNamedArgs pass.
+        // This should have been checked by the CheckNamedArgs pass.
         BUG_CHECK(leftIt != left.end(), "%1%: Duplicate argument name?", param->name);
         left.erase(leftIt);
 
@@ -3305,10 +3311,26 @@ TypeInference::actionCall(bool inActionList,
                    param->direction == IR::Direction::InOut) {
             if (!isLeftValue(arg->expression))
                 typeError("%1%: must be a left-value", arg->expression);
+        } else {
+            // This is like an assignment; may make additional conversions.
+            newExpr = assignment(arg, param->type, arg->expression);
+        }
+        if (::errorCount() > 0)
+            return actionCall;
+        if (newExpr != arg->expression) {
+            LOG2("Changing action argument to " << newExpr);
+            changed = true;
+            newArgs->push_back(new IR::Argument(arg->srcInfo, arg->name, newExpr));
+        } else {
+            newArgs->push_back(arg);
         }
         if (!named)
             ++paramIt;
     }
+    if (changed)
+        actionCall = new IR::MethodCallExpression(
+            actionCall->srcInfo, actionCall->type, actionCall->method,
+            actionCall->typeArguments, newArgs);
 
     // Check remaining parameters: they must be all non-directional
     bool error = false;
@@ -3960,12 +3982,23 @@ const IR::Node* TypeInference::postorder(IR::KeyElement* elem) {
     return elem;
 }
 
-const IR::ActionListElement* TypeInference::validateActionInitializer(
-    const IR::Expression* actionCall,
-    const IR::P4Table* table) {
+const IR::Node* TypeInference::postorder(IR::ActionList* al) {
+    LOG3("TI Visited " << dbp(al));
+    BUG_CHECK(currentActionList == nullptr, "%1%: nested action list?", al);
+    currentActionList = al;
+    return al;
+}
 
-    auto al = table->getActionList();
+const IR::ActionListElement* TypeInference::validateActionInitializer(
+    const IR::Expression* actionCall) {
+
+    // We cannot retrieve the action list from the table, because the
+    // table has not been modified yet.  We want the latest version of
+    // the action list, as it has been already typechecked.
+    auto al = currentActionList;
     if (al == nullptr) {
+        auto table = findContext<IR::P4Table>();
+        BUG_CHECK(table, "%1%: not within a table", actionCall);
         typeError("%1% has no action list, so it cannot have %2%",
                   table, actionCall);
         return nullptr;
@@ -4066,12 +4099,10 @@ const IR::Node* TypeInference::postorder(IR::Property* prop) {
                 return prop;
             }
 
-            auto table = findContext<IR::P4Table>();
-            BUG_CHECK(table != nullptr, "%1%: property not within a table?", prop);
             // Check that the default action appears in the list of actions.
             BUG_CHECK(prop->value->is<IR::ExpressionValue>(), "%1% not an expression", prop);
             auto def = prop->value->to<IR::ExpressionValue>()->expression;
-            auto ale = validateActionInitializer(def, table);
+            auto ale = validateActionInitializer(def);
             if (ale != nullptr) {
                 auto anno = ale->getAnnotation(IR::Annotation::tableOnlyAnnotation);
                 if (anno != nullptr) {
