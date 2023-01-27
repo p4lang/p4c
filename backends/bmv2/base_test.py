54fdd242ef825c314de4fc813dc02482cc6ab9c4
OleksandrLetychevskyiIntel <oleksandrx.letychevskyi@intel.com>
2023-01-27 12:35:09 +0200

    Fix verify counter function [no-jira]

--- backends/bmv2/base_test.py
+++ backends/bmv2/base_test.py
@@ -1030,46 +1030,63 @@ class P4RuntimeTest(BaseTest):
             )
             counter_id = self.get_counter_id(counter_from_testgen["counter_name"])
             for value in counter_from_testgen["counter_values"]:
-                for counter_from_switch in counters_from_switch:
+                if counter_from_testgen["counter_type"] == 0:
+                    counters_from_switch = [
+                        x for x in counters_from_switch if x.data.packet_count != 0
+                    ]
+                if counter_from_testgen["counter_type"] == 1:
+                    counters_from_switch = [
+                        x for x in counters_from_switch if x.data.byte_count != 0
+                    ]
+                if counter_from_testgen["counter_type"] == 2:
+                    counters_from_switch = [
+                        x
+                        for x in counters_from_switch
+                        if x.data.byte_count != 0 or x.data.packet_count != 0
+                    ]
+
+                if (len(counters_from_switch)>0):
+                    counter = next(
+                        item
+                        for item in counters_from_switch
+                        if counter_id == item.counter_id
+                        and value["index"] == item.index.index
+                    )
+
+                if counter_from_testgen["counter_type"] == 0:
+                    if value["value"] != counter.data.packet_count:
+                        e = "Expected {0} counter entry at index {1} with value {2} for type PACKETS , came value {3} ".format(
+                            counter_from_testgen["counter_name"],
+                            value["index"],
+                            value["value"],
+                            counter.data.packet_count,
+                        )
+                        logging.error(e)
+                        raise RuntimeError(e)
+                if counter_from_testgen["counter_type"] == 1:
+                    if value["value"] != counter.data.byte_count:
+                        e = "Expected {0} counter entry at index {1} with value {2} for type BYTES , came value {3} ".format(
+                            counter_from_testgen["counter_name"],
+                            value["index"],
+                            value["value"],
+                            counter.data.byte_count,
+                        )
+                        logging.error(e)
+                        raise RuntimeError(e)
+                if counter_from_testgen["counter_type"] == 2:
                     if (
-                        counter_id == counter_from_switch.counter_id
-                        and value["index"] == counter_from_switch.index.index
+                        value["value"] != counter.data.byte_count
+                        or value["value"] != counter.data.packet_count
                     ):
-                        if counter_from_testgen["counter_type"] == 0:
-                            if value["value"] != counter_from_switch.data.packet_count:
-                                e = "Expected {0} counter entry at index {1} with value {2} for type PACKETS , came value {3} ".format(
-                                    counter_from_testgen["counter_name"],
-                                    value["index"],
-                                    value["value"],
-                                    counter_from_switch.data.packet_count,
-                                )
-                                logging.error(e)
-                                raise RuntimeError(e)
-                        if counter_from_testgen["counter_type"] == 1:
-                            if value["value"] != counter_from_switch.data.byte_count:
-                                e = "Expected {0} counter entry at index {1} with value {2} for type BYTES , came value {3} ".format(
-                                    counter_from_testgen["counter_name"],
-                                    value["index"],
-                                    value["value"],
-                                    counter_from_switch.data.byte_count,
-                                )
-                                logging.error(e)
-                                raise RuntimeError(e)
-                        if counter_from_testgen["counter_type"] == 2:
-                            if (
-                                value["value"] != counter_from_switch.data.byte_count
-                                or value["value"]
-                                != counter_from_switch.data.packet_count
-                            ):
-                                e = "Expected {0} counter entry at index {1} with value {2} for type PACKETS_AND_BYTES , came packet value {3} nad bytes value {4} ".format(
-                                    counter_from_testgen["counter_name"],
-                                    value["index"],
-                                    value["value"],
-                                    counter_from_switch.data.packet_count,
-                                    counter_from_switch.data.byte_count,
-                                )
-                                logging.error(e)
-                                raise RuntimeError(e)
+                        e = "Expected {0} counter entry at index {1} with value {2} for type PACKETS_AND_BYTES , came packet value {3} nad bytes value {4} ".format(
+                            counter_from_testgen["counter_name"],
+                            value["index"],
+                            value["value"],
+                            counter.data.packet_count,
+                            counter.data.byte_count,
+                        )
+                        logging.error(e)
+                        raise RuntimeError(e)
 
     def make_table_read_request(self, table_name):
         req = p4runtime_pb2.ReadRequest()
