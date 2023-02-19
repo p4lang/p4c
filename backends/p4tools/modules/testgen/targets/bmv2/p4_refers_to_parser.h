dc6a60378f64c93c3834b1f2e2f5098b38345000
fruffy <fruffy@nyu.edu>
2023-02-18 20:06:39 -0500

    Fix up the refers_to parser.

--- backends/p4tools/modules/testgen/targets/bmv2/p4_refers_to_parser.h
+++ backends/p4tools/modules/testgen/targets/bmv2/p4_refers_to_parser.h
@@ -8,31 +8,21 @@
 #include "ir/visitor.h"
 #include "lib/cstring.h"
 
-namespace P4Tools {
-namespace RefersToParser {
+namespace P4Tools::RefersToParser {
 
 class RefersToParser : public Inspector {
     std::vector<std::vector<const IR::Expression *>> &restrictionsVec;
 
-    std::vector<const IR::P4Action *> actionVector;
-
-    /// Finds a P4Action in the actionVector according
-    /// to the specified input argument which is an ActionListElement
-    const IR::P4Action *findAction(const IR::ActionListElement *input);
-
     /// An intermediate function that determines the type for future variables and partially
     /// collects their names for them, after which it calls the createConstraint function,
     /// which completes the construction of the constraint
-    void createRefersToConstraint(const IR::Vector<IR::Annotation> &annotations,
-                                  const IR::Type *inputType, cstring controlPlaneName, int id,
-                                  bool isParameter, cstring inputName);
+    void createRefersToConstraint(const IR::Annotation *annotation, const IR::Type *inputType,
+                                  cstring controlPlaneName, bool isParameter, cstring inputName);
 
  public:
     explicit RefersToParser(std::vector<std::vector<const IR::Expression *>> &output);
 
-    void postorder(const IR::ActionListElement *action) override;
-
-    void postorder(const IR::Annotation *annotation) override;
+    bool preorder(const IR::P4Table *table) override;
 
     /// Builds names for the zombie constant and then creates a zombie constant and builds the
     /// refers_to constraints based on them
@@ -40,7 +30,6 @@ class RefersToParser : public Inspector {
                           cstring destKeyName, cstring destTableName, const IR::Type *type);
 };
 
-}  // namespace RefersToParser
-}  // namespace P4Tools
+}  // namespace P4Tools::RefersToParser
 
 #endif /* BACKENDS_P4TOOLS_MODULES_TESTGEN_TARGETS_BMV2_P4_REFERS_TO_PARSER_H_ */
