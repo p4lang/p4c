a1c8654e812eceaf32a4daa3076e238799445c93
Jan Palimąka <25018671+tatry@users.noreply.github.com>
2023-03-03 14:58:23 +0100

    fix fixme description

--- backends/ebpf/codeGen.cpp
+++ backends/ebpf/codeGen.cpp
@@ -463,8 +463,8 @@ unsigned EBPFInitializerUtils::ebpfTypeWidth(P4::TypeMap *typeMap, const IR::Exp
     if (type == nullptr) type = expr->type;
     if (type->is<IR::Type_InfInt>()) return 32;  // let's assume 32 bit for int type
     if (type->is<IR::Type_Set>()) type = type->to<IR::Type_Set>()->elementType;
-    // FIXME: When select expression from parser supports more than one field then only the first
-    //        component will taken into account.
+    // FIXME: When a select expression from the parser supports more than one field then condition
+    //        below takes into account only the first component, remaining fields will be ignored.
     if (type->is<IR::Type_List>()) type = type->to<IR::Type_List>()->components.front();
 
     auto ebpfType = EBPFTypeFactory::instance->create(type);
