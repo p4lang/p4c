b9dc3f482a06a39569cd39f15297ccaaedb5cae8
fruffy <fruffy@nyu.edu>
2023-03-27 20:41:44 -0400

    Clean up the commit hook installation script.

--- tools/install_git_hooks.sh
+++ tools/install_git_hooks.sh
@@ -4,23 +4,24 @@
 
 umask 0022
 set -e
+baseDir="$(realpath "$(dirname "$0")")"
+gitDir="$(git rev-parse --show-superproject-working-tree)/.git"
 
-baseDir="$(realpath "$(dirname "$0")")/.."
-
-pushd "${baseDir}"
-if [[ -d ${baseDir}/.git/hooks ]] ; then
-  for hook in ${baseDir}/tools/hooks/* ; do
+pushd "${baseDir}" > /dev/null
+if [[ -d ${gitDir}/hooks ]] ; then
+  for hook in ${baseDir}/hooks/* ; do
     # Skip the README file.
     if [[ "${hook: -3}" == ".md" ]] ; then
         continue
     fi
-    echo "Installing hook ${hook}"
     # Corner case for when scripts/hooks is empty.
     [[ -e "${hook}" ]] || continue
 
-    hook_base="$(basename "${hook}")"
-    [[ ! -e "${baseDir}/.git/hooks/${hook}" ]] && ln -sf "${hook}" "${baseDir}/.git/hooks/${hook_base}"
-
+    hook_dir="${gitDir}/hooks/$(basename "${hook}")"
+    if [[ ! -e "${hook_dir}" ]] ; then
+      echo "Installing hook \"${hook}\" to \"${hook_dir}\""
+      ln -sf "${hook}" "${hook_dir}"
+    fi
     # Ensure we finish with a success even when the above '-e' test fails.
     true
   done
