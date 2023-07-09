791e743b635232eb0fa4545ac226cb29f0fe0ca4
fruffy <fruffy@nyu.edu>
2023-07-09 18:44:39 -0400

    Install Protobuf using FetchContent.

---
+++ cmake/Protobuf.cmake
@@ -0,0 +1,70 @@
+macro(p4c_obtain_protobuf)
+  option(
+    TOOLS_USE_PREINSTALLED_PROTOBUF
+    "Look for a preinstalled version of Protobuf instead of installing a prebuilt binary using FetchContent."
+    OFF
+  )
+
+  if(TOOLS_USE_PREINSTALLED_PROTOBUF)
+    if(ENABLE_PROTOBUF_STATIC)
+      set(SAVED_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+      set(CMAKE_FIND_LIBRARY_SUFFIXES .a)
+    endif()
+    find_package(Protobuf 3.0.0 REQUIRED)
+    if(ENABLE_PROTOBUF_STATIC)
+      set(CMAKE_FIND_LIBRARY_SUFFIXES ${SAVED_CMAKE_FIND_LIBRARY_SUFFIXES})
+    endif()
+  else()
+    # Google introduced another breaking change with protobuf 22.x by adding abseil as a new
+    # dependency. https://protobuf.dev/news/2022-08-03/#abseil-dep We do not want abseil, so we stay
+    # with 21.x for now.
+    set(P4C_PROTOBUF_VERSION "21.12")
+    message("Fetching Protobuf version ${P4C_PROTOBUF_VERSION} for P4Tools...")
+
+    # Print out download state while setting up Z3.
+    set(FETCHCONTENT_QUIET_PREV ${FETCHCONTENT_QUIET})
+    set(FETCHCONTENT_QUIET OFF)
+    set(protobuf_BUILD_TESTS OFF CACHE BOOL "Build tests.")
+    set(protobuf_BUILD_PROTOC_BINARIES OFF CACHE BOOL "Build libprotoc and protoc compiler.")
+    # Unity builds do not work for Protobuf...
+    set(SAVED_CMAKE_UNITY_BUILD ${CMAKE_UNITY_BUILD})
+    set(CMAKE_UNITY_BUILD OFF)
+    fetchcontent_declare(
+      protobuf
+      URL https://github.com/protocolbuffers/protobuf/releases/download/v${P4C_PROTOBUF_VERSION}/protobuf-cpp-3.${P4C_PROTOBUF_VERSION}.tar.gz
+      URL_HASH SHA256=4eab9b524aa5913c6fffb20b2a8abf5ef7f95a80bc0701f3a6dbb4c607f73460
+      USES_TERMINAL_DOWNLOAD TRUE
+      GIT_PROGRESS TRUE
+    )
+    # Pull a different binary for MacOS.
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+      fetchcontent_declare(
+        protoc
+        URL https://github.com/protocolbuffers/protobuf/releases/download/v${P4C_PROTOBUF_VERSION}/protoc-${P4C_PROTOBUF_VERSION}-osx-x86_64.zip
+            USES_TERMINAL_DOWNLOAD
+            TRUE
+        GIT_PROGRESS TRUE
+      )
+    else()
+      fetchcontent_declare(
+        protoc
+        URL https://github.com/protocolbuffers/protobuf/releases/download/v${P4C_PROTOBUF_VERSION}/protoc-${P4C_PROTOBUF_VERSION}-linux-x86_64.zip
+        URL_HASH SHA256=3a4c1e5f2516c639d3079b1586e703fc7bcfa2136d58bda24d1d54f949c315e8
+        USES_TERMINAL_DOWNLOAD TRUE
+        GIT_PROGRESS TRUE
+      )
+    endif()
+
+    fetchcontent_makeavailable(protobuf protoc)
+    # Reset unity builds to the previous state...
+    set(CMAKE_UNITY_BUILD ${SAVED_CMAKE_UNITY_BUILD})
+    set(FETCHCONTENT_QUIET ${FETCHCONTENT_QUIET_PREV})
+
+    set(PROTOBUF_PROTOC_EXECUTABLE ${protoc_SOURCE_DIR}/bin/protoc CACHE STRING
+                                                                         "Protoc executable."
+    )
+    set(Protobuf_INCLUDE_DIR ${protobuf_SOURCE_DIR}/src/ CACHE STRING "Protobuf include directory.")
+    set(PROTOBUF_LIBRARY libprotobuf)
+    message("Done with setting up Protobuf for P4C.")
+  endif()
+endmacro(p4c_obtain_protobuf)
--- tools/install_mac_deps.sh
+++ tools/install_mac_deps.sh
@@ -6,20 +6,15 @@ if [[ ! -x $BREW ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Google introduced another breaking change with protobuf 22.x by adding abseil as a new dependency.
-# https://protobuf.dev/news/2022-08-03/#abseil-dep
-# We do not want abseil, so we stay with 21.x for now.
-PROTOBUF_LIB="protobuf@21"
 BOOST_LIB="boost@1.76"
 
 $BREW update
 $BREW install autoconf automake bdw-gc bison ${BOOST_LIB} ccache cmake \
-      libtool openssl pkg-config python coreutils grep ${PROTOBUF_LIB}
+      libtool openssl pkg-config python coreutils grep
 
-# Prefer Homebrew's bison, grep, and protobuf over the macOS-provided version
-$BREW link --force bison grep ${PROTOBUF_LIB} ${BOOST_LIB}
+# Prefer Homebrew's bison and grep over the macOS-provided version
+$BREW link --force bison grep ${BOOST_LIB}
 echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' >> ~/.bash_profile
-echo 'export PATH="/usr/local/opt/${PROTOBUF_LIB}/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"' >> ~/.bash_profile
 export PATH="/usr/local/opt/bison/bin:$PATH"
 export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
