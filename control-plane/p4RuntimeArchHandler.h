357898e58a1676ef266d025e43cfb53576dbcf63
fruffy <fruffy@nyu.edu>
2023-07-27 17:51:28 -0400

    Try to prefix Bazel build.

--- control-plane/p4RuntimeArchHandler.h
+++ control-plane/p4RuntimeArchHandler.h
@@ -22,7 +22,7 @@ limitations under the License.
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#include "p4/config/v1/p4info.pb.h"
+#include "control-plane/p4/config/v1/p4info.pb.h"
 #pragma GCC diagnostic pop
 
 #include "frontends/common/resolveReferences/referenceMap.h"
