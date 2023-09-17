8903af7340e1a83e96cd8753b59281af99e11d1b
fruffy <fruffy@nyu.edu>
2023-09-17 20:28:18 +0200

    Try to use fewer environment variables and instead pass folders directly.

--- tools/dpdk-ci-build.sh
+++ tools/dpdk-ci-build.sh
@@ -3,66 +3,16 @@
 set -e  # Exit on error.
 set -x  # Make command execution verbose
 
+THIS_DIR=$( cd -- "$( dirname -- "${0}" )" &> /dev/null && pwd )
+P4C_DIR=$(readlink -f ${THIS_DIR}/..)
+
 if [ -z "$1" ]; then
-    echo "- Missing mandatory argument: P4C_DIR"
-    echo " - Usage: source dpdk-ci-build.sh <P4C_DIR> <IPDK_RECIPE> <SDE_INSTALL> <DEPEND_INSTALL>"
+    echo "- Missing mandatory argument: IPDK_INSTALL_DIR"
+    echo " - Usage: source dpdk-ci-build.sh <IPDK_INSTALL_DIR>"
     return 0
 fi
 
-if [ -z "$2" ]; then
-    echo "- Missing mandatory argument: IPDK_RECIPE"
-    echo " - Usage: source dpdk-ci-build.sh <P4C_DIR> <IPDK_RECIPE> <SDE_INSTALL> <DEPEND_INSTALL>"
-    return 0
-fi
-
-if [ -z "$3" ]; then
-    echo "- Missing mandatory argument: SDE_INSTALL"
-    echo " - Usage: source dpdk-ci-build.sh <P4C_DIR> <IPDK_RECIPE> <SDE_INSTALL> <DEPEND_INSTALL>"
-    return 0
-fi
-
-if [ -z "$4" ]; then
-    echo "- Missing mandatory argument: DEPEND_INSTALL"
-    echo " - Usage: source dpdk-ci-build.sh <P4C_DIR> <IPDK_RECIPE> <SDE_INSTALL> <DEPEND_INSTALL>"
-    return 0
-fi
-
-export P4C_DIR=$1
-export IPDK_RECIPE=$2
-export SDE_INSTALL=$3
-export DEPEND_INSTALL=$4
-
-
-
-# Update SDE libraries for infrap4d; commands are copied from  ipdk.recipe/scripts/dpdk/dpdk-ci-build.sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib64
-
-
- #if OS =Fedora:   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDE_INSTALL/lib/x86_64-linux-gnu
-
-
-# Update IPDK RECIPE libraries
-export LD_LIBRARY_PATH=$IPDK_RECIPE/install/lib:$IPDK_RECIPE/install/lib64:$LD_LIBRARY_PATH
-export PATH=$IPDK_RECIPE/install/bin:$IPDK_RECIPE/install/sbin:$PATH
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64
-
-# Update Dependent libraries
-export LD_LIBRARY_PATH=$DEPEND_INSTALL/lib:$DEPEND_INSTALL/lib64:$LD_LIBRARY_PATH
-export PATH=$DEPEND_INSTALL/bin:$DEPEND_INSTALL/sbin:$PATH
-export LIBRARY_PATH=$DEPEND_INSTALL/lib:$DEPEND_INSTALL/lib64:$LIBRARY_PATH
-
-echo ""
-echo ""
-echo "Updated Environment Variables ..."
-echo "SDE_INSTALL: $SDE_INSTALL"
-echo "LIBRARY_PATH: $LIBRARY_PATH"
-echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
-echo "PATH: $PATH"
-echo ""
+export IPDK_INSTALL_DIR=$1
 
 # Install P4C deps and build p4c-dpdk
 cd $P4C_DIR
@@ -87,8 +37,8 @@ P4C_DEPS="bison \
           tcpreplay \
           python3-netaddr"
 
-apt-get update
-apt-get install -y --no-install-recommends  ${P4C_DEPS}
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends  ${P4C_DEPS}
 pip3 install --upgrade pip
 pip3 install -r ${P4C_DIR}/requirements.txt
 pip3 install git+https://github.com/p4lang/p4runtime-shell.git
@@ -106,7 +56,8 @@ CMAKE_FLAGS+="
   -DENABLE_PROTOBUF_STATIC=OFF \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DENABLE_TEST_TOOLS=ON \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DIPDK_INSTALL_DIR=${IPDK_INSTALL_DIR}
 "
 
 mkdir build
