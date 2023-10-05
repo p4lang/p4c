5a5eac7e5f165c65ba975847e03ae108570dcca0
Vladimir Still <vladimir.still@intel.com>
2023-10-04 23:23:03 -0700

    Show annotations in actions' dbprint

--- ir/dbprint-p4.cpp
+++ ir/dbprint-p4.cpp
@@ -145,7 +145,7 @@ void IR::ActionFunction::dbprint(std::ostream &out) const {
 }
 
 void IR::P4Action::dbprint(std::ostream &out) const {
-    out << "action " << name << "(";
+    out << annotations << "action " << name << "(";
     const char *sep = "";
     for (auto arg : parameters->parameters) {
         out << sep << arg->direction << ' ' << arg->type << ' ' << arg->name;
