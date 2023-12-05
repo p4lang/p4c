49310bb314cc54de1f952577c333352dd8138371
Vladimir Still <vladimir.still@intel.com>
2023-12-04 23:48:53 -0800

    Add coverageEnabled() to CoverageOptions struct

--- midend/coverage.h
+++ midend/coverage.h
@@ -29,6 +29,10 @@ struct CoverageOptions {
     bool coverTableEntries = false;
     /// Skip tests which do not increase coverage.
     bool onlyCoveringTests = false;
+
+    /// @returns true if and only if at least one of the coverage-collection options (cover*) is
+    /// enabled
+    bool coverageEnabled() const { return coverStatements || coverTableEntries; }
 };
 
 /// Set of nodes used for coverage purposes. Compares nodes based on their
