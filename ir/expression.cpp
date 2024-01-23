f1b5b5dd7984ca062d42d986dd0eb64f74109e0d
Kyle Cripps <kyle@pensando.io>
2024-01-23 14:20:42 -0800

    Attempt to appease clang-format

--- ir/expression.cpp
+++ ir/expression.cpp
@@ -148,20 +148,24 @@ IR::Constant IR::Constant::GetMask(unsigned width) {
 
 cstring IR::BaseListExpression::toString() const {
     cstring str = "{";
-    if (!components.empty())
+    if (!components.empty()) {
         str += " " + components.at(0)->toString();
-    for (unsigned i = 1; i < size(); i++)
+    }
+    for (unsigned i = 1; i < size(); i++) {
         str += ", " + components.at(i)->toString();
+    }
     return str + " }";
 }
 
 cstring IR::StructExpression::toString() const {
     cstring str = "{";
-    if (!components.empty())
+    if (!components.empty()) {
         str += " " + components.at(0)->toString()
             + " = " + components.at(0)->expression->toString();
-    for (unsigned i = 1; i < size(); i++)
+    }
+    for (unsigned i = 1; i < size(); i++) {
         str += ", " + components.at(i)->toString()
             + " = " + components.at(i)->expression->toString();
+    }
     return str + " }";
 }
