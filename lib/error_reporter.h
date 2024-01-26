3b6b56035edc3a2305a44bd995a34b8c7e03e013
Kyle Cripps <kyle@pensando.io>
2024-01-25 16:16:07 -0800

    Do not allow --Wdisable to disable errors.

--- lib/error_reporter.h
+++ lib/error_reporter.h
@@ -231,6 +231,7 @@ class ErrorReporter {
     /// @return the action to take for the given diagnostic, falling back to the
     /// default action if it wasn't overridden via the command line or a pragma.
     DiagnosticAction getDiagnosticAction(cstring diagnostic, DiagnosticAction defaultAction) {
+        if (defaultAction == DiagnosticAction::Error) return defaultAction;
         auto it = diagnosticActions.find(diagnostic);
         if (it != diagnosticActions.end()) return it->second;
         // if we're dealing with warnings and they have been globally modified
