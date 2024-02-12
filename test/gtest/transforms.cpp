a3ba5dc906b93040cf283ef9c43acc381cf3b3b7
Vladimir Still <vladimir.still@intel.com>
2024-02-12 07:33:01 -0800

    Add test for IR::inlineBlock

--- test/gtest/transforms.cpp
+++ test/gtest/transforms.cpp
@@ -18,6 +18,7 @@ limitations under the License.
 #include "helpers.h"
 #include "ir/ir.h"
 #include "ir/visitor.h"
+#include "ir/irutils.h"
 #include "lib/source_file.h"
 
 namespace Test {
@@ -42,5 +43,43 @@ TEST_F(P4C_IR, Transform) {
     auto *n = e->apply(TestTrans(c));
     EXPECT_EQ(e, n);
 }
+
+TEST_F(P4C_IR, InlineBlock) {
+    struct TestTrans : public Transform {
+        explicit TestTrans(const IR::Constant *c) : c(c) {}
+
+        const IR::Node *postorder(IR::AssignmentStatement *a) override {
+            a->right = c;
+            return IR::inlineBlock(*this, {a, a});
+        }
+
+        const IR::Constant *c;
+    };
+
+    const auto *c2 = new IR::Constant(2);
+    const auto *c4 = new IR::Constant(4);
+    const auto *pe = new IR::PathExpression(new IR::Path("foo"));
+    const auto *stmt = new IR::BlockStatement(IR::IndexedVector<IR::StatOrDecl>({
+        new IR::AssignmentStatement(pe, c2),
+        new IR::IfStatement(IR::getBoolLiteral(true),
+            new IR::AssignmentStatement(pe, c2),
+            new IR::BlockStatement(IR::IndexedVector<IR::StatOrDecl>({
+                new IR::AssignmentStatement(pe, c2)
+            })))
+    }));
+
+    const auto *n = stmt->apply(TestTrans(c4));
+    ASSERT_TRUE(n);
+    const auto *bl0 = n->to<IR::BlockStatement>();
+    ASSERT_TRUE(bl0);
+    ASSERT_EQ(bl0->components.size(), 3);
+    EXPECT_TRUE(bl0->components[0]->is<IR::AssignmentStatement>());
+    EXPECT_TRUE(bl0->components[1]->is<IR::AssignmentStatement>());
+    const auto *ifs = bl0->components[2]->to<IR::IfStatement>();
+    ASSERT_TRUE(ifs);
+    EXPECT_TRUE(ifs->ifTrue->is<IR::BlockStatement>());
+    EXPECT_TRUE(ifs->ifFalse->is<IR::BlockStatement>());
+}
+
 
 }  // namespace Test
