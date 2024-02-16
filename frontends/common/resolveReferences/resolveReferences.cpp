dbbf65423b1cca2ef74227693b26013401c634fe
Anton Korobeynikov <anton@korobeynikov.info>
2024-02-15 23:00:02 -0800

    Unbreak non-unity build

--- frontends/common/resolveReferences/resolveReferences.cpp
+++ frontends/common/resolveReferences/resolveReferences.cpp
@@ -16,6 +16,8 @@ limitations under the License.
 
 #include "resolveReferences.h"
 
+#include "frontends/common/parser_options.h"
+
 namespace P4 {
 
 ResolutionContext::ResolutionContext() { anyOrder = P4CContext::get().options().isv1(); }
