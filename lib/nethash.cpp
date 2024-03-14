2f2161caf023679709e56cd513e485b8d89edaab
Vladimir Still <vladimir.still@intel.com>
2024-03-14 13:38:02 -0700

    Shuffle the file-level comments and license to make more sense

--- lib/nethash.cpp
+++ lib/nethash.cpp
@@ -1,13 +1,33 @@
+/* Copyright 2013-present Barefoot Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "lib/nethash.h"
 
 #include <arpa/inet.h>
-
 #include <algorithm>
 
-namespace NetHash {
+/** \file
+ * \author Antonin Bas (antonin@barefootnetworks.com)
+ *
+ * \brief This code was adapted from:
+ * https://github.com/p4lang/behavioral-model/blob/main/src/bm_sim/calculations.cpp
+ * which was in turn adapted from
+ * http://www.barrgroup.com/Embedded-Systems/How-To/CRC-Calculation-C-Code
+ */
 
-/* This code was adapted from:
-    http://www.barrgroup.com/Embedded-Systems/How-To/CRC-Calculation-C-Code */
+namespace NetHash {
 
 template <typename T>
 static T reflect(T data, int nBits) {
