ec060b789cce5ea447e2b25008b53dad41d08b4a
Vladimir Still <vladimir.still@intel.com>
2024-03-19 11:30:01 -0700

    Add more CRC checksum variants to lib/nethash

--- lib/nethash.h
+++ lib/nethash.h
@@ -8,15 +8,23 @@
 /// A collection of hashing functions commonly used in network protocols.
 namespace NetHash {
 
-/// CRC-16/CRC-16-IBM/CRC-16-ANSI (parameters: bit-reflection, polynomial 0x8005, init = 0 and
-/// xor_out = 0).
+/// CRC-16 used in BMv2 (parameters: bit-reflection, polynomial 0x8005, init = 0 and xor_out = 0).
 uint16_t crc16(const uint8_t *buf, size_t len);
+
+/// CRC-16-ANSI/CRC-16-IBM (parameters: NO bit-reflection, polynomial 0x8005, init = 0 and
+/// xor_out = 0).
+uint16_t crc16ANSI(const uint8_t *buf, size_t len);
 
 /// CRC16-CCITT (parameters: NO bit-reflection, polynomial 0x1021, init = ~0 and xor_out = 0).
 uint16_t crcCCITT(const uint8_t *buf, size_t len);
 
-/// CRC-32 (parameters: bit-reflection, polynomial 0x04C11DB7, init = ~0 and xor_out = ~0).
+/// CRC-32, used in BMv2 (parameters: bit-reflection, polynomial 0x04C11DB7, init = ~0 and
+/// xor_out = ~0).
 uint32_t crc32(const uint8_t *buf, size_t len);
+
+/// CRC-32 used for Ethernet FCS (parameters: NO bit-reflection, polynomial 0x04C11DB7, init = ~0
+/// and xor_out = ~0).
+uint32_t crc32FCS(const uint8_t *buf, size_t len);
 
 /// 16-bit ones' complement checksum (used in IP, TCP, UDP, ...).
 uint16_t csum16(const uint8_t *buf, size_t len);
