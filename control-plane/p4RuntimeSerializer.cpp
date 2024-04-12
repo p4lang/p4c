75aaf8281de9f03579be7c071cd00c664d696cfc
Jonathan DiLorenzo <dilo@google.com>
2024-04-12 02:52:10 +0000

    Add tentative support for new platform property annotations.

--- control-plane/p4RuntimeSerializer.cpp
+++ control-plane/p4RuntimeSerializer.cpp
@@ -950,6 +950,39 @@ class P4RuntimeAnalyzer {
                 }
             }
         }
+      
+        // @platform_property annotation
+        for (auto *annotation : decl->getAnnotations()->annotations) {
+            if (annotation->name != "platform_property") continue;
+            // TODO(dilo): Add once supported by P4Runtime protobuf.
+            // auto *platform_property = pkginfo->mutable_platform_property();
+            for (auto *kv : annotation->kv) {
+                auto name = kv->name.name;
+                auto setInt64Field = [kv](cstring fName) {
+                    auto *v = kv->expression->to<IR::Constant>();
+                    if (v == nullptr) {
+                        ::error(ErrorType::ERR_UNSUPPORTED,
+                                "Value for '%1%' key in @platform_property annotation is not a integer", kv);
+                        return;
+                    }
+                    // TODO(dilo): Fix below once supported by P4Runtime protobuf.
+                    ::warning(ErrorType::WARN_IGNORE, "Got Int64 field: %1% with value %2%", fName, static_cast<int64_t>(v->value));
+                    // use Protobuf reflection library to minimize code
+                    // duplication.
+                    // auto *descriptor = platform_property->GetDescriptor();
+                    // auto *f = descriptor->FindFieldByName(static_cast<std::string>(fName));
+                    // platform_property->GetReflection()->SetInt64(platform_property, f,
+                                                        // static_cast<int64_t>(v->value));
+                };
+                if (name == "multicast_table_size" || name == "multicast_table_total_replicas" || 
+                    name == "multicast_table_max_replicas_per_entry") {
+                    setInt64Field(name);
+                } else {
+                    ::warning(ErrorType::WARN_UNKNOWN,
+                              "Unknown key name '%1%' in @platform_property annotation", name);
+                }
+            }
+        }
 
         // add other annotations on the P4 package to the message. @pkginfo is
         // ignored using the unary predicate argument to addAnnotations.
