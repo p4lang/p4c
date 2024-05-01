56e75b1c12c8ed837d30aa08701887809ef12812
fruffy <fruffy@nyu.edu>
2024-05-01 08:33:59 -0400

    Introduce fix for aarch64 GCC compilation.

--- lib/gc.cpp
+++ lib/gc.cpp
@@ -19,7 +19,10 @@ limitations under the License.
 // of this to allow posix_memalign redeclaration with / without exception
 // specifier. As we define posix_memalign below in this file we really need to
 // ensure the proper include order to workaround this weirdness.
+// GCC compiling on arm64 can not find mm_malloc.h. We need to skip this include.
+#if defined(__clang__) || !defined(__aarch64__)
 #include <mm_malloc.h>  // NOLINT(build/include_order)
+#endif
 
 #include "config.h"
 #if HAVE_LIBGC
