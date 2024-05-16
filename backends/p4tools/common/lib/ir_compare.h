6ada4a68ba607de82b72d212057f886034b01fff
fruffy <fruffy@nyu.edu>
2024-05-16 14:42:01 -0400

    Add a hashing function for state and symbolic variables.
    Signed-off-by: fruffy <fruffy@nyu.edu>

--- backends/p4tools/common/lib/ir_compare.h
+++ backends/p4tools/common/lib/ir_compare.h
@@ -25,6 +25,17 @@ struct StateVariableLess {
     }
 };
 
+/// Hash for StateVariable pointers. We only hash the label.
+struct StateVariableHash {
+    size_t operator()(const IR::StateVariable *s1) const {
+        return Util::Hasher<uint64_t>()(s1->hash());
+    }
+
+    size_t operator()(const IR::StateVariable &s1) const {
+        return Util::Hasher<uint64_t>()(s1.hash());
+    }
+};
+
 }  // namespace P4::IR
 
 #endif /* BACKENDS_P4TOOLS_COMMON_LIB_IR_COMPARE_H_ */
