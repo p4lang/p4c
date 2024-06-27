846ccc387ec7e4db3814b0ec38f6b130b2e63ecb
zzmic <100326374+zzmic@users.noreply.github.com>
2024-06-26 21:48:47 -0400

    Experimentally add support for for-loop generations into backends/p4tools/modules/smith/util/util.cpp

--- backends/p4tools/modules/smith/util/util.cpp
+++ backends/p4tools/modules/smith/util/util.cpp
@@ -14,8 +14,9 @@
 namespace P4Tools::P4Smith {
 
 std::string getRandomString(size_t len) {
+    // Add "for" to the list of P4 keywords.
     static const std::vector<std::string> P4_KEYWORDS = {"if",  "void",    "else",
-                                                         "key", "actions", "true"};
+                                                         "key", "actions", "true", "for"};  
     static const std::array<char, 53> ALPHANUMERIC_CHARACTERS = {
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "abcdefghijklmnopqrstuvwxyz"};
@@ -56,6 +57,139 @@ std::string getRandomString(size_t len) {
 
     P4Scope::usedNames.insert(ret);
     return ret;
+}
+
+/// Generate an arbitrary variable name for loop control.
+std::string generateLoopControlVariable() {
+    // Generate a random string of length 1 (i.e., a single character variable name, such as i, j, k, etc.).
+    return getRandomString(1); 
+}
+
+/// Generate a random loop initialization statement.
+std::string generateLoopInitialization(const std::string &var) {
+    std::stringstream ss;
+
+    // TOOD: Add support for other types of initialization.
+    int values[] = {8, 16, 32};
+    int bitFieldWidth = values[Utils::getRandInt(0, 2)];
+    ss << "bit<" << std::to_string(bitFieldWidth) << "> " << var << " = 0";
+    
+    return ss.str();
+}
+
+/// Generate a random loop condition statement.
+std::string generateLoopCondition(const std::string &var) {
+    std::stringstream ss;
+
+    // TODO: Add support for other types of conditions.
+    int upperBound = Utils::getRandInt(0, 10);
+    ss << var << " < " << std::to_string(upperBound);
+
+    return ss.str();
+}
+
+/// Generate a random loop update (only consider increments for now) statement.
+std::string generateLoopUpdate(const std::string &var) {
+    std::stringstream ss;
+
+    // TOOD: Add support for other types of updates.
+    ss << var << " = " << var << " + 1";
+
+    return ss.str();
+}
+
+/// Generate a random loop body that involves one variable.
+std::string generateLoopBody(const std::string &var) {
+    // TODO: Add actual support for the loop body.
+    return "";
+}
+
+/// Generate a random loop body that involves two variables.
+std::string generateLoopBody(const std::string &var1, const std::string &var2) {
+    // TODO: Add actual support for the loop body.
+    return "";
+}
+
+/// Generate a trivial but complete for loop.
+/// The generated for-loop will be similar to `testdata/p4_16_samples/forloop5.p4` (except the loop body).
+std::string generateTrivialForLoop() {
+    std::string loopControlVar = generateLoopControlVariable();
+    std::string loopInit = generateLoopInitialization(loopControlVar);
+    std::string loopCondition = generateLoopCondition(loopControlVar);
+    std::string loopUpdate = generateLoopUpdate(loopControlVar);
+    std::string loopBody = generateLoopBody(loopControlVar);
+
+    std::stringstream ss;
+    ss << "for (" << loopInit << "; " << loopCondition << "; " << loopUpdate << ") {\n";
+    ss << loopBody << "\n";
+    ss << "}";
+    return ss.str();
+}
+
+/// Generate a for-loop iterating over a header stack. 
+// The generated for-loop will be similar to the first for-loop in `testdata/p4_16_samples/forloop1.p4` (except the loop body).
+std::string generateHeaderStackForLoop() {
+    std::string varType = "t2";
+    std::string loopVar = generateLoopControlVariable();
+    std::string stackName = "hdrs.stack";
+    std::string loopBody = generateLoopBody(loopVar);
+
+    std::stringstream ss;
+    ss << "for (" << varType << " " << loopVar << " in " << stackName << ") {\n";
+    ss << loopBody << "\n";
+    ss << "}";
+    return ss.str();
+}
+
+/// Generate a for-loop with multiple initialization and update statements.
+/// The generated for-loop will be similar to the second for-loop in `testdata/p4_16_samples/forloop1.p4` (except the loop body).
+std::string generateMultipleInitUpdateForLoop() {
+    std::string loopVar1 = generateLoopControlVariable();
+    std::string loopVar2 = generateLoopControlVariable();
+    int values[] = {8, 16, 32};
+    int bitFieldWidth = values[Utils::getRandInt(0, 2)];
+    std::string loopInit = "bit<" + std::to_string(bitFieldWidth) + "> " + loopVar1 + " = 0, " + loopVar2 + " = 1";
+    std::string loopCondition = loopVar1 + " < hdrs.head.cnt";
+    std::string loopUpdate = loopVar1 + " = " + loopVar1 + " + 1, " + loopVar2 + " = " + loopVar2 + " << 1";
+    std::string loopBody = generateLoopBody(loopVar1, loopVar2);
+
+    std::stringstream ss;
+    ss << "for (" << loopInit << "; " << loopCondition << "; " << loopUpdate << ") {\n";
+    ss << loopBody << "\n";
+    ss << "}";
+    return ss.str();
+}
+
+/// Generate a range-based for-loop.
+std::string generateRangeBasedForLoop(const std::string &start, const std::string &end) {
+    std::string loopVar = generateLoopControlVariable();
+    int values[] = {8, 16, 32};
+    int bitFieldWidth = values[Utils::getRandInt(0, 2)];
+    
+    std::stringstream ss;
+    ss << "for (bit<" + std::to_string(bitFieldWidth) + "> " << loopVar << " in " << start << " .. " << end << ") {\n";
+    ss << generateLoopBody(loopVar) << "\n";
+    ss << "}";
+    return ss.str();
+}
+
+/// Main/major function to generate a for-loop.
+std::string generateForLoop() {
+    // TODO: Increment the range of the random number generator if more types of for-loop generations are added,
+    int choice = Utils::getRandInt(0, 3);
+
+    switch (choice) {
+        case 0:
+            return generateTrivialForLoop();
+        case 1:
+            return generateHeaderStackForLoop();
+        case 2:
+            return generateMultipleInitUpdateForLoop();
+        case 3:
+            return generateRangeBasedForLoop("0", std::to_string(Utils::getRandInt(0, 10)));
+        default:
+            return generateTrivialForLoop();
+    }
 }
 
 }  // namespace P4Tools::P4Smith
