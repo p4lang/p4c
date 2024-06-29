7e2f53d6e9f856a898c5dc77f7e776829d6765c4
zzmic <100326374+zzmic@users.noreply.github.com>
2024-06-28 23:55:11 -0400

    Rename (and make minor modifications) test/gtest/for_loop_test.cpp to test/gtest/smith_for_loop_test.cpp, and add unit tests for for-in-loops in test/gtest/smith_for_in_loop_test.cpp

---
+++ test/gtest/smith_for_in_loop_test.cpp
@@ -0,0 +1,69 @@
+#include "gtest/gtest.h"
+#include "backends/p4tools/modules/smith/smith.h"
+#include "backends/p4tools/modules/smith/common/declarations.h"
+#include "backends/p4tools/modules/smith/common/expressions.h"
+#include "backends/p4tools/modules/smith/common/generator.h"
+#include "backends/p4tools/modules/smith/common/parser.h"
+#include "backends/p4tools/modules/smith/common/probabilities.h"
+#include "backends/p4tools/modules/smith/common/scope.h"
+#include "backends/p4tools/modules/smith/common/statements.h"
+#include "backends/p4tools/modules/smith/common/table.h"
+#include "backends/p4tools/modules/smith/core/target.h"
+#include "ir/ir.h"
+#include "ir/ir-generated.h"
+
+namespace Test {
+
+class P4SmithForInLoopTest : public ::testing::Test {
+protected:
+    P4Tools::P4Smith::StatementGenerator* generator;
+
+    // TODO(zzmic): Figure out how to properly initialize and clean up the test object.
+    P4SmithForInLoopTest() {}
+    ~P4SmithForInLoopTest() override {}
+};
+
+/// @brief Test the generation of a for-in-loop statement.
+TEST_F(P4SmithForInLoopTest, ForLoopGeneration) {
+    auto forInLoopStmt = generator->genForInLoopStatement(false);
+    ASSERT_NE(forInLoopStmt, nullptr);
+    EXPECT_TRUE(forInLoopStmt->is<IR::ForInStatement>());
+}
+
+/// @brief Test the for-in-loop's declaration variable.
+TEST_F(P4SmithForInLoopTest, CheckForLoopDeclarationVariable) {
+    auto forInLoopStmt = generator->genForInLoopStatement(false);
+    ASSERT_NE(forInLoopStmt, nullptr);
+    EXPECT_TRUE(forInLoopStmt->is<IR::ForInStatement>());
+
+    // TODO(zzmic): Figure out whether this "static_cast" is necessary.
+    auto forInStmt = forInLoopStmt->to<IR::ForInStatement>();
+    ASSERT_NE(forInStmt->decl, nullptr);    
+    EXPECT_TRUE(forInStmt->decl->is<IR::Declaration_Variable>());
+}
+
+/// @brief Test the for-in-loop's collection.
+TEST_F(P4SmithForInLoopTest, CheckForLoopCollection) {
+    auto forInLoopStmt = generator->genForInLoopStatement(false);
+    ASSERT_NE(forInLoopStmt, nullptr);
+    EXPECT_TRUE(forInLoopStmt->is<IR::ForInStatement>());
+
+    // TODO(zzmic): Figure out whether this "static_cast" is necessary.
+    auto forInStmt = forInLoopStmt->to<IR::ForInStatement>();
+    ASSERT_NE(forInStmt, nullptr);
+    EXPECT_TRUE(forInStmt->collection->is<IR::PathExpression>());
+}
+
+/// @brief Test the for-in-loop's body.
+TEST_F(P4SmithForInLoopTest, CheckForLoopBody) {
+    auto forInLoopStmt = generator->genForInLoopStatement(false);
+    ASSERT_NE(forInLoopStmt, nullptr);
+    EXPECT_TRUE(forInLoopStmt->is<IR::ForInStatement>());
+
+    // TODO(zzmic): Figure out whether this "static_cast" is necessary.
+    auto forInStmt = forInLoopStmt->to<IR::ForInStatement>();
+    ASSERT_NE(forInStmt->body, nullptr);
+    EXPECT_FALSE(forInStmt->body->is<IR::Statement>());
+}
+
+} // namespace Test
