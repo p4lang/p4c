a258b7e1adb77ecf9183cdff7b0e8a2eaed32d2d
Kyle Cripps <60898032+kfcripps@users.noreply.github.com>
2024-07-12 15:46:01 -0700

    Fix loc_t operator< when parent is nullptr (#4798)
    Signed-off-by: Kyle Cripps <kyle@pensando.io>

--- midend/def_use.h
+++ midend/def_use.h
@@ -66,7 +66,7 @@ class ComputeDefUse : public Inspector,
         const loc_t *parent;
         bool operator<(const loc_t &a) const {
             if (node != a.node) return node->id < a.node->id;
-            if (!a.parent) return parent != nullptr;
+            if (!parent || !a.parent) return parent != nullptr;
             return *parent < *a.parent;
         }
         template <class T>
