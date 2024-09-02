27c26b9b825ba33552b27d68ed4bf538ddf6d6d4
Anton Korobeynikov <anton@korobeynikov.info>
2024-09-01 18:59:07 -0700

    Fix new linting errors
    Signed-off-by: Anton Korobeynikov <anton@korobeynikov.info>

--- frontends/p4/typeChecking/typeCheckDecl.cpp
+++ frontends/p4/typeChecking/typeCheckDecl.cpp
@@ -173,8 +173,8 @@ bool TypeInferenceBase::checkAbstractMethods(const IR::Declaration_Instance *ins
         if (m->isAbstract) {
             auto [it, inserted] = virt.emplace(m->name, m);
             if (!inserted)
-                P4::error(ErrorType::ERR_DUPLICATE, "%1%: duplicated name (%2% is previous instance)",
-                          m->name, it->second);
+                P4::error(ErrorType::ERR_DUPLICATE,
+                          "%1%: duplicated name (%2% is previous instance)", m->name, it->second);
         }
     }
     if (virt.size() == 0 && inst->initializer == nullptr) return true;
