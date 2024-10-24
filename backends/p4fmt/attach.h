eb7a89758dc3fa0c5fa6b38e816b7e31a016dea6
Nitish Kumar <snapdgnn@proton.me>
2024-10-24 21:41:40 +0530

    [P4fmt]: attaching comments to IR Nodes (#4845)
    * [P4fmt]: attach comments
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * fix: resolve issue with modifying container during iteration
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * refactor: rename variables, remove unused headers
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * doc comments
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * use node's clone-id
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * rebase & resolve nits
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * expose IHasSourceInfo for the Comment class
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * extract comments from InputSources instead of partial SourceInfo
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * remove extraneous method
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * fix missing `toString()` override
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * chore: comment fixes & minor refactoring
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * refactor `parseProgram` fn
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * resolve: nits & suggestions
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * switch to Inspector pass
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    * take globalCommentMap by ref
    
    Signed-off-by: Nitish <snapdgnn@proton.me>
    
    ---------
    
    Signed-off-by: Nitish <snapdgnn@proton.me>

---
+++ backends/p4fmt/attach.h
@@ -0,0 +1,41 @@
+#ifndef BACKENDS_P4FMT_ATTACH_H_
+#define BACKENDS_P4FMT_ATTACH_H_
+
+#include "ir/visitor.h"
+#include "lib/source_file.h"
+
+namespace P4::P4Fmt {
+
+class Attach : public Inspector {
+ public:
+    using NodeId = int;
+    struct Comments {
+        std::vector<const Util::Comment *> prefix;
+        std::vector<const Util::Comment *> suffix;
+    };
+    using CommentsMap = std::unordered_map<NodeId, Comments>;
+    enum class TraversalType { Preorder, Postorder };
+
+    explicit Attach(std::unordered_map<const Util::Comment *, bool> &processedComments)
+        : processedComments(processedComments){};
+
+    void attachCommentsToNode(const IR::Node *, TraversalType);
+
+    bool preorder(const IR::Node *node) override;
+    void postorder(const IR::Node *node) override;
+
+    void addPrefixComments(NodeId, const Util::Comment *);
+    void addSuffixComments(NodeId, const Util::Comment *);
+    const CommentsMap &getCommentsMap() const;
+
+ private:
+    /// This Hashmap tracks each comment’s attachment status to IR nodes. Initially, all comments
+    /// are set to 'false'.
+    std::unordered_map<const Util::Comment *, bool> &processedComments;
+
+    CommentsMap commentsMap;
+};
+
+}  // namespace P4::P4Fmt
+
+#endif /* BACKENDS_P4FMT_ATTACH_H_ */
