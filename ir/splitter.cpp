a81ebe2f15ba82289e74cd0adc379ca056e31162
Vladimír Štill <vladimir.still@altera.com>
2025-02-28 21:14:34 +0100

    WIP: Statement splitter
    Signed-off-by: Vladimír Štill <vladimir.still@altera.com>

---
+++ ir/splitter.cpp
@@ -0,0 +1,194 @@
+/*
+Copyright 2025-present Altera Corporation.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "splitter.h"
+
+#include <utility>
+#include <vector>
+
+#include "frontends/common/resolveReferences/referenceMap.h"
+#include "ir/ir-traversal.h"
+#include "ir/visitor.h"
+
+namespace P4 {
+
+struct StatementSplitter : Inspector {
+    StatementSplitter(
+        std::function<bool(const IR::Statement *, const Visitor::Context *)> predicate,
+        P4::NameGenerator &nameGen)
+        : predicate(predicate), nameGen(nameGen) {}
+
+    bool preorder(const IR::Statement *stmt) override {
+        handleStmt(stmt);
+        return false;
+    }
+
+    bool handleStmt(const IR::Statement *stmt) {
+        BUG_CHECK(result.before == nullptr && result.after == nullptr,
+                  "More than one leaf statement found: %1% and %2%",
+                  result.before ? result.before : result.after, stmt);
+        if (predicate(stmt, getChildContext())) {
+            result.after = stmt;
+            return true;
+        }
+        result.before = stmt;
+        return false;
+    }
+
+    bool preorder(const IR::BlockStatement *bs) override {
+        if (handleStmt(bs)) {
+            // split on the bs itself
+            return false;
+        }
+
+        for (size_t i = 0, sz = bs->components.size(); i < sz; i++) {
+            StatementSplitter sub(predicate, nameGen);
+            bs->apply(sub, getChildContext());
+            const auto &[before, after, hoist] = sub.result;
+            if (after) {
+                auto *copy = bs->clone();
+                copy->components.erase(copy->components.begin() + i, copy->components.end());
+                if (before) {
+                    copy->components.push_back(before);
+                }
+                result.before = copy;
+                copy = bs->clone();
+                copy->components.erase(copy->components.begin(), copy->components.begin() + i);
+                copy->components.replace(copy->components.begin(), after);
+                result.after = copy;
+                addHoisted(hoist);
+            }
+        }
+        return false;
+    }
+
+    bool preorder(const IR::IfStatement *ifs) override {
+        if (handleStmt(ifs)) {
+            return false;  // split on the if itself
+        }
+
+        auto [results, anySplit] = splitBranches({ifs->ifTrue, ifs->ifFalse});
+        if (!anySplit) {
+            return false;
+        }
+
+        IR::ID condName{nameGen.newName("cond"), nullptr};
+        const auto &si = ifs->srcInfo;
+        const auto *decl = new IR::Declaration_Variable(si, condName, IR::Type::Boolean::get());
+        result.hoistedDeclarations.push_back(decl);
+
+        const auto *condPE = new IR::PathExpression(si, new IR::Path(si, condName));
+        const auto *asgn = new IR::AssignmentStatement(si, condPE, ifs->condition);
+
+        auto *beforeIf = ifs->clone();
+        beforeIf->condition = condPE->clone();
+        beforeIf->ifTrue = results[0].before;
+        beforeIf->ifFalse = results[1].before;
+        result.before = new IR::BlockStatement(si, {asgn, beforeIf});
+
+        auto *afterIf = beforeIf->clone();
+        afterIf->ifTrue = results[0].after;
+        afterIf->ifFalse = results[1].after;
+        result.after = afterIf;
+        return false;
+    }
+
+    bool preorder(const IR::SwitchStatement *sw) override {
+        if (handleStmt(sw)) {
+            return false;  // split on the switch itself
+        }
+
+        std::vector<const IR::Statement *> branches;
+        for (const auto *case_ : sw->cases) {
+            branches.push_back(case_->statement);
+        }
+        auto [results, anySplit] = splitBranches(branches);
+
+        if (!anySplit) {
+            return false;
+        }
+        // ensure we don't accidentally create fallthrough
+        for (size_t i = 0; i < branches.size(); ++i) {
+            for (const auto **val : {&results[i].before, &results[i].after}) {
+                if (!*val && branches[i]) {
+                    *val = new IR::BlockStatement(branches[i]->srcInfo);
+                }
+            }
+        }
+
+        auto *beforeSw = sw->clone();
+        for (size_t i = 0; i < branches.size(); ++i) {
+            setCase(beforeSw, i, results[i].before);
+        }
+        result.before = beforeSw;
+
+        auto *afterSw = sw->clone();
+        for (size_t i = 0; i < branches.size(); ++i) {
+            setCase(afterSw, i, results[i].after);
+        }
+        result.after = afterSw;
+        return false;
+    }
+
+    SplitResult<IR::Statement> result;
+
+ private:
+    void setCase(IR::SwitchStatement *sw, size_t i, const IR::Statement *value) {
+        modify(sw, &IR::SwitchStatement::cases, IR::Traversal::Index(i), &IR::SwitchCase::statement,
+               IR::Traversal::Assign(value));
+    }
+
+    void addHoisted(const std::vector<const IR::Declaration *> decls) {
+        result.hoistedDeclarations.insert(result.hoistedDeclarations.end(), decls.begin(),
+                                          decls.end());
+    }
+
+    std::pair<std::vector<SplitResult<IR::Statement>>, bool> splitBranches(
+        std::vector<const IR::Statement *> branches) {
+        std::vector<SplitResult<IR::Statement>> res;
+        bool anySplit = false;
+        res.reserve(branches.size());
+
+        for (const auto *branch : branches) {
+            if (!branch) {
+                res.emplace_back();
+                continue;
+            }
+            StatementSplitter sub(predicate, nameGen);
+            branch->apply(sub, getChildContext());
+            if (sub.result.after) {
+                anySplit = true;
+            }
+            addHoisted(sub.result.hoistedDeclarations);
+            res.emplace_back(std::move(sub.result));
+        }
+        return {res, anySplit};
+    }
+
+    std::function<bool(const IR::Statement *, const Visitor::Context *)> predicate;
+    P4::NameGenerator &nameGen;
+};
+
+SplitResult<IR::Statement> splitStatementBefore(
+    const IR::Statement *stat,
+    std::function<bool(const IR::Statement *, const P4::Visitor_Context *)> predicate,
+    P4::NameGenerator &nameGen) {
+    StatementSplitter split(predicate, nameGen);
+    stat->apply(split);
+    return split.result;
+}
+
+}  // namespace P4
