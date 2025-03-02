50ff3e954d8c6f78c16c2d23469894cdb38735fd
Andy Fingerhut <andy.fingerhut@gmail.com>
2025-03-02 16:54:20 -0500

    Add extra debug in failing EBPF test
    Signed-off-by: Andy Fingerhut <andy.fingerhut@gmail.com>

--- backends/ebpf/tests/ptf/test.py
+++ backends/ebpf/tests/ptf/test.py
@@ -683,6 +683,7 @@ class PSATernaryTest(P4EbpfTest):
         pkt = testutils.simple_udp_packet(
             eth_src="11:22:33:44:55:66", ip_src="1.2.3.4", ip_dst="192.168.2.1"
         )
+        pkt.show()
         testutils.send_packet(self, PORT0, pkt)
         pkt[Ether].type = 0x1122
         pkt[IP].proto = 0x7
@@ -691,6 +692,7 @@ class PSATernaryTest(P4EbpfTest):
         pkt[IP].src = "17.17.17.17"
         pkt[IP].dst = "255.255.255.255"
         pkt[UDP].chksum = 0x044D
+        pkt.show()
         testutils.verify_packet(self, pkt, PORT1)
 
 
