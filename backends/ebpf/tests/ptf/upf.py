21fdf396365f4cf4a51769544bb1b9442f0d64ce
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-03-19 14:59:18 -0400

    Add SPDX-License-Identifier: GPL-2.0-only to Python source files using scapy (#5111)
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
    Signed-off-by: Andy Fingerhut <andy.fingerhut@gmail.com>

--- backends/ebpf/tests/ptf/upf.py
+++ backends/ebpf/tests/ptf/upf.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # Copyright 2022-present Orange
 # Copyright 2022-present Open Networking Foundation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: GPL-2.0-only
+# Reason-GPL: import-scapy
+# Note: This program was originally contributed with the expectation
+# that it would be licensed as Apache-2.0.  It is only licensed as
+# GPL-2.0-only because it imports the GPL-2.0-only module Scapy.  If
+# someone later modifies it so that it no longer uses Scapy, nor any
+# other GPL modules, please change the license to Apache-2.0.
 
 from common import *
 from scapy.contrib.gtp import GTP_U_Header
