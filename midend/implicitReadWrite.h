61ab5ab042f2172474077050167f5907f8e7913a
Chris Dodd <cdodd@nvidia.com>
2025-07-02 03:04:10 +0000

    Allow implict read/write of extern instances
    Signed-off-by: Chris Dodd <cdodd@nvidia.com>

---
+++ midend/implicitReadWrite.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MIDEND_IMPLICITREADWRITE_H_
+#define MIDEND_IMPLICITREADWRITE_H_
+
+#include "frontends/p4/typeMap.h"
+#include "ir/pass_manager.h"
+
+namespace P4 {
+
+class ImplicitReadWrite : public PassManager {
+ public:
+    explicit ImplicitReadWrite(TypeMap *typeMap = nullptr);
+};
+
+}  // namespace P4
+
+#endif /* MIDEND_IMPLICITREADWRITE_H_ */
