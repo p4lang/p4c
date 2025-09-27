ec57bdcaf6c4ece7a474ad6d25952bb6273b9695
fruffy <fruffy@nyu.edu>
2025-09-27 12:39:49 +0200

    Make P4-14 optional.
    Signed-off-by: fruffy <fruffy@nyu.edu>

--- backends/bmv2/common/extern.cpp
+++ backends/bmv2/common/extern.cpp
@@ -5,7 +5,6 @@
 
 #include "extern.h"
 
-#include "frontends/p4-14/fromv1.0/v1model.h"
 #include "lib/json.h"
 
 namespace P4::BMV2 {
--- backends/bmv2/simple_switch/simpleSwitch.h
+++ backends/bmv2/simple_switch/simpleSwitch.h
@@ -23,7 +23,6 @@
 #include "backends/bmv2/common/sharedActionSelectorCheck.h"
 #include "backends/common/programStructure.h"
 #include "frontends/common/constantFolding.h"
-#include "frontends/p4-14/fromv1.0/v1model.h"
 #include "frontends/p4/evaluator/evaluator.h"
 #include "frontends/p4/simplify.h"
 #include "frontends/p4/unusedDeclarations.h"
