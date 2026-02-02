9611e116f939bf26a916b9e3349ba2d29fb44c24
Jiri Havranek <jiri@pantherun.com>
2026-02-02 17:17:14 +0100

    Added v1model version check
    Signed-off-by: Jiri Havranek <jiri@pantherun.com>

--- frontends/p4/getV1ModelVersion.h
+++ frontends/p4/getV1ModelVersion.h
@@ -1,6 +1,8 @@
 #ifndef FRONTENDS_P4_GETV1MODELVERSION_H_
 #define FRONTENDS_P4_GETV1MODELVERSION_H_
 
+#include "absl/strings/numbers.h"
+#include "frontends/p4-14/fromv1.0/v1model.h"
 #include "ir/ir.h"
 #include "ir/visitor.h"
 
@@ -19,6 +21,19 @@ class GetV1ModelVersion : public Inspector {
                 return false;
             }
             version = static_cast<unsigned>(val->value);
+
+            unsigned initialVersion;
+            unsigned currentVersion;
+            BUG_CHECK(absl::SimpleAtoi(V1Model::versionInitial, &initialVersion) &&
+                          absl::SimpleAtoi(V1Model::versionCurrent, &currentVersion),
+                      "Unable to check v1model version");
+            if (version < initialVersion || version > currentVersion) {
+                P4::error(
+                    ErrorType::ERR_UNKNOWN,
+                    "%1%: unknown v1model version; initial supported version is %2%, latest is %3%",
+                    dc->initializer, initialVersion, currentVersion);
+                return false;
+            }
         }
         return false;
     }
