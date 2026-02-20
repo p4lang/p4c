1005d2ef9cb83e8f7d8bdd9bad297ca19c21fa9d
Attaullah Ansari <mdattaullahansari152@gmail.com>
2026-02-20 06:27:37 +0530

    Make ErrorType constants constexpr
    Move the ErrorType integer constants from out-of-line definitions in
    error_catalog.cpp to inline constexpr initializers in error_catalog.h.
    This resolves the FIXME in the class definition.
    
    Signed-off-by: Attaullah Ansari <mdattaullahansari152@gmail.com>

--- lib/error_catalog.h
+++ lib/error_catalog.h
@@ -36,73 +36,74 @@ class ErrorType {
  public:
     // -------- Errors -------------
     // errors as initially defined with a format string
-    // FIXME: make these constexpr
-    static const int LEGACY_ERROR;
-    static const int ERR_UNKNOWN;                // unknown construct (in context)
-    static const int ERR_UNSUPPORTED;            // unsupported construct
-    static const int ERR_UNEXPECTED;             // unexpected construct
-    static const int ERR_UNINITIALIZED;          // uninitialized reads/writes
-    static const int ERR_EXPECTED;               // language, compiler expects a different construct
-    static const int ERR_NOT_FOUND;              // A different way to say ERR_EXPECTED
-    static const int ERR_INVALID;                // invalid construct
-    static const int ERR_EXPRESSION;             // expression related errors
-    static const int ERR_OVERLIMIT;              // program node exceeds target limits
-    static const int ERR_INSUFFICIENT;           // program node does not have enough of ...
-    static const int ERR_TYPE_ERROR;             // P4 type checking errors
-    static const int ERR_UNSUPPORTED_ON_TARGET;  // target can not handle construct
-    static const int ERR_DUPLICATE;              // duplicate objects
-    static const int ERR_IO;                     // IO error
-    static const int ERR_UNREACHABLE;            // unreachable code
-    static const int ERR_MODEL;                  // something is wrong with the target model
-    static const int ERR_TABLE_KEYS;             // something is wrong with a table key
-    static const int ERR_RESERVED;               // Reserved for target use
+    static constexpr int LEGACY_ERROR = 0;
+    static constexpr int ERR_UNKNOWN = 1;        // unknown construct (in context)
+    static constexpr int ERR_UNSUPPORTED = 2;    // unsupported construct
+    static constexpr int ERR_UNEXPECTED = 3;     // unexpected construct
+    static constexpr int ERR_UNINITIALIZED = 4;  // uninitialized reads/writes
+    static constexpr int ERR_EXPECTED = 5;       // language, compiler expects a different construct
+    static constexpr int ERR_NOT_FOUND = 6;      // A different way to say ERR_EXPECTED
+    static constexpr int ERR_INVALID = 7;        // invalid construct
+    static constexpr int ERR_EXPRESSION = 8;     // expression related errors
+    static constexpr int ERR_OVERLIMIT = 9;      // program node exceeds target limits
+    static constexpr int ERR_INSUFFICIENT = 10;  // program node does not have enough of ...
+    static constexpr int ERR_TYPE_ERROR = 11;    // P4 type checking errors
+    static constexpr int ERR_UNSUPPORTED_ON_TARGET = 12;  // target can not handle construct
+    static constexpr int ERR_DUPLICATE = 13;              // duplicate objects
+    static constexpr int ERR_IO = 14;                     // IO error
+    static constexpr int ERR_UNREACHABLE = 15;            // unreachable code
+    static constexpr int ERR_MODEL = 16;       // something is wrong with the target model
+    static constexpr int ERR_TABLE_KEYS = 17;  // something is wrong with a table key
+    static constexpr int ERR_RESERVED = 18;    // Reserved for target use
     // Backends should extend this class with additional errors in the range 500-999.
-    static const int ERR_MIN_BACKEND = 500;  // first allowed backend error code
-    static const int ERR_MAX = 999;          // last allowed error code
+    static constexpr int ERR_MIN_BACKEND = 500;  // first allowed backend error code
+    static constexpr int ERR_MAX = 999;          // last allowed error code
 
     // -------- Warnings -----------
     // warnings as initially defined with a format string
-    static const int LEGACY_WARNING;
-    static const int WARN_FAILED;                   // non-fatal failure!
-    static const int WARN_UNKNOWN;                  // unknown construct (in context)
-    static const int WARN_INVALID;                  // invalid construct
-    static const int WARN_UNSUPPORTED;              // unsupported construct
-    static const int WARN_DEPRECATED;               // deprecated feature
-    static const int WARN_UNINITIALIZED;            // unitialized instance
-    static const int WARN_UNINITIALIZED_USE;        // use of uninitialized value
-    static const int WARN_UNINITIALIZED_OUT_PARAM;  // output parameter may be uninitialized
-    static const int WARN_UNUSED;                   // unused instance
-    static const int WARN_MISSING;                  // missing construct
-    static const int WARN_ORDERING;                 // inconsistent statement ordering
-    static const int WARN_MISMATCH;                 // mismatched constructs
-    static const int WARN_OVERFLOW;                 // values do not fit
-    static const int WARN_IGNORE_PROPERTY;          // invalid property for object, ignored
-    static const int WARN_TYPE_INFERENCE;           // type inference can not infer, substitutes
-    static const int WARN_PARSER_TRANSITION;        // parser transition non-fatal issues
-    static const int WARN_UNREACHABLE;              // unreachable code
-    static const int WARN_SHADOWING;                // instance shadowing
-    static const int WARN_IGNORE;                   // simply ignore
-    static const int WARN_INVALID_HEADER;           // access to fields of an invalid header
-    static const int WARN_DUPLICATE_PRIORITIES;     // two entries with the same priority
-    static const int WARN_ENTRIES_OUT_OF_ORDER;     // entries with priorities out of order
-    static const int WARN_MULTI_HDR_EXTRACT;        // same header may be extracted more than once
-    static const int WARN_EXPRESSION;               // expression related warnings
-    static const int WARN_DUPLICATE;                // duplicate objects
-    static const int WARN_BRANCH_HINT;              // branch frequency/likely hints
-    static const int WARN_TABLE_KEYS;               // something is wrong with a table key
+    static constexpr int LEGACY_WARNING = ERR_MAX + 1;
+    static constexpr int WARN_FAILED = 1001;             // non-fatal failure!
+    static constexpr int WARN_UNKNOWN = 1002;            // unknown construct (in context)
+    static constexpr int WARN_INVALID = 1003;            // invalid construct
+    static constexpr int WARN_UNSUPPORTED = 1004;        // unsupported construct
+    static constexpr int WARN_DEPRECATED = 1005;         // deprecated feature
+    static constexpr int WARN_UNINITIALIZED = 1006;      // unitialized instance
+    static constexpr int WARN_UNINITIALIZED_USE = 1019;  // use of uninitialized value
+    static constexpr int WARN_UNINITIALIZED_OUT_PARAM =
+        1018;                                          // output parameter may be uninitialized
+    static constexpr int WARN_UNUSED = 1007;           // unused instance
+    static constexpr int WARN_MISSING = 1008;          // missing construct
+    static constexpr int WARN_ORDERING = 1009;         // inconsistent statement ordering
+    static constexpr int WARN_MISMATCH = 1010;         // mismatched constructs
+    static constexpr int WARN_OVERFLOW = 1011;         // values do not fit
+    static constexpr int WARN_IGNORE_PROPERTY = 1012;  // invalid property for object, ignored
+    static constexpr int WARN_TYPE_INFERENCE = 1013;   // type inference can not infer, substitutes
+    static constexpr int WARN_PARSER_TRANSITION = 1014;     // parser transition non-fatal issues
+    static constexpr int WARN_UNREACHABLE = 1015;           // unreachable code
+    static constexpr int WARN_SHADOWING = 1016;             // instance shadowing
+    static constexpr int WARN_IGNORE = 1017;                // simply ignore
+    static constexpr int WARN_INVALID_HEADER = 1020;        // access to fields of an invalid header
+    static constexpr int WARN_DUPLICATE_PRIORITIES = 1021;  // two entries with the same priority
+    static constexpr int WARN_ENTRIES_OUT_OF_ORDER = 1022;  // entries with priorities out of order
+    static constexpr int WARN_MULTI_HDR_EXTRACT =
+        1023;                                      // same header may be extracted more than once
+    static constexpr int WARN_EXPRESSION = 1024;   // expression related warnings
+    static constexpr int WARN_DUPLICATE = 1025;    // duplicate objects
+    static constexpr int WARN_BRANCH_HINT = 1026;  // branch frequency/likely hints
+    static constexpr int WARN_TABLE_KEYS = 1027;   // something is wrong with a table key
     // Backends should extend this class with additional warnings in the range 1500-2141.
-    static const int WARN_MIN_BACKEND = 1500;  // first allowed backend warning code
-    static const int WARN_MAX = 2141;          // last allowed warning code
+    static constexpr int WARN_MIN_BACKEND = 1500;  // first allowed backend warning code
+    static constexpr int WARN_MAX = 2141;          // last allowed warning code
 
     // -------- Info messages -------------
     // info messages as initially defined with a format string
-    static const int INFO_INFERRED;  // information inferred by compiler
-    static const int INFO_PROGRESS;  // compilation progress
-    static const int INFO_REMOVED;   // instance removed
+    static constexpr int INFO_INFERRED = WARN_MAX + 1;  // information inferred by compiler
+    static constexpr int INFO_PROGRESS = 2143;          // compilation progress
+    static constexpr int INFO_REMOVED = 2144;           // instance removed
 
     // Backends should extend this class with additional info messages in the range 3000-3999.
-    static const int INFO_MIN_BACKEND = 3000;  // first allowed backend info code
-    static const int INFO_MAX = 3999;          // last allowed info code
+    static constexpr int INFO_MIN_BACKEND = 3000;  // first allowed backend info code
+    static constexpr int INFO_MAX = 3999;          // last allowed info code
 };
 
 class ErrorCatalog {
