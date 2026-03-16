cc4ceb1e9a523c2cf319a4028b1b86a6d6f1eec0
Kyle Cripps <kyle@pensando.io>
2026-03-16 16:01:12 -0700

    Try to fix XFAIL reason for test testgen-tofino/switchml.p4
    Signed-off-by: Kyle Cripps <kyle@pensando.io>

--- backends/p4tools/modules/testgen/targets/tofino/test/TofinoXfail.cmake
+++ backends/p4tools/modules/testgen/targets/tofino/test/TofinoXfail.cmake
@@ -307,7 +307,7 @@ p4tools_add_xfail_reason(
 
 p4tools_add_xfail_reason(
   "testgen-tofino"
-  "exceeded the maximum number of permitted guard violations for this run"
+  "PHV allocation was not successful (1 set of unsatisfiable constraints remaining)"
   # lookahead1.p4
   switchml.p4
   # parser_multi_write_checksum_verify_5.p4
