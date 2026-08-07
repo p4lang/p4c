c8eda6a2be26c5d2289fdb5e33468cda214f1dc6
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-08-07 11:26:57 -0400

    Add copyright and license annotations for files added by Steffen Smolka
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- tools/format-bazel-files.sh
+++ tools/format-bazel-files.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2025 Google Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # Formats and fixes all Bazel files (*.bazel, *.bzl) using Buildifier.
 bazel run -- @buildifier_prebuilt//:buildifier \
   --lint=fix \
