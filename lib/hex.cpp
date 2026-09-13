1551cec644d61e4c021feeab23b2e12fe89873e2
Devansh-567 <devansh.jay.singh@gmail.com>
2026-09-13 11:30:53 +0530

    Remove unrelated hexvec changes
    Signed-off-by: Devansh-567 <devansh.jay.singh@gmail.com>

--- lib/hex.cpp
+++ lib/hex.cpp
@@ -14,9 +14,8 @@ std::ostream &operator<<(std::ostream &os, const hexvec &h) {
     auto save = os.flags();
     auto save_fill = os.fill();
     char *p = reinterpret_cast<char *>(h.data);
-    os << '[';
     for (size_t i = 0; i < h.len; i++, p += h.elsize) {
-        if (i) os << ' ';
+        os << (i ? ' ' : '[');
         uintmax_t val;
         switch (h.elsize) {
             case 1:
